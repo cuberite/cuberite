cc2b15a2335db9a2c1f2e8549b028b179b105a1a
faketruth <faketruth@0a769ca7-a7f5-676a-18bf-c427514a06d6>
2011-10-03 18:40:09 +0000

    Visual Studio 2010 solution and project files
    git-svn-id: http://mc-server.googlecode.com/svn/trunk@2 0a769ca7-a7f5-676a-18bf-c427514a06d6

---
+++ VC2010/clean.bat
@@ -0,0 +1,25 @@
+del *.ncb
+del *.ilk
+del *.lib
+del *.exp
+del *.map
+del *.pdb
+del *.bsc
+del *.sdf
+del *.opensdf
+del *.insprc
+del *.aps
+del applog.txt
+del tmpl83.00c.vcproj.LAPTOPF.Kevin.user
+del *.suo /AH
+del debug\*.* /Q
+del release\*.* /Q
+del x64\*.* /Q
+del "My Inspector Results"\*.* /Q
+del ipch\*.* /Q
+rd release /Q
+rd debug /Q
+rd ipch /Q
+rd x64 /Q
+rd "My Inspector Results" /Q
+rd ipch /Q
