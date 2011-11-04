2ffedf16aeb95dda3771fded0db98c9825e79b3b
admin@omencraft.com <admin@omencraft.com@0a769ca7-a7f5-676a-18bf-c427514a06d6>
2011-11-04 07:50:18 +0000

    Changed use of deprecated function.
    git-svn-id: http://mc-server.googlecode.com/svn/trunk@53 0a769ca7-a7f5-676a-18bf-c427514a06d6

--- source/cRedstone.cpp
+++ source/cRedstone.cpp
@@ -1,4 +1,5 @@
 #include "cRedstone.h"
+#include "cRoot.h"
 #include "cWorld.h"
 
 
@@ -16,50 +17,54 @@ void cRedstone::ChangeRedstoneTorch( int fillx, int filly, int fillz, bool added
         } else {
                 metadata = 0;
         }
-        if ( ( m_World->GetWorld()->GetBlock( fillx+1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
+
+	cWorld* World = cRoot::Get()->GetWorld();
+
+        if ( ( (int)World->GetBlock( fillx+1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx+1, filly, fillz, metadata );
         }
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx-1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx-1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx-1, filly, fillz, metadata );
         }
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx, filly, fillz+1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx, filly, fillz+1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx, filly, fillz+1, metadata );
         }
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx, filly, fillz-1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx, filly, fillz-1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx, filly, fillz-1, metadata );
         }
 
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx+1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx+1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx+1, filly-1, fillz, metadata );
         }
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx-1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx-1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx-1, filly-1, fillz, metadata );
         }
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx, filly, fillz+1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx, filly, fillz+1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx, filly-1, fillz+1, metadata );
         }
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx, filly, fillz-1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx, filly, fillz-1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx, filly-1, fillz-1, metadata );
         }
 
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx+1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx+1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx+1, filly+1, fillz, metadata );
         }
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx-1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx-1, filly, fillz ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx-1, filly+1, fillz, metadata );
         }
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx, filly, fillz+1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx, filly, fillz+1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx, filly+1, fillz+1, metadata );
         }
-        if ( ((int)m_World->GetWorld()->GetBlock( fillx, filly, fillz-1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
+        if ( ( (int)World->GetBlock( fillx, filly, fillz-1 ) == E_BLOCK_REDSTONE_WIRE)  ) {
                 LightRedstone( fillx, filly+1, fillz-1, metadata );
         }
 }
 
 void cRedstone::LightRedstone( int fillx, int filly, int fillz, char metadata)
 {
-    if ( ( (int)m_World->GetWorld()->GetBlock( fillx, filly, fillz ) == E_BLOCK_REDSTONE_WIRE) && ( (int)m_World->GetWorld()->GetBlockMeta( fillx, filly, fillz) != metadata ) ) {
-        m_World->GetWorld()->SetBlock( fillx, filly, fillz, (char)E_BLOCK_REDSTONE_WIRE, metadata );
+    cWorld* World = cRoot::Get()->GetWorld();
+    if ( ( (int)World->GetBlock( fillx, filly, fillz ) == E_BLOCK_REDSTONE_WIRE) && ( (int)World->GetBlockMeta( fillx, filly, fillz) != metadata ) ) {
+        World->SetBlock( fillx, filly, fillz, (char)E_BLOCK_REDSTONE_WIRE, metadata );
         LightRedstone(fillx-1,filly,fillz,metadata);
         LightRedstone(fillx+1,filly,fillz,metadata);
         LightRedstone(fillx,filly,fillz-1,metadata);
