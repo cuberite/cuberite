ea1ecd36db72910ce4198ea1ddb073481470cae6
faketruth <faketruth@0a769ca7-a7f5-676a-18bf-c427514a06d6>
2012-03-06 13:25:14 +0000

    Fixed bug FS#157 http://mc-server.org/support/index.php?do=details&task_id=157
    git-svn-id: http://mc-server.googlecode.com/svn/trunk@370 0a769ca7-a7f5-676a-18bf-c427514a06d6

--- source/cWorld.cpp
+++ source/cWorld.cpp
@@ -261,11 +261,11 @@ cWorld::cWorld( const AString & a_WorldName )
 	m_SimulatorManager->RegisterSimulator(m_FireSimulator, 10);
 	m_SimulatorManager->RegisterSimulator(m_RedstoneSimulator, 1);
 
-	memset( g_BlockLightValue, 0x0, ARRAYSIZE( g_BlockLightValue ) );
-	memset( g_BlockSpreadLightFalloff, 0xf, ARRAYSIZE( g_BlockSpreadLightFalloff ) ); // 0xf means total falloff
-	memset( g_BlockTransparent, 0x0, ARRAYSIZE( g_BlockTransparent ) );
-	memset( g_BlockOneHitDig, 0x0, ARRAYSIZE( g_BlockOneHitDig ) );
-	memset( g_BlockPistonBreakable, 0x0, ARRAYSIZE( g_BlockPistonBreakable ) );
+	memset( g_BlockLightValue, 0x0, ARRAYCOUNT( g_BlockLightValue ) );
+	memset( g_BlockSpreadLightFalloff, 0xf, ARRAYCOUNT( g_BlockSpreadLightFalloff ) ); // 0xf means total falloff
+	memset( g_BlockTransparent, 0x0, ARRAYCOUNT( g_BlockTransparent ) );
+	memset( g_BlockOneHitDig, 0x0, ARRAYCOUNT( g_BlockOneHitDig ) );
+	memset( g_BlockPistonBreakable, 0x0, ARRAYCOUNT( g_BlockPistonBreakable ) );
 
 	// Emissive blocks
 	g_BlockLightValue[ E_BLOCK_TORCH ] =			14;
