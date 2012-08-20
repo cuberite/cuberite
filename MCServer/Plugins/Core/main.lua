12906c026d414c752d3c0ba9481f425b24b29c67
faketruth <faketruth@0a769ca7-a7f5-676a-18bf-c427514a06d6>
2012-08-20 12:20:20 +0000

    Moved files used by MCServer to a special MCServer folder
    git-svn-id: http://mc-server.googlecode.com/svn/trunk@763 0a769ca7-a7f5-676a-18bf-c427514a06d6

--- MCServer/Plugins/Core/main.lua
+++ MCServer/Plugins/Core/main.lua
@@ -140,6 +140,7 @@ function Initialize( Plugin )
 	WebPlugin:AddTab( "Whitelist", HandleRequest_WhiteList )
 	WebPlugin:AddTab( "Permissions", HandleRequest_Permissions )
 	WebPlugin:AddTab( "Manage Plugins", HandleRequest_ManagePlugins )
+	WebPlugin:AddTab( "Ini Editor", HandleRequest_IniEditor )
 	
 	LOG( "Initialized " .. Plugin:GetName() .. " v." .. Plugin:GetVersion() )
 	return true
