573480b1fe9a582f434eecae9ad37620aecfe566
madmaxoft@gmail.com <madmaxoft@gmail.com@0a769ca7-a7f5-676a-18bf-c427514a06d6>
2012-10-20 19:38:03 +0000

    Got rid of nonsense logging in cPiston's direction calc.
    git-svn-id: http://mc-server.googlecode.com/svn/trunk@990 0a769ca7-a7f5-676a-18bf-c427514a06d6

--- source/Piston.h
+++ source/Piston.h
@@ -18,29 +18,40 @@ public:
 
 	cPiston( cWorld* a_World );
 
-	static char RotationPitchToMetaData( float a_Rotation, float a_Pitch )
+	static NIBBLETYPE RotationPitchToMetaData(float a_Rotation, float a_Pitch)
 	{
-		LOGD("pre:a_Rotation %f \n",a_Rotation);
-		LOGD("a_Pitch %f \n",a_Pitch);
-
-		if (a_Pitch >= 50.f ){
+		if (a_Pitch >= 50.f)
+		{
 			return 0x1;
-		} else if ( a_Pitch <= -50.f ) {
+		}
+		else if (a_Pitch <= -50.f)
+		{
 			return 0x0;
-		} else {
+		}
+		else
+		{
+			a_Rotation += 90 + 45;  // So its not aligned with axis
 
-			a_Rotation += 90 + 45; // So its not aligned with axis
-			std::printf("a_Rotation %f \n",a_Rotation);
-
-			if( a_Rotation > 360.f ) a_Rotation -= 360.f;
-			if( a_Rotation >= 0.f && a_Rotation < 90.f )
-			{ LOG("1111\n");return 0x4;}
-			else if( a_Rotation >= 180 && a_Rotation < 270 )
-			{ LOG("2222\n");return 0x5;}
-			else if( a_Rotation >= 90 && a_Rotation < 180 )
-			{ LOG("3333\n");return 0x2;}
+			if (a_Rotation > 360.f)
+			{
+				a_Rotation -= 360.f;
+			}
+			if ((a_Rotation >= 0.f) && (a_Rotation < 90.f))
+			{
+				return 0x4;
+			}
+			else if ((a_Rotation >= 180) && (a_Rotation < 270))
+			{
+				return 0x5;
+			}
+			else if ((a_Rotation >= 90) && (a_Rotation < 180))
+			{
+				return 0x2;
+			}
 			else
-			{ LOG("4444\n");return 0x3;}
+			{
+				return 0x3;
+			}
 		}
 	}
 
