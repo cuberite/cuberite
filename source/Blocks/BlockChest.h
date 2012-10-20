4afb8bceb78f85e63e951f945c9387fef56d3fb4
madmaxoft@gmail.com <madmaxoft@gmail.com@0a769ca7-a7f5-676a-18bf-c427514a06d6>
2012-10-20 19:25:24 +0000

    Chest does check if it can be placed at the coords specified (the client does the check itself, too, but this may be good for scripts)
    git-svn-id: http://mc-server.googlecode.com/svn/trunk@989 0a769ca7-a7f5-676a-18bf-c427514a06d6

--- source/Blocks/BlockChest.h
+++ source/Blocks/BlockChest.h
@@ -31,6 +31,74 @@ public:
 	{
 		return "step.wood";
 	}
+	
+	
+	virtual bool CanBeAt(cWorld * a_World, int a_BlockX, int a_BlockY, int a_BlockZ)
+	{
+		cBlockArea Area;
+		if (!Area.Read(a_World, a_BlockX - 2, a_BlockX + 2, a_BlockY, a_BlockY, a_BlockZ - 2, a_BlockZ + 2))
+		{
+			// Cannot read the surroundings, probably at the edge of loaded chunks. Disallow.
+			return false;
+		}
+		
+		int NumChestNeighbors = 0;
+		if (Area.GetRelBlockType(1, 0, 2) == E_BLOCK_CHEST)
+		{
+			if (
+				(Area.GetRelBlockType(0, 0, 2) == E_BLOCK_CHEST) ||
+				(Area.GetRelBlockType(1, 0, 1) == E_BLOCK_CHEST) ||
+				(Area.GetRelBlockType(1, 0, 3) == E_BLOCK_CHEST)
+			)
+			{
+				// Already a doublechest neighbor, disallow:
+				return false;
+			}
+			NumChestNeighbors += 1;
+		}
+		if (Area.GetRelBlockType(3, 0, 2) == E_BLOCK_CHEST)
+		{
+			if (
+				(Area.GetRelBlockType(4, 0, 2) == E_BLOCK_CHEST) ||
+				(Area.GetRelBlockType(3, 0, 1) == E_BLOCK_CHEST) ||
+				(Area.GetRelBlockType(3, 0, 3) == E_BLOCK_CHEST)
+			)
+			{
+				// Already a doublechest neighbor, disallow:
+				return false;
+			}
+			NumChestNeighbors += 1;
+		}
+		if (Area.GetRelBlockType(2, 0, 1) == E_BLOCK_CHEST)
+		{
+			if (
+				(Area.GetRelBlockType(2, 0, 0) == E_BLOCK_CHEST) ||
+				(Area.GetRelBlockType(1, 0, 1) == E_BLOCK_CHEST) ||
+				(Area.GetRelBlockType(3, 0, 1) == E_BLOCK_CHEST)
+			)
+			{
+				// Already a doublechest neighbor, disallow:
+				return false;
+			}
+			NumChestNeighbors += 1;
+		}
+		if (Area.GetRelBlockType(2, 0, 3) == E_BLOCK_CHEST)
+		{
+			if (
+				(Area.GetRelBlockType(2, 0, 4) == E_BLOCK_CHEST) ||
+				(Area.GetRelBlockType(1, 0, 3) == E_BLOCK_CHEST) ||
+				(Area.GetRelBlockType(3, 0, 3) == E_BLOCK_CHEST)
+			)
+			{
+				// Already a doublechest neighbor, disallow:
+				return false;
+			}
+			NumChestNeighbors += 1;
+		}
+		return (NumChestNeighbors < 2);
+	}
+	
+	
 } ;
 
 
