793ae5359de3680ea0b58fa37f07c81d2605ab12
madmaxoft@gmail.com <madmaxoft@gmail.com@0a769ca7-a7f5-676a-18bf-c427514a06d6>
2012-10-22 18:00:05 +0000

    Vine decay (patch contributed by funmaker)
    git-svn-id: http://mc-server.googlecode.com/svn/trunk@1002 0a769ca7-a7f5-676a-18bf-c427514a06d6

--- source/Blocks/BlockVine.h
+++ source/Blocks/BlockVine.h
@@ -16,18 +16,49 @@ public:
 		: cBlockHandler(a_BlockType)
 	{
 	}
-
-
-	virtual bool DoesIgnoreBuildCollision(void) override
-	{
-		return true;
-	}
-	
 	
 	virtual void PlaceBlock(cWorld * a_World, cPlayer * a_Player, NIBBLETYPE a_BlockMeta, int a_BlockX, int a_BlockY, int a_BlockZ, char a_Dir) override
 	{
 		a_World->SetBlock(a_BlockX, a_BlockY, a_BlockZ, m_BlockType, cVine::DirectionToMetaData(a_Dir));
 		OnPlacedByPlayer(a_World, a_Player, a_BlockX, a_BlockY, a_BlockZ, a_Dir);
+	}
+
+
+	virtual bool CanBePlacedAt(cWorld * a_World, int a_BlockX, int a_BlockY, int a_BlockZ, char a_Dir) override
+	{
+		if (
+			(a_World->GetBlock( a_BlockX, a_BlockY+1, a_BlockZ ) == E_BLOCK_VINES) &&
+			(cVine::MetaDataToDirection(a_World->GetBlockMeta( a_BlockX, a_BlockY+1, a_BlockZ )) == a_Dir)
+		)
+		{
+			return true;
+		}
+		
+		if ((a_World->GetBlock( a_BlockX, a_BlockY+1, a_BlockZ ) != E_BLOCK_AIR))
+		{
+			AddDirection( a_BlockX, a_BlockY, a_BlockZ, a_Dir, true );
+			if(a_World->GetBlock( a_BlockX, a_BlockY, a_BlockZ ) == E_BLOCK_AIR){
+				AddDirection( a_BlockX, a_BlockY, a_BlockZ, a_Dir, false );
+				a_World->FastSetBlock( a_BlockX, a_BlockY, a_BlockZ, E_BLOCK_VINES, 0);
+			}
+			return true;
+		}
+
+		AddDirection( a_BlockX, a_BlockY, a_BlockZ, a_Dir, true );
+
+		return a_World->GetBlock( a_BlockX, a_BlockY, a_BlockZ ) != E_BLOCK_AIR;
+	}
+
+
+	virtual bool CanBeAt(cWorld * a_World, int a_BlockX, int a_BlockY, int a_BlockZ) override
+	{
+		char Dir = cVine::MetaDataToDirection(a_World->GetBlockMeta( a_BlockX, a_BlockY, a_BlockZ));
+		return CanBePlacedAt(a_World, a_BlockX, a_BlockY, a_BlockZ, Dir);
+	}
+
+	virtual bool DoesIgnoreBuildCollision(void) override
+	{
+		return true;
 	}
 	
 	
@@ -40,6 +71,12 @@ public:
 	virtual const char * GetStepSound(void) override
 	{
 		return "step.grass";
+	}
+
+
+	bool DoesDropOnUnsuitable(void)
+	{
+		return false;
 	}
 } ;
 
--- source/Vine.h
+++ source/Vine.h
@@ -4,7 +4,7 @@ class cVine												//tolua_export
 {														//tolua_export
 public:
 	
-	static char DirectionToMetaData( char a_Direction )	//tolua_export
+	static NIBBLETYPE DirectionToMetaData( char a_Direction )	//tolua_export
 	{													//tolua_export
 		switch (a_Direction)
 		{
@@ -17,7 +17,25 @@ public:
 			case 0x5:
 				return 0x2;
 			default:
-				return 0xf;
+				return 0x0;
 		};
 	}													//tolua_export
+
+	static char MetaDataToDirection(NIBBLETYPE a_MetaData )		//tolua_export
+	{														//tolua_export
+		switch(a_MetaData)
+		{
+			case 0x1:
+				return 0x2;
+			case 0x4:
+				return 0x3;
+			case 0x8:
+				return 0x4;
+			case 0x2:
+				return 0x5;
+			default:
+				return 0x1;
+		};
+	}														//tolua_export
+
 };														//tolua_export
