fd4f5b0ce552b5eea087f29542c120e4a0b33ec9
nielsbreu@gmail.com <nielsbreu@gmail.com@0a769ca7-a7f5-676a-18bf-c427514a06d6>
2013-04-20 08:49:41 +0000

    Simplified onkilling.lua and added /time set and /time add
    git-svn-id: http://mc-server.googlecode.com/svn/trunk@1398 0a769ca7-a7f5-676a-18bf-c427514a06d6

--- MCServer/Plugins/Core/onkilling.lua
+++ MCServer/Plugins/Core/onkilling.lua
@@ -1,5 +1,5 @@
 function OnKilling(Victim, Killer)
-	if Victim:IsPlayer() == true then
+	if Victim:IsPlayer() then
 		Server = cRoot:Get():GetServer()
 		if Killer == nil then
 			if Victim:IsOnFire() then
@@ -13,67 +13,44 @@ function OnKilling(Victim, Killer)
 				return false
 			end
 		else
-			if Killer:IsPlayer() == true then
+			if Killer:IsPlayer() then
 				Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by " .. Killer:GetName() )
 				CheckHardcore(Victim)
 				return false
-			elseif Killer:IsMob() == true then
+			elseif Killer:IsMob() then
 				if Killer:IsA("cZombie") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is eaten by a zombie")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cSkeleton") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a skeleton" )
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cCreeper") then
-					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a creeper")
-					CheckHardcore(Victim)
-					return false
+					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is blown up by a creeper")
 				elseif Killer:IsA("cSpider") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a spider")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cCaveSpider") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a cavespider")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cBlaze") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a blaze")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cEnderman") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is slain by a enderman")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cSilverfish") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a silverfish")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cSlime") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a slime")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cWitch") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a witch")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cZombiepigman") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is slain by a zombiepigman")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cMagmacube") then
 					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a magmacube")
-					CheckHardcore(Victim)
-					return false
 				elseif Killer:IsA("cWolf") then
-					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a wolf")
-					CheckHardcore(Victim)
-					return false
+					Server:SendMessage( cChatColor.Red .. "Player " .. Victim:GetName() .. " is killed by a wolf")					
 				end
+				CheckHardcore(Victim)
+				return false
 			end
 		end
 		Server:SendMessage( cChatColor.Red .. Victim:GetName() .. " Died")
+		CheckHardcore(Victim)
 	end
 end
 
--- MCServer/Plugins/Core/time.lua
+++ MCServer/Plugins/Core/time.lua
@@ -1,9 +1,8 @@
 function HandleTimeCommand( Split, Player )
-	if( #Split ~= 2 ) then
-		Player:SendMessage( cChatColor.Green .. "Usage: /time [Day/Night]" )
+	if( #Split ~= 3 ) then
+		Player:SendMessage( cChatColor.Green .. "Usage: /time [Day/Night/Set/Add]" )
 		return true;
 	end
-
 	local Server = cRoot:Get():GetServer()
 	if( string.upper( Split[2] ) == "DAY") then
 		Player:GetWorld():SetTimeOfDay( 0 )
@@ -11,8 +10,15 @@ function HandleTimeCommand( Split, Player )
 	elseif( string.upper( Split[2] ) == "NIGHT") then
 		Player:GetWorld():SetTimeOfDay( 12000 + 1000 )
 		Server:SendMessage( cChatColor.Green .. Player:GetName() .. " set the time to Night.")
+	elseif( string.upper(Split[2]) == "SET" ) and ( tonumber(Split[3]) ~= nil) then
+		Player:GetWorld():SetTimeOfDay( tonumber(Split[3]) )
+		Server:SendMessage( cChatColor.Green .. Player:GetName() .. " set the time to " .. Split[3] )
+	elseif( string.upper(Split[2]) == "ADD" ) and ( tonumber(Split[3]) ~= nil) then
+		Player:GetWorld():SetTimeOfDay( Player:GetWorld():GetTimeOfDay() + Split[3] )
+		Server:SendMessage( cChatColor.Green .. Player:GetName() .. " Added " .. Split[3] .. " to the time" )
 	else
-		Player:SendMessage( cChatColor.Green .. "Usage: /time [Day/Night]" )
+		Player:SendMessage( cChatColor.Green .. "Usage: /time [Day/Night/Set/Add]" )
 	end
+	
 	return true
 end
