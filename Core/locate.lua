66e837ecbc08ece3150a364976db87f56813d30b
Alexander Harkness <bearbin@gmail.com>
2013-07-27 16:22:27 +0100

    Removed extraneous core.

--- Core/locate.lua
+++
@@ -1,4 +0,0 @@
-function HandleLocateCommand( Split, Player )
-	Player:SendMessage(cChatColor.Green .. string.format("[X:%0.2f] [Y:%0.2f] [Z:%0.2f]", Player:GetPosX(), Player:GetPosY(), Player:GetPosZ() ) )
-	return true
-end
