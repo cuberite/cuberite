13c5a7864eca5fa47e7608fc4e44558c8bf9e7f8
madmaxoft <github@xoft.cz>
2013-08-02 09:25:12 +0200

    AllToLua now resets the git status for the files.
    This should auto-fix merge conflicts when they occur on the Bindings.cpp / Bindings.h files.
    http://forum.mc-server.org/showthread.php?tid=1200&pid=9315#pid9315

--- source/AllToLua.bat
+++ source/AllToLua.bat
@@ -1,3 +1,25 @@
+
+:: AllToLua.bat
+
+:: This scripts updates the automatically-generates Lua bindings in Bindings.cpp / Bindings.h
+
+
+
+
+
+:: If there was a Git conflict, resolve it by resetting to HEAD; we're regenerating the files from scratch anyway
+git checkout -- Bindings.cpp
+git checkout -- Bindings.h
+
+
+
+
+
+:: Regenerate the files:
 "tolua++.exe" -L virtual_method_hooks.lua -o Bindings.cpp -H Bindings.h AllToLua.pkg
+
+
+
+
 
 if %ALLTOLUA_WAIT%N == N pause
