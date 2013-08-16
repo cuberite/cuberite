98d574f05ea46763fc9e762b0719ab3ef2271230
madmaxoft <github@xoft.cz>
2013-08-16 09:20:05 +0200

    No more Core in the MCServer repo

--- MCServer/Plugins/Core/locate.lua
+++
@@ -1,4 +0,0 @@
-function HandleLocateCommand( Split, Player )
-	SendMessage( Player, string.format("You are at [X:%0.2f Y:%0.2f Z:%0.2f] in world %s", Player:GetPosX(), Player:GetPosY(), Player:GetPosZ(), Player:GetWorld():GetName()) )
-	return true
-end
