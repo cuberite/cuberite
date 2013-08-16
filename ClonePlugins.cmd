8326aff5b4c2b9e5ddc5b4c1c40fd791d7e8ef4b
madmaxoft <github@xoft.cz>
2013-08-16 09:30:27 +0200

    Added the ClonePlugins script.
    This will clone the base plugins into their respective folders.

---
+++ ClonePlugins.cmd
@@ -0,0 +1,6 @@
+
+:: ClonePlugins.cmd
+
+:: Clones the base plugins from their respective repos into proper folders (./MCServer/Plugins
+
+git clone https://github.com/mc-server/Core.git ./MCServer/Plugins/Core
