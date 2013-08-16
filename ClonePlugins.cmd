0074432ccd64791382f0629c1e863ec20a78949d
madmaxoft <github@xoft.cz>
2013-08-16 09:38:57 +0200

    Moved ProtectionAreas into a separate repository

--- ClonePlugins.cmd
+++ ClonePlugins.cmd
@@ -4,3 +4,4 @@
 :: Clones the base plugins from their respective repos into proper folders (./MCServer/Plugins
 
 git clone https://github.com/mc-server/Core.git ./MCServer/Plugins/Core
+git clone https://github.com/mc-server/ProtectionAreas.git ./MCServer/Plugins/ProtectionAreas
