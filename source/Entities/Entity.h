c565950e1fd336e08bda35c26cb9b8983681fd60
madmaxoft <github@xoft.cz>
2013-08-22 08:51:40 +0200

    Removed unwanted VirtualHooks remnants.
    cEntity no longer needs its SpawnOn() default-implemented, it can now be a true pure virtual function.

--- source/Entities/Entity.h
+++ source/Entities/Entity.h
@@ -276,9 +276,8 @@ public:
 
 	/** Descendants override this function to send a command to the specified client to spawn the entity on the client.
 	To spawn on all eligible clients, use cChunkMap::BroadcastSpawnEntity()
-	Needs to have a default implementation due to Lua bindings.
 	*/
-	virtual void SpawnOn(cClientHandle & a_Client) {ASSERT(!"SpawnOn() unimplemented!"); }
+	virtual void SpawnOn(cClientHandle & a_Client) = 0;
 
 	// tolua_begin
 	
