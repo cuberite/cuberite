e844612503e0d78723c76423e93165164505f7ac
mgueydan <gueydan.mathieu@gmail.com>
2013-09-07 20:51:31 +0200

    Adding a getType in Monster that return an enum instead of an int or char

--- source/Mobs/Monster.h
+++ source/Mobs/Monster.h
@@ -89,7 +89,8 @@ public:
 	virtual void MoveToPosition(const Vector3f & a_Position);
 	virtual bool ReachedDestination(void);
 	
-	char GetMobType(void) const {return m_MobType; }
+	char GetMobType(void) const {return m_MobType; } // MG TODO : see if we can delete this one.
+	eType GetMobTypeAsEnum(void) const {return (eType)m_MobType; }  // MG TODO : see if we should store m_MobType as enum instead of char.
 	virtual eFamily GetMobFamily(void) const = 0;
 
 	const char * GetState();
