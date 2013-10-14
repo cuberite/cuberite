436adc48ea76fc9131d0a87bc42c9c527b0f4c61
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2013-10-14 23:04:48 +0100

    Fixes to button and lever code
    * Enhanced sound-playing code
    * Enhanced comment grammar
    - Use m_BlockType instead of GetBlock because dupe prevention wasn't
    actually doing anything
    * Enhanced pickup pushback code

--- source/Blocks/BlockButton.cpp
+++ source/Blocks/BlockButton.cpp
@@ -17,21 +17,14 @@ cBlockButtonHandler::cBlockButtonHandler(BLOCKTYPE a_BlockType)
 
 void cBlockButtonHandler::OnUse(cWorld *a_World, cPlayer *a_Player, int a_BlockX, int a_BlockY, int a_BlockZ, char a_BlockFace, int a_CursorX, int a_CursorY, int a_CursorZ)
 {
-	// Flip the ON bit on/off. Using XOR bitwise operation to turn it on/off.
+	// Flip the ON bit on/off using the XOR bitwise operation
 	NIBBLETYPE Meta = ((a_World->GetBlockMeta(a_BlockX, a_BlockY, a_BlockZ) ^ 0x08) & 0x0f);
-	a_World->SetBlockMeta(a_BlockX, a_BlockY, a_BlockZ, Meta);
 
-	if (Meta & 0x08)
-	{
-		a_World->BroadcastSoundEffect("random.click", a_BlockX * 8, a_BlockY * 8, a_BlockZ * 8, 0.5f, 0.6f);
-	}
-	else
-	{
-		a_World->BroadcastSoundEffect("random.click", a_BlockX * 8, a_BlockY * 8, a_BlockZ * 8, 0.5f, 0.5f);
-	}
+	a_World->SetBlock(a_BlockX, a_BlockY, a_BlockZ, m_BlockType, Meta);
+	a_World->BroadcastSoundEffect("random.click", a_BlockX * 8, a_BlockY * 8, a_BlockZ * 8, 0.5f, (Meta & 0x08) ? 0.6f : 0.5f);
 
-	// Queue a button reset (unpress), with a GetBlock to prevent duplication of buttons (press, break, wait for reset)
-	a_World->QueueSetBlock(a_BlockX, a_BlockY, a_BlockZ, a_World->GetBlock(a_BlockX, a_BlockY, a_BlockZ), ((a_World->GetBlockMeta(a_BlockX, a_BlockY, a_BlockZ) ^ 0x08) & 0x0f), m_BlockType == E_BLOCK_STONE_BUTTON ? 20 : 25);
+	// Queue a button reset (unpress)
+	a_World->QueueSetBlock(a_BlockX, a_BlockY, a_BlockZ, m_BlockType, ((a_World->GetBlockMeta(a_BlockX, a_BlockY, a_BlockZ) ^ 0x08) & 0x0f), m_BlockType == E_BLOCK_STONE_BUTTON ? 40 : 50);
 }
 
 
--- source/Blocks/BlockButton.h
+++ source/Blocks/BlockButton.h
@@ -18,7 +18,7 @@ public:
 	virtual void ConvertToPickups(cItems & a_Pickups, NIBBLETYPE a_BlockMeta) override
 	{
 		// Reset meta to 0
-		a_Pickups.push_back(cItem(m_BlockType == E_BLOCK_WOODEN_BUTTON ? E_BLOCK_WOODEN_BUTTON : E_BLOCK_STONE_BUTTON, 1, 0));
+		a_Pickups.push_back(cItem(m_BlockType, 1, 0));
 	}
 
 
