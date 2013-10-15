167d8d346ec0fd0b150116619fa3cde760e3e369
madmaxoft <github@xoft.cz>
2013-10-15 17:55:01 +0200

    APIDump: Documented HOOK_SPAWNED_ENTITY.

--- MCServer/Plugins/APIDump/APIDesc.lua
+++ MCServer/Plugins/APIDump/APIDesc.lua
@@ -2923,6 +2923,26 @@ end;
 			]],
 		},  -- HOOK_PRE_CRAFTING
 
+		HOOK_SPAWNED_ENTITY =
+		{
+			CalledWhen = "After an entity is spawned in the world.",
+			DefaultFnName = "OnSpawnedEntity",  -- also used as pagename
+			Desc = [[
+				This callback is called after the server spawns an {{cEntity|entity}}. This is an information-only
+				callback, the entity is already spawned by the time it is called. If the entity spawned is a
+				{{cMonster|monster}}, the {{OnSpawnedMonster|HOOK_SPAWNED_MONSTER}} hook is called before this hook.
+			]],
+			Params =
+			{
+				{ Name = "World", Type = "{{cWorld}}", Notes = "The world in which the entity has spawned" },
+				{ Name = "Entity", Type = "{{cEntity}} descentant", Notes = "The entity that has spawned" },
+			},
+			Returns = [[
+				If the function returns false or no value, the next plugin's callback is called. If the function
+				returns true, no other callback is called for this event.
+			]],
+		},  -- HOOK_SPAWNED_ENTITY
+
 	},  -- Hooks[]
 	
 
