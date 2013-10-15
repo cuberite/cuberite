fbba2e79eb0ab60e5515a00944313486131c1a35
tonibm19 <tonibm19@gmail.com>
2013-10-15 17:31:26 +0200

    Added basic milk code.

--- source/Mobs/Cow.cpp
+++ source/Mobs/Cow.cpp
@@ -1,13 +1,8 @@
-
 #include "Globals.h"  // NOTE: MSVC stupidness requires this to be the same across all modules
 
 #include "Cow.h"
 
 
-
-
-
-// TODO: Milk Cow
 
 
 
@@ -28,6 +23,18 @@ void cCow::GetDrops(cItems & a_Drops, cEntity * a_Killer)
 	AddRandomDropItem(a_Drops, 1, 3, IsOnFire() ? E_ITEM_STEAK : E_ITEM_RAW_BEEF);
 }
 
+void cCow::OnRightClicked(cPlayer & a_Player)
+{
+	if ((a_Player.GetEquippedItem().m_ItemType == E_ITEM_BUCKET))
+	{
+		if (!a_Player.IsGameModeCreative())
+		{
+			a_Player.GetInventory().RemoveOneEquippedItem();
+			a_Player.GetInventory().AddItem(E_ITEM_MILK)
+		}
+
+	}
+}
 
 
 
