1a29c19619a70cd78ce7a2e18881ebdde83650ac
madmaxoft <github@xoft.cz>
2013-10-28 13:08:14 +0100

    APIDump: Documented HTTPFormData and HTTPRequest.

--- MCServer/Plugins/APIDump/APIDesc.lua
+++ MCServer/Plugins/APIDump/APIDesc.lua
@@ -2348,7 +2348,40 @@ World:ForEachEntity(
 					]],
 				},
 			},  -- AdditionalInfo
-		},
+		},  -- cWorld
+
+		HTTPFormData =
+		{
+			Desc = "This class stores data for one form element for a {{HTTPRequest|HTTP request}}.",
+			Variables =
+			{
+				Name = { Type = "string", Notes = "Name of the form element" },
+				Type = { Type = "string", Notes = "Type of the data (usually empty)" },
+				Value = { Type = "string", Notes = "Value of the form element. Contains the raw data as sent by the browser." },
+			},
+		},  -- HTTPFormData
+
+		HTTPRequest =
+		{
+			Desc = [[
+				This class encapsulates all the data that is sent to the WebAdmin through one HTTP request. Plugins
+				receive this class as a parameter to the function handling the web requests, as registered in the
+				FIXME: {{cPluginLua}}:AddWebPage().
+			]],
+			Constants =
+			{
+				FormData = { Notes = "Array-table of {{HTTPFormData}}, contains the values of individual form elements submitted by the client" },
+				Params = { Notes = "Map-table of parameters given to the request in the URL (?param=value); if a form uses GET method, this is the same as FormData. For each parameter given as \"param=value\", there is an entry in the table with \"param\" as its key and \"value\" as its value." },
+				PostParams = { Notes = "Map-table of data posted through a FORM - either a GET or POST method. Logically the same as FormData, but in a map-table format (for each parameter given as \"param=value\", there is an entry in the table with \"param\" as its key and \"value\" as its value)." },
+			},
+
+			Variables =
+			{
+				Method = { Type = "string", Notes = "The HTTP method used to make the request. Usually GET or POST." },
+				Path = { Type = "string", Notes = "The Path part of the URL (excluding the parameters)" },
+				Username = { Type = "string", Notes = "Name of the logged-in user." },
+			},
+		},  -- HTTPRequest
 
 		TakeDamageInfo =
 		{
@@ -2360,7 +2393,7 @@ World:ForEachEntity(
 			Constants =
 			{
 			},
-		},
+		},  -- TakeDamageInfo
 
 		Vector3d =
 		{
@@ -2373,7 +2406,7 @@ World:ForEachEntity(
 			Constants =
 			{
 			},
-		},
+		},  -- Vector3d
 
 		Vector3f =
 		{
@@ -2385,7 +2418,7 @@ World:ForEachEntity(
 			Constants =
 			{
 			},
-		},
+		},  -- Vector3f
 
 		Vector3i =
 		{
@@ -2397,7 +2430,8 @@ World:ForEachEntity(
 			Constants =
 			{
 			},
-		},
+		},  -- Vector3i
+		
 		Globals =
 		{
 			Desc = [[These functions are available directly, without a class instance. Any plugin cal call them at any time.]],
