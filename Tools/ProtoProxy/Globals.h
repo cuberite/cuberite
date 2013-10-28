fd85ac23c1160910042c0887ea7e4e3460d1cb71
madmaxoft <github@xoft.cz>
2013-10-28 21:12:49 +0100

    ProtoProxy: Fixed compilation after the previous cByteBuffer changes.

--- Tools/ProtoProxy/Globals.h
+++ Tools/ProtoProxy/Globals.h
@@ -70,6 +70,10 @@ typedef long long Int64;
 typedef int       Int32;
 typedef short     Int16;
 
+typedef unsigned long long UInt64;
+typedef unsigned int       UInt32;
+typedef unsigned short     UInt16;
+
 
 
 
