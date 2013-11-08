20d9886847ead593b66bbef779bb4ddda3a3abdd
tonibm19 <tonibm19@gmail.com>
2013-11-08 14:04:41 +0100

    Now chicken drop eggs

--- source/Mobs/Chicken.h
+++ source/Mobs/Chicken.h
@@ -18,8 +18,13 @@ public:
 	CLASS_PROTODEF(cChicken);
 
 	virtual void GetDrops(cItems & a_Drops, cEntity * a_Killer = NULL) override;
-} ;
+	virtual void Tick(float a_Dt, cChunk & a_Chunk) override;
 
+private:
+
+
+	int m_DropEggCount;
+} ;
 
 
 
