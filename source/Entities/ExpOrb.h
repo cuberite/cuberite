6641db058392dd6d7e170b95cd8616b920add63c
STRWarrior <niels.breuker@hotmail.nl>
2013-11-25 20:05:52 +0100

    Implented ExpOrb class.

---
+++ source/Entities/ExpOrb.h
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include "Entity.h"
+
+
+
+
+
+class cExpOrb :
+	public cEntity
+{
+	typedef cExpOrb super;
+	
+public:
+	
+	cExpOrb(double a_X, double a_Y, double a_Z, int a_Reward);
+	cExpOrb(const Vector3d & a_Pos, int a_Reward);
+
+	// Override functions
+	virtual void Tick(float a_Dt, cChunk & a_Chunk) override;
+	virtual void SpawnOn(cClientHandle & a_Client) override;
+
+	// cExpOrb functions
+	short int GetReward(void) const { return m_Reward; }
+
+protected:
+	int m_Reward;
+} ;
