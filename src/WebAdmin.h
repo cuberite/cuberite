f792117ce58c11d110bd3399d1a1e4c90ba61c6f
Alexander Harkness <bearbin@gmail.com>
2013-11-26 17:04:07 +0000

    Fixed a mis-include

--- src/WebAdmin.h
+++ src/WebAdmin.h
@@ -7,7 +7,7 @@
 
 #include "OSSupport/Socket.h"
 #include "LuaState.h"
-#include "../lib/iniFile/iniFile.h"
+#include "lib/inifile/iniFile.h"
 #include "HTTPServer/HTTPServer.h"
 #include "HTTPServer/HTTPFormParser.h"
 
