6c7de446ae2a691296a25dc74022e09761ef8197
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2013-12-06 20:00:49 +0000

    Spawn eggs use IsGameMode()
    Possible fix for #316.

--- src/Items/ItemSpawnEgg.h
+++ src/Items/ItemSpawnEgg.h
@@ -28,14 +28,14 @@ public:
 		
 		AddFaceDirection(a_BlockX, a_BlockY, a_BlockZ, a_BlockFace);
 
-		if (a_BlockFace == BLOCK_FACE_BOTTOM)
+		if (a_BlockFace == BLOCK_FACE_YM)
 		{
 			a_BlockY--;
 		}
 
 		if (a_World->SpawnMob(a_BlockX + 0.5, a_BlockY, a_BlockZ + 0.5, (cMonster::eType)(a_Item.m_ItemDamage)) >= 0)
 		{
-			if (a_Player->GetGameMode() != 1)
+			if (!a_Player->IsGameModeCreative())
 			{
 				// The mob was spawned, "use" the item:
 				a_Player->GetInventory().RemoveOneEquippedItem();
