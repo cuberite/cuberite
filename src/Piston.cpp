16db8533e1a9cde333d64dc5a8c8722aa15556cb
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2013-12-07 20:29:09 +0000

    Improved piston animations

--- src/Piston.cpp
+++ src/Piston.cpp
@@ -15,7 +15,7 @@
 
 
 /// Number of ticks that the piston extending / retracting waits before setting the block
-const int PISTON_TICK_DELAY = 6;
+const int PISTON_TICK_DELAY = 1;
 
 
 
@@ -142,16 +142,16 @@ void cPiston::RetractPiston(int pistx, int pisty, int pistz)
 	}
 	
 	AddDir(pistx, pisty, pistz, pistonMeta, -1);
-	m_World->BroadcastBlockAction(pistx, pisty, pistz, 1, pistonMeta & ~(8), pistonBlock);
-	m_World->BroadcastSoundEffect("tile.piston.in", pistx * 8, pisty * 8, pistz * 8, 0.5f, 0.7f);			
 	m_World->SetBlock(pistx, pisty, pistz, pistonBlock, pistonMeta & ~(8));
+	m_World->BroadcastBlockAction(pistx, pisty, pistz, 1, pistonMeta & ~(8), pistonBlock);
+	m_World->BroadcastSoundEffect("tile.piston.in", pistx * 8, pisty * 8, pistz * 8, 0.5f, 0.7f);
 	AddDir(pistx, pisty, pistz, pistonMeta, 1);
 	
 	// Retract the extension, pull block if appropriate
 	if (IsSticky(pistonBlock))
 	{
 		int tempx = pistx, tempy = pisty, tempz = pistz;
-		AddDir( tempx, tempy, tempz, pistonMeta, 1);
+		AddDir(tempx, tempy, tempz, pistonMeta, 1);
 		BLOCKTYPE tempBlock;
 		NIBBLETYPE tempMeta;
 		m_World->GetBlockTypeMeta(tempx, tempy, tempz, tempBlock, tempMeta);
