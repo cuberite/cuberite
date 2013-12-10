6cdc990884b03395cfddf5afe699a76aa491e7e3
Tycho Bickerstaff <work.tycho@gmail.com>
2013-12-10 21:39:20 +0000

    more cmake

---
+++ src/MCServer/MCServer.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/../MCServer/MCServer.dir/main.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/Root.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/MCLogger.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/Authenticator.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/StringUtils.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/Server.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/DeadlockDetect.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/WebAdmin.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/GroupManager.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/CraftingRecipes.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/FurnaceRecipe.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/MonsterConfig.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/World.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/CommandOutput.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/RCONServer.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/Log.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/ClientHandle.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/ChatColor.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/Group.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/Enchantments.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/ChunkMap.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/ChunkSender.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/LightingThread.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/MobCensus.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/MobSpawner.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/MobProximityCounter.cpp.o"
+  "CMakeFiles/../MCServer/MCServer.dir/Noise.cpp.o"
+  "../MCServer/MCServer.pdb"
+  "../MCServer/MCServer"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/../MCServer/MCServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
