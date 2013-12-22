7a299f1ba62217c6d214f6cfbd266a41938ad577
STRWarrior <niels.breuker@hotmail.nl>
2013-12-22 14:48:22 +0100

    Fishing now uses a countdown instead of a random number each tick.

--- src/Entities/Floater.cpp
+++ src/Entities/Floater.cpp
@@ -9,11 +9,12 @@
 
 
 
-cFloater::cFloater(double a_X, double a_Y, double a_Z, Vector3d a_Speed, int a_PlayerID) :
+cFloater::cFloater(double a_X, double a_Y, double a_Z, Vector3d a_Speed, int a_PlayerID, int a_CountDownTime) :
 	cEntity(etFloater, a_X, a_Y, a_Z, 0.98, 0.98),
 	m_PickupCountDown(0),
 	m_PlayerID(a_PlayerID),
-	m_CanPickupItem(false)
+	m_CanPickupItem(false),
+	m_CountDownTime(a_CountDownTime)
 {
 	SetSpeed(a_Speed);
 }
@@ -36,17 +37,31 @@ void cFloater::Tick(float a_Dt, cChunk & a_Chunk)
 	HandlePhysics(a_Dt, a_Chunk);
 	if (IsBlockWater(m_World->GetBlock((int) GetPosX(), (int) GetPosY(), (int) GetPosZ())) && m_World->GetBlockMeta((int) GetPosX(), (int) GetPosY(), (int) GetPosZ()) == 0)
 	{
-		if ((!m_CanPickupItem) && (m_World->GetTickRandomNumber(100) == 0))
+		if (!m_CanPickupItem)
 		{
-			SetPosY(GetPosY() - 1);
-			m_CanPickupItem = true;
-			m_PickupCountDown = 20;
-			LOGD("Floater %i can be picked up", GetUniqueID());
+			if (m_CountDownTime <= 0)
+			{
+				m_World->BroadcastSoundEffect("random.splash", (int) floor(GetPosX() * 8), (int) floor(GetPosY() * 8), (int) floor(GetPosZ() * 8), 1, 1);
+				SetPosY(GetPosY() - 1);
+				m_CanPickupItem = true;
+				m_PickupCountDown = 20;
+				m_CountDownTime = 100 + m_World->GetTickRandomNumber(800);
+				LOGD("Floater %i can be picked up", GetUniqueID());
+			}
+			else if (m_CountDownTime == 20) // Calculate the position where the particles should spawn and start producing them.
+			{
+				LOGD("Started producing particles for floater %i", GetUniqueID());
+				m_ParticlePos.Set(GetPosX() + (-4 + m_World->GetTickRandomNumber(8)), GetPosY(), GetPosZ() + (-4 + m_World->GetTickRandomNumber(8)));
+				m_World->BroadcastParticleEffect("splash", m_ParticlePos.x, m_ParticlePos.y, m_ParticlePos.z, 0, 0, 0, 0, 15);
+			}
+			else if (m_CountDownTime < 20)
+			{
+				m_ParticlePos = (m_ParticlePos + (GetPosition() - m_ParticlePos) / 6);
+				m_World->BroadcastParticleEffect("splash", m_ParticlePos.x, m_ParticlePos.y, m_ParticlePos.z, 0, 0, 0, 0, 15);
+			}
+			m_CountDownTime--;
 		}
-		else
-		{
-			SetSpeedY(0.7);
-		}
+		SetSpeedY(0.7);
 	}
 	SetSpeedX(GetSpeedX() * 0.95);
 	SetSpeedZ(GetSpeedZ() * 0.95);
--- src/Entities/Floater.h
+++ src/Entities/Floater.h
@@ -14,7 +14,7 @@ class cFloater :
 	
 public:
 	
-	cFloater(double a_X, double a_Y, double a_Z, Vector3d a_Speed, int a_PlayerID);
+	cFloater(double a_X, double a_Y, double a_Z, Vector3d a_Speed, int a_PlayerID, int a_CountDownTime);
 
 	virtual void SpawnOn(cClientHandle & a_Client) override;
 	virtual void Tick(float a_Dt, cChunk & a_Chunk) override;
@@ -23,7 +23,9 @@ public:
 
 protected:
 	Vector3d m_Speed;
+	Vector3d m_ParticlePos;
 	int m_PickupCountDown;
 	int m_PlayerID;
+	int m_CountDownTime;
 	bool m_CanPickupItem;
 } ;
