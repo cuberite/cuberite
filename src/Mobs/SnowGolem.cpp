5a9b26d3226c86bef212b080ed2ffcd2129567ba
STRWarrior <niels.breuker@hotmail.nl>
2013-12-22 20:40:07 +0100

    Snow golems die in hot biomes and leave a snow trail.

--- src/Mobs/SnowGolem.cpp
+++ src/Mobs/SnowGolem.cpp
@@ -2,6 +2,7 @@
 #include "Globals.h"  // NOTE: MSVC stupidness requires this to be the same across all modules
 
 #include "SnowGolem.h"
+#include "../World.h"
 
 
 
@@ -24,3 +25,19 @@ void cSnowGolem::GetDrops(cItems & a_Drops, cEntity * a_Killer)
 
 
 
+
+void cSnowGolem::Tick(float a_Dt, cChunk & a_Chunk)
+{
+	super::Tick(a_Dt, a_Chunk);
+	if (IsBiomeNoDownfall((EMCSBiome) m_World->GetBiomeAt((int) floor(GetPosX()), (int) floor(GetPosZ())) ))
+	{
+		TakeDamage(*this);
+	}
+	else
+	{
+		if (g_BlockIsSolid[m_World->GetBlock((int) floor(GetPosX()), (int) floor(GetPosY()) - 1, (int) floor(GetPosZ()))])
+		{
+			m_World->SetBlock((int) floor(GetPosX()), (int) floor(GetPosY()), (int) floor(GetPosZ()), E_BLOCK_SNOW, 0);
+		}
+	}
+}
