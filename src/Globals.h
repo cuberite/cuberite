0d5b581fcd07c0bb61373abd6b0404215c0b2301
Diusrex <killakan002@hotmail.com>
2014-01-05 14:42:22 -0700

    Making all of the useful level 4 warnings be active.

--- src/Globals.h
+++ src/Globals.h
@@ -14,7 +14,19 @@
 	#pragma warning(disable:4481)
 
 	// Disable some warnings that we don't care about:
-	#pragma warning(disable:4100)
+	#pragma warning(disable:4100) // Unreferenced formal parameter
+    
+    // Useful warnings from warning level 4:
+	#pragma warning(3 : 4189) // Local variable is initialized but not referenced
+	#pragma warning(3 : 4702) // Unreachable code
+	#pragma warning(3 : 4245) // Conversion from 'type1' to 'type2', signed/unsigned mismatch
+	#pragma warning(3 : 4389) // Signed/unsigned mismatch
+	#pragma warning(3 : 4701) // Potentially unitialized local variable used
+	#pragma warning(3 : 4244) // Conversion from 'type1' to 'type2', possible loss of data
+	#pragma warning(3 : 4310) // Cast truncates constant value
+	#pragma warning(3 : 4505) // Unreferenced local function has been removed
+	#pragma warning(3 : 4127) // Conditional expression is constant
+	#pragma warning(3 : 4706) // Assignment within conditional expression
 
 	#define OBSOLETE __declspec(deprecated)
 
