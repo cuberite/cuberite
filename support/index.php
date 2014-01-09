8757a508f78113388072ee230bbe9e204d2fe10d
Tycho <work.tycho+git@gmail.com>
2014-01-09 10:06:27 -0800

    added javascript redirects with querystrings

--- support/index.php
+++ support/index.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta http-equiv="refresh" content="0;url=http://wiki.MCServer.com/index.php" />
-    <link rel="canonical" href="http://wiki.MCServer.com/index.php"/>
-  </head>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<noscript>
+			<meta http-equiv="refresh" content="0;url=http://wiki.MCServer.com/index.php" />
+		</noscript>
+		<link rel="canonical" href="http://wiki.MCServer.com/index.php"/>
+		<script>
+			window.location = "http://wiki.MCServer.com/index.php" + location.search;
+		</script>
+	</head>
 </html>
--- wiki/doku.php
+++ wiki/doku.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta http-equiv="refresh" content="0;url=http://support.MCServer.com/doku.php" />
-    <link rel="canonical" href="http://wiki.MCServer.com/index.php"/
-  </head>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<noscript>
+			<meta http-equiv="refresh" content="0;url=http://support.MCServer.com/doku.php" />
+		</noscript>
+		<link rel="canonical" href="http://support.MCServer.com/doku.php"/>
+		<script>
+			window.location = "http://support.MCServer.com/doku.php" + location.search;
+		</script>
+	</head>
 </html>
