257ba4c56dd4bd68b3d60248f9267557dbec9c7d
Tycho <work.tycho+git@gmail.com>
2014-01-09 10:00:31 -0800

    Added link rel=canonical

--- support/index.php
+++ support/index.php
@@ -3,5 +3,6 @@
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta http-equiv="refresh" content="0;url=http://wiki.MCServer.com/index.php" />
+    <link rel="canonical" href="http://wiki.MCServer.com/index.php"/>
   </head>
 </html>
