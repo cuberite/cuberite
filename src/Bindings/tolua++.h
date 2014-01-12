328b2db252fe42774cc001bf1a62d32fe7317c72
madmaxoft <github@xoft.cz>
2014-01-12 08:43:13 +0100

    Disabled a useless MSVC warning in Bindings.cpp.

--- src/Bindings/tolua++.h
+++ src/Bindings/tolua++.h
@@ -2,11 +2,17 @@
 // tolua++.h
 
 // Redirection file, needed because ToLua++ generates the Bindings.cpp file with >> #include "tolua++.h" <<
+// Only used from Bindings.cpp
 
 
 
 
 #include "tolua++/include/tolua++.h"
+
+#ifdef _MSC_VER
+	// Disable specific warnings for the generated Bindings.cpp file:
+	#pragma warning(disable: 4800)  // 'int' : forcing value to bool 'true' or 'false' (performance warning)
+#endif  // _MSC_VER
 
 
 
