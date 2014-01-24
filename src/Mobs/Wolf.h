0583b9df391d3b7c8a7ff4f982c4d2e28c42fa36
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2014-01-24 20:46:47 +0000

    Made wolves compatible with new AI code

--- src/Mobs/Wolf.h
+++ src/Mobs/Wolf.h
@@ -22,6 +22,7 @@ public:
 	virtual void OnRightClicked(cPlayer & a_Player) override;
 	virtual void Tick(float a_Dt, cChunk & a_Chunk) override;
 	virtual void TickFollowPlayer();
+	virtual void Attack(float a_Dt) override;
 
 	// Get functions
 	bool    IsSitting     (void) const { return m_IsSitting; }
