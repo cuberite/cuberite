b2bacf3a25001ed855319d6f2146a3d5e734f43c
STRWarrior <niels.breuker@hotmail.nl>
2014-01-28 15:40:13 +0100

    Villager: NoCountDown and Action function don't check VillagersShouldHarvestCrops anymore because it shoudn't even be activated anywhere.

--- src/Mobs/Villager.cpp
+++ src/Mobs/Villager.cpp
@@ -146,11 +146,6 @@ void cVillager::HandleFarmerAttemptSpecialAction()
 
 void cVillager::HandleFarmerAction()
 {
-	if (!m_World->VillagersShouldHarvestCrops())
-	{
-		return;
-	}
-
 	// Harvest the crops if the villager isn't moving and if the crops are closer then 2 blocks.
 	if (!m_bMovingToDestination && (GetPosition() - m_CropsPos).Length() < 2)
 	{
@@ -171,11 +166,6 @@ void cVillager::HandleFarmerAction()
 
 void cVillager::HandleFarmerEndCountDown()
 {
-	if (!m_World->VillagersShouldHarvestCrops())
-	{
-		return;
-	}
-
 	// Check if there is still farmland at the spot where the crops were.
 	if (m_World->GetBlock(m_CropsPos.x, m_CropsPos.y - 1, m_CropsPos.z) == E_BLOCK_FARMLAND)
 	{
