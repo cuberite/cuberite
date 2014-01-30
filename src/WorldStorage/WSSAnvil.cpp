1a361be44fe2a454734e3ac0e93211e33e566976
STRWarrior <niels.breuker@hotmail.nl>
2014-01-30 21:46:45 +0100

    Check if the tag is found.

--- src/WorldStorage/WSSAnvil.cpp
+++ src/WorldStorage/WSSAnvil.cpp
@@ -1877,6 +1877,11 @@ void cWSSAnvil::LoadWolfFromNBT(cEntityList & a_Entities, const cParsedNBT & a_N
 		return;
 	}
 	int OwnerIdx = a_NBT.FindChildByName(a_TagIdx, "Owner");
+	if (TypeIdx < 0) 
+	{ 
+		return;
+	}
+
 	AString OwnerName = a_NBT.GetString(OwnerIdx);
 	if (OwnerName != "")
 	{
