550a09020d4e40904a205ebe7daf1d2421e51070
STRWarrior <niels.breuker@hotmail.nl>
2014-01-30 21:49:39 +0100

    Fixed bad variable.

--- src/WorldStorage/WSSAnvil.cpp
+++ src/WorldStorage/WSSAnvil.cpp
@@ -1877,7 +1877,7 @@ void cWSSAnvil::LoadWolfFromNBT(cEntityList & a_Entities, const cParsedNBT & a_N
 		return;
 	}
 	int OwnerIdx = a_NBT.FindChildByName(a_TagIdx, "Owner");
-	if (TypeIdx < 0) 
+	if (OwnerIdx < 0) 
 	{ 
 		return;
 	}
