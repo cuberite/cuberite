10169220123b19f0ebcfd7330bb52adaf06f4ec0
Howaner <franzi.moos@googlemail.com>
2014-02-20 20:58:23 +0100

    Fix Cauldron

--- src/Blocks/BlockCauldron.h
+++ src/Blocks/BlockCauldron.h
@@ -21,19 +21,22 @@ public:
 		a_Pickups.push_back(cItem(E_ITEM_CAULDRON, 1, 0));
 	}
 
-	void OnUse(cChunkInterface * a_ChunkInterface, cWorldInterface * a_WorldInterface, cPlayer * a_Player, int a_BlockX, int a_BlockY, int a_BlockZ)
+	virtual void OnUse(cChunkInterface & a_ChunkInterface, cWorldInterface & a_WorldInterface, cPlayer * a_Player, int a_BlockX, int a_BlockY, int a_BlockZ, eBlockFace a_BlockFace, int a_CursorX, int a_CursorY, int a_CursorZ) override
 	{
-		char Meta = a_ChunkInterface->GetBlockMeta( a_BlockX, a_BlockY, a_BlockZ );
-		switch( a_Player->GetEquippedItem().m_ItemType )
+		char Meta = a_ChunkInterface.GetBlockMeta(a_BlockX, a_BlockY, a_BlockZ);
+		switch (a_Player->GetEquippedItem().m_ItemType)
 		{
 			case E_ITEM_WATER_BUCKET:
 			{
 				if (Meta < 3)
 				{
-					a_ChunkInterface->SetBlockMeta( a_BlockX, a_BlockY, a_BlockZ, 3 );
-					a_Player->GetInventory().RemoveOneEquippedItem();
-					cItem NewItem(E_ITEM_BUCKET, 1);
-					a_Player->GetInventory().AddItem(NewItem);
+					a_ChunkInterface.SetBlockMeta(a_BlockX, a_BlockY, a_BlockZ, 3);
+					if (!a_Player->IsGameModeCreative())
+					{
+						a_Player->GetInventory().RemoveOneEquippedItem();
+						cItem NewItem(E_ITEM_BUCKET, 1);
+						a_Player->GetInventory().AddItem(NewItem);
+					}
 				}
 				break;
 			}
@@ -41,7 +44,7 @@ public:
 			{
 				if (Meta > 0)
 				{
-					a_ChunkInterface->SetBlockMeta( a_BlockX, a_BlockY, a_BlockZ, --Meta);
+					a_ChunkInterface.SetBlockMeta(a_BlockX, a_BlockY, a_BlockZ, --Meta);
 					a_Player->GetInventory().RemoveOneEquippedItem();
 					cItem NewItem(E_ITEM_POTIONS, 1, 0);
 					a_Player->GetInventory().AddItem(NewItem);
