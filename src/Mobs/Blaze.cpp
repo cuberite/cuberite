764de9c39939e2184671e52ebab599c4e1c6b47d
TheJumper <maximilian.springer@web.de>
2014-02-22 01:27:32 +0100

    Changed killer detection by using cEntity methods

--- src/Mobs/Blaze.cpp
+++ src/Mobs/Blaze.cpp
@@ -19,7 +19,7 @@ cBlaze::cBlaze(void) :
 
 void cBlaze::GetDrops(cItems & a_Drops, cEntity * a_Killer)
 {
-	if (a_Killer->IsA("cPlayer") || a_Killer->IsA("cWolf"))
+	if (a_Killer->IsPlayer() || a_Killer->IsA("cWolf"))
 	{
 		int LootingLevel = a_Killer->GetEquippedWeapon().m_Enchantments.GetLevel(E_ENCHANTMENT_LOOTING);
 		AddRandomDropItem(a_Drops, 0, 1 + LootingLevel, E_ITEM_BLAZE_ROD);
