47be0487c5959d55c9840affcc560464ba035e48
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2014-03-02 17:03:29 +0000

    Commands are bound lower-case

--- src/Bindings/ManualBindings.cpp
+++ src/Bindings/ManualBindings.cpp
@@ -1477,6 +1477,8 @@ static int tolua_cPluginManager_BindCommand(lua_State * L)
 	AString Command   (tolua_tocppstring(L, idx,     ""));
 	AString Permission(tolua_tocppstring(L, idx + 1, ""));
 	AString HelpString(tolua_tocppstring(L, idx + 3, ""));
+
+	std::transform(Command.begin(), Command.end(), Command.begin(), ::tolower);
 	
 	// Store the function reference:
 	lua_pop(L, 1);  // Pop the help string off the stack
@@ -1541,6 +1543,8 @@ static int tolua_cPluginManager_BindConsoleCommand(lua_State * L)
 	cPluginManager * self = cPluginManager::Get();
 	AString Command   (tolua_tocppstring(L, idx,     ""));
 	AString HelpString(tolua_tocppstring(L, idx + 2, ""));
+
+	std::transform(Command.begin(), Command.end(), Command.begin(), ::tolower);
 	
 	// Store the function reference:
 	lua_pop(L, 1);  // Pop the help string off the stack
