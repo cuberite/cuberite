e73caf30f09248a7e428a937226e0383e69ff6ce
Tycho <work.tycho+git@gmail.com>
2014-03-09 09:33:40 -0700

    Fix gcc error attempt 2

--- src/WorldStorage/WSSCompact.cpp
+++ src/WorldStorage/WSSCompact.cpp
@@ -39,7 +39,7 @@ struct cWSSCompact::sChunkHeader
 
 
 /// The maximum number of PAK files that are cached
-const int MAX_PAK_FILES = 16U;
+const int MAX_PAK_FILES = static_cast<size_t>(16);
 
 /// The maximum number of unsaved chunks before the cPAKFile saves them to disk
 const int MAX_DIRTY_CHUNKS = 16;
