cff66315137e90db1292fa01640aae2e2d23b862
Tycho <work.tycho+git@gmail.com>
2014-03-10 11:14:34 -0700

    Removed unused macro from WSSCompact

--- src/WorldStorage/WSSCompact.cpp
+++ src/WorldStorage/WSSCompact.cpp
@@ -764,7 +764,6 @@ void cWSSCompact::cPAKFile::UpdateChunk2To3()
 
 		// Cannot use cChunk::MakeIndex because it might change again?????????
 		// For compatibility, use what we know is current
-		#define MAKE_2_INDEX( x, y, z ) ( y + (z * 256) + (x * 256 * 16) )
 		#define MAKE_3_INDEX( x, y, z ) ( x + (z * 16) + (y * 16 * 16) )
 
 		unsigned int InChunkOffset = 0;
