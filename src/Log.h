a19f5fc484648d226899667410aeb82de354c714
Tycho <work.tycho+git@gmail.com>
2014-03-11 13:51:56 -0700

    Move Format issues

--- src/Log.h
+++ src/Log.h
@@ -14,8 +14,8 @@ private:
 public:
 	cLog(const AString & a_FileName);
 	~cLog();
-	void Log(const char * a_Format, va_list argList);
-	void Log(const char * a_Format, ...);
+	void Log(const char * a_Format, va_list argList) FORMATSTRING(2,0);
+	void Log(const char * a_Format, ...) FORMATSTRING(2,3);
 	// tolua_begin
 	void SimpleLog(const char * a_String);
 	void OpenLog(const char * a_FileName);
