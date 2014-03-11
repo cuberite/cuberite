0fc4a1ee06199db80edd4a9f79a4ba1c4369f656
Tycho <work.tycho+git@gmail.com>
2014-03-11 12:36:51 -0700

    Move comment

--- SetFlags.cmake
+++ SetFlags.cmake
@@ -187,8 +187,8 @@ macro(set_exe_flags)
 		# we support non-IEEE 754 fpus so can make no guarentees about error
 		add_flags_cxx("-ffast-math")
 		
-		# clang does not provide the __extern_always_inline macro and a part of libm depends on this when using fast-math
 		if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+			# clang does not provide the __extern_always_inline macro and a part of libm depends on this when using fast-math
 			add_flags_cxx("-D__extern_always_inline=inline")
 			add_flags_cxx("-Werror -Weverything -Wno-c++98-compat-pedantic -Wno-string-conversion")
 			add_flags_cxx("-Wno-extra-semi -Wno-error=switch-enum -Wno-documentation")
