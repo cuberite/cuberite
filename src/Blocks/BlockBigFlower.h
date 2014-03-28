a78bacac3cb915a82b73bf8fe0ccc0a153190df9
Howaner <franzi.moos@googlemail.com>
2014-03-28 23:14:58 +0100

    Add tallgrass drop to big flowers. Add tallgrass drop, when a players break a tallgrass with the shear.

--- src/Blocks/BlockBigFlower.h
+++ src/Blocks/BlockBigFlower.h
@@ -44,10 +44,47 @@ public:
 		
 		a_Pickups.push_back(cItem(E_BLOCK_BIG_FLOWER, 1, Meta));
 	}
-	
+
+
+	virtual void OnDestroyedByPlayer(cChunkInterface & a_ChunkInterface, cWorldInterface & a_WorldInterface, cPlayer * a_Player, int a_BlockX, int a_BlockY, int a_BlockZ) override
+	{
+		NIBBLETYPE Meta = a_ChunkInterface.GetBlockMeta(a_BlockX, a_BlockY, a_BlockZ);
+		if (Meta & 0x8)
+		{
+			Meta = a_ChunkInterface.GetBlockMeta(a_BlockX, a_BlockY - 1, a_BlockZ);
+		}
+		
+		NIBBLETYPE FlowerMeta = Meta & 0x7;
+		if (!a_Player->IsGameModeCreative())
+		{
+			if (((FlowerMeta == 2) || (FlowerMeta == 3)) && (a_Player->GetEquippedItem().m_ItemType == E_ITEM_SHEARS))
+			{
+				MTRand r1;
+				if (r1.randInt(10) == 5)
+				{
+					cItems Pickups;
+					if (FlowerMeta == 2)
+					{
+						Pickups.Add(E_BLOCK_TALL_GRASS, 2, 1);
+					}
+					else if (FlowerMeta == 3)
+					{
+						Pickups.Add(E_BLOCK_TALL_GRASS, 2, 2);
+					}
+					a_WorldInterface.SpawnItemPickups(Pickups, a_BlockX, a_BlockY, a_BlockZ);
+				}
+				a_Player->UseEquippedItem();
+			}
+		}
+	}
+
+
 	virtual bool CanBeAt(cChunkInterface & a_ChunkInterface, int a_RelX, int a_RelY, int a_RelZ, const cChunk & a_Chunk) override
 	{
-		return ((a_RelY > 0) && (a_Chunk.GetBlock(a_RelX, a_RelY - 1, a_RelZ) != E_BLOCK_AIR));
+		return (
+			a_RelY > 0
+			&& a_Chunk.GetBlock(a_RelX, a_RelY - 1, a_RelZ) != E_BLOCK_AIR
+		);
 	}
 
 
@@ -59,7 +96,7 @@ public:
 	) override
 	{
 		int Meta = (((int)floor(a_Player->GetYaw() * 4.0 / 360.0 + 0.5) & 0x3) + 2) % 4;
-		a_ChunkInterface.SetBlock(a_WorldInterface, a_BlockX, a_BlockY + 1, a_BlockZ, m_BlockType, 0x8 | Meta);
+		a_ChunkInterface.FastSetBlock(a_BlockX, a_BlockY + 1, a_BlockZ, m_BlockType, 0x8 | Meta);
 	}
 
 
