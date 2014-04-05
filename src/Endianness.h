cuberite/cuberite

commit 8825d30aabbee8cb2e452dc5a17deb6f9b6892a7

Fixed some Clang warnings in protocols.

src/Endianness.h
@@ -1,12 +1,14 @@
 
 #pragma once
 
+#define ntohll(x) ((((UInt64)ntohl((u_long)x)) << 32) + ntohl(x >> 32))
+
 
 
 
 
 // Changes endianness
-inline unsigned long long HostToNetwork8(const void* a_Value )
+inline UInt64 HostToNetwork8(const void * a_Value)
 {
 	unsigned long long __HostToNetwork8;
 	memcpy( &__HostToNetwork8, a_Value, sizeof( __HostToNetwork8 ) );
@@ -18,7 +20,7 @@ inline unsigned long long HostToNetwork8(const void* a_Value )
 
 
 
-inline unsigned int HostToNetwork4(const void* a_Value )
+inline UInt32 HostToNetwork4(const void* a_Value )
 {
 	unsigned int __HostToNetwork4;
 	memcpy( &__HostToNetwork4, a_Value, sizeof( __HostToNetwork4 ) );
@@ -30,11 +32,10 @@ inline unsigned int HostToNetwork4(const void* a_Value )
 
 
 
-inline double NetworkToHostDouble8(const void* a_Value )
+inline double NetworkToHostDouble8(const void * a_Value)
 {
-#define ntohll(x)   ((((unsigned long long)ntohl((u_long)x)) << 32) + ntohl(x >> 32))
-	unsigned long long buf = 0;//(*(unsigned long long*)a_Value);
-	memcpy( &buf, a_Value, 8 );
+	UInt64 buf = 0;
+	memcpy(&buf, a_Value, 8);
 	buf = ntohll(buf);
 	double x;
 	memcpy(&x, &buf, sizeof(double));
@@ -45,23 +46,25 @@ inline double NetworkToHostDouble8(const void* a_Value )
 
 
 
-inline long long NetworkToHostLong8(const void * a_Value )
+inline Int64 NetworkToHostLong8(const void * a_Value)
 {
-	unsigned long long buf = *(unsigned long long*)a_Value;
+	UInt64 buf;
+	memcpy(&buf, &a_Value, 8);
 	buf = ntohll(buf);
-	return *reinterpret_cast<long long *>(&buf);
+	return *reinterpret_cast<Int64 *>(&buf);
 }
 
 
 
 
 
-inline float NetworkToHostFloat4(const void* a_Value )
+inline float NetworkToHostFloat4(const void * a_Value)
 {
-	u_long buf = *(u_long*)a_Value;
-	buf = ntohl( buf );
-	float x = 0;
-	memcpy( &x, &buf, sizeof(float) );
+	UInt32 buf;
+	float x;
+	memcpy(&buf, &a_Value, 4);
+	buf = ntohl(buf);
+	memcpy(&x, &buf, sizeof(float));
 	return x;
 }
 

src/ClientHandle.cpp
@@ -1532,7 +1532,7 @@ void cClientHandle::HandleTabCompletion(const AString & a_Text)
 
 
 
-void cClientHandle::SendData(const char * a_Data, int a_Size)
+void cClientHandle::SendData(const char * a_Data, size_t a_Size)
 {
 	if (m_HasSentDC)
 	{
@@ -1547,7 +1547,7 @@ void cClientHandle::SendData(const char * a_Data, int a_Size)
 		if (m_OutgoingDataOverflow.empty())
 		{
 			// No queued overflow data; if this packet fits into the ringbuffer, put it in, otherwise put it in the overflow buffer:
-			int CanFit = m_OutgoingData.GetFreeSpace();
+			size_t CanFit = m_OutgoingData.GetFreeSpace();
 			if (CanFit > a_Size)
 			{
 				CanFit = a_Size;

src/ClientHandle.h
@@ -225,7 +225,7 @@ public:
 	*/
 	bool HandleLogin(int a_ProtocolVersion, const AString & a_Username);
 	
-	void SendData(const char * a_Data, int a_Size);
+	void SendData(const char * a_Data, size_t a_Size);
 	
 	/** Called when the player moves into a different world; queues sreaming the new chunks */
 	void MoveToWorld(cWorld & a_World, bool a_SendRespawnPacket);

src/Protocol/Protocol.h
@@ -132,7 +132,7 @@ protected:
 	cCriticalSection m_CSPacket;  //< Each SendXYZ() function must acquire this CS in order to send the whole packet at once
 	
 	/// A generic data-sending routine, all outgoing packet data needs to be routed through this so that descendants may override it
-	virtual void SendData(const char * a_Data, int a_Size) = 0;
+	virtual void SendData(const char * a_Data, size_t a_Size) = 0;
 	
 	/// Called after writing each packet, enables descendants to flush their buffers
 	virtual void Flush(void) {};
@@ -143,10 +143,15 @@ protected:
 		SendData((const char *)&a_Value, 1);
 	}
 	
+	void WriteChar(char a_Value)
+	{
+		SendData(&a_Value, 1);
+	}
+	
 	void WriteShort(short a_Value)
 	{
-		a_Value = htons(a_Value);
-		SendData((const char *)&a_Value, 2);
+		u_short Value = htons((u_short)a_Value);
+		SendData((const char *)&Value, 2);
 	}
 	
 	/*
@@ -159,8 +164,8 @@ protected:
 	
 	void WriteInt(int a_Value)
 	{
-		a_Value = htonl(a_Value);
-		SendData((const char *)&a_Value, 4);
+		u_long Value = htonl((u_long)a_Value);
+		SendData((const char *)&Value, 4);
 	}
 	
 	void WriteUInt(unsigned int a_Value)
@@ -171,19 +176,19 @@ protected:
 	
 	void WriteInt64 (Int64 a_Value)
 	{
-		a_Value = HostToNetwork8(&a_Value);
-		SendData((const char *)&a_Value, 8);
+		UInt64 Value = HostToNetwork8(&a_Value);
+		SendData((const char *)Value, 8);
 	}
 	
 	void WriteFloat (float a_Value)
 	{
-		unsigned int val = HostToNetwork4(&a_Value);
+		UInt32 val = HostToNetwork4(&a_Value);
 		SendData((const char *)&val, 4);
 	}
 	
 	void WriteDouble(double a_Value)
 	{
-		unsigned long long val = HostToNetwork8(&a_Value);
+		UInt64 val = HostToNetwork8(&a_Value);
 		SendData((const char *)&val, 8);
 	}
 	
@@ -191,7 +196,7 @@ protected:
 	{
 		AString UTF16;
 		UTF8ToRawBEUTF16(a_Value.c_str(), a_Value.length(), UTF16);
-		WriteShort((unsigned short)(UTF16.size() / 2));
+		WriteShort((short)(UTF16.size() / 2));
 		SendData(UTF16.data(), UTF16.size());
 	}
 	
@@ -224,7 +229,7 @@ protected:
 	
 	void WriteVarUTF8String(const AString & a_String)
 	{
-		WriteVarInt(a_String.size());
+		WriteVarInt((UInt32)a_String.size());
 		SendData(a_String.data(), a_String.size());
 	}
 } ;

src/Protocol/Protocol125.cpp
@@ -1156,7 +1156,7 @@ AString cProtocol125::GetAuthServerID(void)
 
 
 
-void cProtocol125::SendData(const char * a_Data, int a_Size)
+void cProtocol125::SendData(const char * a_Data, size_t a_Size)
 {
 	m_Client->SendData(a_Data, a_Size);
 }

src/Protocol/Protocol125.h
@@ -125,7 +125,7 @@ protected:
 	
 	AString m_Username;  ///< Stored in ParseHandshake(), compared to Login username
 	
-	virtual void SendData(const char * a_Data, int a_Size) override;
+	virtual void SendData(const char * a_Data, size_t a_Size) override;
 	
 	/// Sends the Handshake packet
 	void SendHandshake(const AString & a_ConnectionHash);

src/Protocol/Protocol132.cpp
@@ -605,7 +605,7 @@ int cProtocol132::ParseTabCompletion(void)
 
 
 
-void cProtocol132::SendData(const char * a_Data, int a_Size)
+void cProtocol132::SendData(const char * a_Data, size_t a_Size)
 {
 	m_DataToSend.append(a_Data, a_Size);
 }

src/Protocol/Protocol132.h
@@ -87,7 +87,7 @@ protected:
 	/// The ServerID used for session authentication; set in StartEncryption(), used in GetAuthServerID()
 	AString m_AuthServerID;
 	
-	virtual void SendData(const char * a_Data, int a_Size) override;
+	virtual void SendData(const char * a_Data, size_t a_Size) override;
 	
 	// DEBUG:
 	virtual void Flush(void) override;

commit 143a5e61fc8e704f3df776d089217f031bc47779

Fixed Endiannes conversion routines for floats and doubles.
This bug has been introduced in 8825d30aabbee8cb2e452dc5a17deb6f9b6892a7.
This change fixes #854.

src/Endianness.h
@@ -49,7 +49,7 @@ inline double NetworkToHostDouble8(const void * a_Value)
 inline Int64 NetworkToHostLong8(const void * a_Value)
 {
 	UInt64 buf;
-	memcpy(&buf, &a_Value, 8);
+	memcpy(&buf, a_Value, 8);
 	buf = ntohll(buf);
 	return *reinterpret_cast<Int64 *>(&buf);
 }
@@ -62,7 +62,7 @@ inline float NetworkToHostFloat4(const void * a_Value)
 {
 	UInt32 buf;
 	float x;
-	memcpy(&buf, &a_Value, 4);
+	memcpy(&buf, a_Value, 4);
 	buf = ntohl(buf);
 	memcpy(&x, &buf, sizeof(float));
 	return x;

src/Entities/Entity.cpp
@@ -1469,7 +1469,7 @@ void cEntity::SteerVehicle(float a_Forward, float a_Sideways)
 Vector3d cEntity::GetLookVector(void) const
 {
 	Matrix4d m;
-	m.Init(Vector3f(), 0, m_Rot.x, -m_Rot.y);
+	m.Init(Vector3d(), 0, m_Rot.x, -m_Rot.y);
 	Vector3d Look = m.Transform(Vector3d(0, 0, 1));
 	return Look;
 }