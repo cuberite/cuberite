256691461bd9c4719e94d17942372ef8302a3607
archshift <admin@archshift.com>
2014-04-25 05:49:18 -0700

    Comments for TrySpawnChicken() and TeleportUser().

--- src/Entities/ProjectileEntity.h
+++ src/Entities/ProjectileEntity.h
@@ -210,6 +210,7 @@ protected:
 	virtual void OnHitSolidBlock(const Vector3d & a_HitPos, eBlockFace a_HitFace) override;
 	virtual void OnHitEntity    (cEntity & a_EntityHit, const Vector3d & a_HitPos) override;
 	
+	// Randomly decides whether to spawn a chicken where the egg lands.
 	void TrySpawnChicken(const Vector3d & a_HitPos);
 	
 	// tolua_begin
@@ -241,6 +242,7 @@ protected:
 	virtual void OnHitSolidBlock(const Vector3d & a_HitPos, eBlockFace a_HitFace) override;
 	virtual void OnHitEntity    (cEntity & a_EntityHit, const Vector3d & a_HitPos) override;
 	
+	// Teleports the user where the ender pearl lands.
 	void TeleportUser(const Vector3d & a_HitPos);
 
 	// tolua_begin
