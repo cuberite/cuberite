2f9580ad650ff69e71991b87d6b554ce080624a3
archshift <admin@archshift.com>
2014-04-26 17:19:45 -0700

    Moved cExpBottleEntity out of ProjectileEntity.h

--- src/Entities/ProjectileEntity.cpp
+++ src/Entities/ProjectileEntity.cpp
@@ -16,6 +16,7 @@
 #include "ProjectileArrow.h"
 #include "ProjectileEgg.h"
 #include "ProjectileEnderPearl.h"
+#include "ProjectileExpBottle.h"
 
 
 
@@ -451,32 +452,6 @@ void cThrownSnowballEntity::OnHitEntity(cEntity & a_EntityHit, const Vector3d & 
 	a_EntityHit.TakeDamage(dtRangedAttack, this, TotalDamage, 1);
 
 	Destroy(true);
-}
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// cBottleOEnchantingEntity :
-
-cExpBottleEntity::cExpBottleEntity(cEntity * a_Creator, double a_X, double a_Y, double a_Z, const Vector3d & a_Speed) :
-super(pkExpBottle, a_Creator, a_X, a_Y, a_Z, 0.25, 0.25)
-{
-	SetSpeed(a_Speed);
-}
-
-
-
-
-
-void cExpBottleEntity::OnHitSolidBlock(const Vector3d & a_HitPos, eBlockFace a_HitFace)
-{
-	// Spawn an experience orb with a reward between 3 and 11.
-	m_World->BroadcastSoundParticleEffect(2002, POSX_TOINT, POSY_TOINT, POSZ_TOINT, 0);
-	m_World->SpawnExperienceOrb(GetPosX(), GetPosY(), GetPosZ(), 3 + m_World->GetTickRandomNumber(8));
-
-	Destroy();
 }
 
 
--- src/Entities/ProjectileEntity.h
+++ src/Entities/ProjectileEntity.h
@@ -128,32 +128,6 @@ protected:
 
 
 
-class cExpBottleEntity :
-	public cProjectileEntity
-{
-	typedef cProjectileEntity super;
-
-public:
-
-	// tolua_end
-
-	CLASS_PROTODEF(cExpBottleEntity);
-
-	cExpBottleEntity(cEntity * a_Creator, double a_X, double a_Y, double a_Z, const Vector3d & a_Speed);
-
-protected:
-
-	// cProjectileEntity overrides:
-	virtual void OnHitSolidBlock(const Vector3d & a_HitPos, eBlockFace a_HitFace) override;
-
-	// tolua_begin
-
-};
-
-
-
-
-
 class cFireworkEntity :
 	public cProjectileEntity
 {
