a303adca8b4b86bc67f6557328768b28232580e4
worktycho <work.tycho@gmail.com>
2014-04-27 13:03:34 -0700

    Update CoverityModel.cpp

--- CoverityModel.cpp
+++ CoverityModel.cpp
@@ -1,5 +1,17 @@
 
-void tolua_error (lua_State* L, const char* msg, tolua_Error* err)
-{
-  __coverity_panic__();
+
+extern "C" { 
+  struct lua_State;
+  struct tolua_Error
+  {
+          int index;
+          int array;
+          const char* type;
+  };
+  
+  void tolua_error (lua_State* L, const char* msg, tolua_Error* err)
+  {
+    __coverity_panic__();
+  }
+  
 }
