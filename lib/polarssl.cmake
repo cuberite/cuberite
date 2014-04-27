1d3ad6faa22a040bc6bb15727b9783ae2f547a02
Tycho <work.tycho+git@gmail.com>
2014-04-27 12:25:03 -0700

    Added Testing capability

--- lib/polarssl.cmake
+++ lib/polarssl.cmake
@@ -1,5 +1,9 @@
 
 if(NOT TARGET polarssl)
 	message("including polarssl")
-	add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/polarssl/ ${CMAKE_CURRENT_BINARY_DIR}/lib/polarssl EXCLUDE_FROM_ALL )
+	if (SELF_TEST)
+		add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/polarssl/ ${CMAKE_CURRENT_BINARY_DIR}/lib/polarssl)
+	else()
+		add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/polarssl/ ${CMAKE_CURRENT_BINARY_DIR}/lib/polarssl EXCLUDE_FROM_ALL)
+	endif()
 endif()
