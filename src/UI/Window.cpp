7fe6e40bf7d40347f7f452ea2b7c353a5bc8073f
Howaner <franzi.moos@googlemail.com>
2014-05-03 23:42:26 +0200

    Add clicks, exp subtraction, item check, ...

--- src/UI/Window.cpp
+++ src/UI/Window.cpp
@@ -807,13 +807,42 @@ cCraftingWindow::cCraftingWindow(int a_BlockX, int a_BlockY, int a_BlockZ) :
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // cAnvilWindow:
 
-cAnvilWindow::cAnvilWindow() :
+cAnvilWindow::cAnvilWindow(int a_BlockX, int a_BlockY, int a_BlockZ) :
 	cWindow(wtAnvil, "Repair"),
-	m_RepairedItemName("")
+	m_RepairedItemName(""),
+	m_BlockX(a_BlockX),
+	m_BlockY(a_BlockY),
+	m_BlockZ(a_BlockZ)
 {
-	m_SlotAreas.push_back(new cSlotAreaAnvil(*this));
+	m_AnvilSlotArea = new cSlotAreaAnvil(*this);
+	m_SlotAreas.push_back(m_AnvilSlotArea);
 	m_SlotAreas.push_back(new cSlotAreaInventory(*this));
 	m_SlotAreas.push_back(new cSlotAreaHotBar(*this));
+}
+
+
+
+
+
+void cAnvilWindow::SetRepairedItemName(const AString & a_Name, cPlayer * a_Player)
+{
+	m_RepairedItemName = a_Name;
+
+	if (a_Player != NULL)
+	{
+		m_AnvilSlotArea->UpdateResult(*a_Player);
+	}
+}
+
+
+
+
+
+void cAnvilWindow::GetBlockPos(int & a_PosX, int & a_PosY, int & a_PosZ)
+{
+	a_PosX = m_BlockX;
+	a_PosY = m_BlockY;
+	a_PosZ = m_BlockZ;
 }
 
 
