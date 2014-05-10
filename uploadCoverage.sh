5b23bc53cc200b5fe9592c3c5f6fda1622e99b54
Tycho <work.tycho+git@gmail.com>
2014-05-10 14:05:39 +0100

    rewrote exclustions

--- uploadCoverage.sh
+++ uploadCoverage.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_MCSERVER_BUILD_TYPE" == "COVERAGE" ]
 	then
 	find tests -type f -name '*.gcda' -exec cp {} $(dirname {})/../$(basename {})
-	coveralls --exclude lib --exclude tests --gcov-options -q
+	coveralls --exclude lib --exclude Android --gcov-options -q
 fi
 
 
