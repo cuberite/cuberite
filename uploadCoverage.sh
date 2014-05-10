3b8f28aa49e53af8c5832a6975fea85ede583a35
Tycho <work.tycho+git@gmail.com>
2014-05-10 14:13:25 +0100

    quieted gcov 2

--- uploadCoverage.sh
+++ uploadCoverage.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_MCSERVER_BUILD_TYPE" == "COVERAGE" ]
 	then
 	find tests -type f -name '*.gcda' -exec sh -c 'cp {} $(dirname {})/../$(basename {})' \;
-	coveralls --exclude lib --exclude Android --gcov-options -q
+	coveralls --exclude lib --exclude Android --gcov-options q
 fi
 
 
