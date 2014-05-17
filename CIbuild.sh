8bb8c2ca7ded71f57ec83ebc8a5214d01ae0b9b7
worktycho <work.tycho@gmail.com>
2014-05-17 13:04:40 +0100

    Don't start mcserver up in coverage builds
    Starting the server up is not a test.

--- CIbuild.sh
+++ CIbuild.sh
@@ -6,4 +6,6 @@ cmake . -DBUILD_TOOLS=1 -DSELF_TEST=1;
 make -j 2;
 make -j 2 test;
 cd MCServer/;
-echo stop | $MCSERVER_PATH;
+if [ "$TRAVIS_MCSERVER_BUILD_TYPE" != "COVERAGE" ]
+	then echo stop | $MCSERVER_PATH;
+fi
