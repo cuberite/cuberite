941cb88ae456f0d8b91f6b5dc835c2536eef15f6
Mattes D <github@xoft.cz>
2014-05-23 12:33:30 +0200

    Fixed datatype conversion warning.

--- src/Entities/Entity.cpp
+++ src/Entities/Entity.cpp
@@ -1018,10 +1018,10 @@ void cEntity::TickInVoid(cChunk & a_Chunk)
 
 
 
-void cEntity::DetectCacti()
+void cEntity::DetectCacti(void)
 {
 	int X = POSX_TOINT, Y = POSY_TOINT, Z = POSZ_TOINT;
-	float w = m_Width / 2;
+	double w = m_Width / 2;
 	if (
 		(((X + 1) - GetPosX() < w) && (GetWorld()->GetBlock(X + 1, Y, Z) == E_BLOCK_CACTUS)) ||
 		(((GetPosX() - (X - 1)) - 1 < w) && (GetWorld()->GetBlock(X - 1, Y, Z) == E_BLOCK_CACTUS)) ||
