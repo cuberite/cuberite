8604dedfdf4e9522fa99e6280e09180523ecce09
Julian Laubstein <julianlaubstein@yahoo.de>
2014-06-04 10:20:20 +0200

    Update IncrementalRedstoneSimulator.cpp

--- src/Simulator/IncrementalRedstoneSimulator.cpp
+++ src/Simulator/IncrementalRedstoneSimulator.cpp
@@ -17,14 +17,14 @@
 
 	
 
-cIncrementalRedstoneSimulator::cIncrementalRedstoneSimulator(cWorld & a_World)
-	: super(a_World),
-	  m_RedstoneSimulatorChunkData(),
-	  m_PoweredBlocks(),
-	  m_LinkedPoweredBlocks(),
-	  m_SimulatedPlayerToggleableBlocks(),
-	  m_RepeatersDelayList(),
-	  m_Chunk()
+cIncrementalRedstoneSimulator::cIncrementalRedstoneSimulator(cWorld & a_World) :
+	super(a_World),
+	m_RedstoneSimulatorChunkData(),
+	m_PoweredBlocks(),
+	m_LinkedPoweredBlocks(),
+	m_SimulatedPlayerToggleableBlocks(),
+	m_RepeatersDelayList(),
+	m_Chunk()
 {
 }
 
