62669f64dec3f615518af56a864a72d5c18280b8
worktycho <work.tycho@gmail.com>
2014-06-27 10:27:02 +0100

    Update SetFlags.cmake

--- SetFlags.cmake
+++ SetFlags.cmake
@@ -27,7 +27,7 @@ endmacro()
 
 macro(set_flags)
 	# Add coverage processing, if requested:
-	if (BUILD_WITH_COVERAGE)
+	if (CMAKE_BUILD_TYPE STREQUALS "COVERAGE")
 		message("Including CodeCoverage")
 		set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/lib/cmake-coverage/")
 		include(CodeCoverage)
