835d00428c2af43050fa248ab988c431ec1be152
Howaner <franzi.moos@googlemail.com>
2014-07-02 14:49:08 +0200

    Update documentation

--- MCServer/Plugins/APIDump/Hooks/OnPlayerFoodLevelChange.lua
+++ MCServer/Plugins/APIDump/Hooks/OnPlayerFoodLevelChange.lua
@@ -2,17 +2,17 @@ return
 {
 	HOOK_PLAYER_FOOD_LEVEL_CHANGE =
 	{
-		CalledWhen = "Called before the player food level changed. Plugin may override / refuse.",
+		CalledWhen = "Called before the player food level changed. Plugin may override",
 		DefaultFnName = "OnPlayerFoodLevelChange", -- also used as pagename
 		Desc = [[
 			This hook is called before the food level changes.
 			The food level is not changed yet, plugins may choose
-			to override the new food level or refuse the change.
+			to refuse the change.
 		]],
 		Params =
 		{
 			{ Name = "Player", Type = "{{cPlayer}}", Notes = "The player who changes the food level." },
-			{ Name = "NewFoodLevel", Type = "number", Notes = "The new food level. You can override it." },
+			{ Name = "NewFoodLevel", Type = "number", Notes = "The new food level." },
 		},
 		Returns = [[
 			If the function returns false or no value, the next plugin's callback is called. Afterwards, the
