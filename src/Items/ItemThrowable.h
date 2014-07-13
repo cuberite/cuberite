2a0c041ad8eaa42e4293762df18d14c77e11ea49
Tycho <work.tycho+git@gmail.com>
2014-07-13 15:25:52 +0100

    Adjusted calls to CreateProjectile that passed Items

--- src/Items/ItemThrowable.h
+++ src/Items/ItemThrowable.h
@@ -35,7 +35,7 @@ public:
 		cFastRandom Random;
 		a_World->BroadcastSoundEffect("random.bow", a_Player->GetPosX(), a_Player->GetPosY() - a_Player->GetHeight(), a_Player->GetPosZ(), 0.5f, 0.4f / (Random.NextFloat(1.0f) * 0.4f + 0.8f));
 
-		if (a_World->CreateProjectile(Pos.x, Pos.y, Pos.z, m_ProjectileKind, a_Player, a_Player->GetEquippedItem(), &Speed) < 0)
+		if (a_World->CreateProjectile(Pos.x, Pos.y, Pos.z, m_ProjectileKind, a_Player, &a_Player->GetEquippedItem(), &Speed) < 0)
 		{
 			return false;
 		}
@@ -135,7 +135,7 @@ public:
 			return false;
 		}
 
-		if (a_World->CreateProjectile(a_BlockX + 0.5, a_BlockY + 1, a_BlockZ + 0.5, m_ProjectileKind, a_Player, a_Player->GetEquippedItem()) < 0)
+		if (a_World->CreateProjectile(a_BlockX + 0.5, a_BlockY + 1, a_BlockZ + 0.5, m_ProjectileKind, a_Player, &a_Player->GetEquippedItem()) < 0)
 		{
 			return false;
 		}
