0e5fa2662aa2b559335a25ee78cd086f795be79e
Tycho <work.tycho+git@gmail.com>
2014-07-15 00:01:16 +0100

    COnverted Lilypad back to mixin

--- src/Blocks/BlockLilypad.h
+++ src/Blocks/BlockLilypad.h
@@ -8,18 +8,12 @@
 
 
 class cBlockLilypadHandler :
-	public cBlockHandler
+	public cClearMetaOnDrop<cBlockHandler>
 {
 public:
 	cBlockLilypadHandler(BLOCKTYPE a_BlockType)
-		: cBlockHandler(a_BlockType)
+		: cClearMetaOnDrop<cBlockHandler>(a_BlockType)
 	{
-	}
-	
-	virtual void ConvertToPickups(cItems & a_Pickups, NIBBLETYPE a_BlockMeta) override
-	{
-		// Reset meta to zero
-		a_Pickups.push_back(cItem(E_BLOCK_LILY_PAD, 1, 0));
 	}
 };
 
