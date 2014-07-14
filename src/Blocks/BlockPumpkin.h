515c3cbc67816fb770ff7e32bd593bd55c26788c
Tycho <work.tycho+git@gmail.com>
2014-07-14 20:28:16 +0100

    Added cClearOnDrop Mixin to cPumpkin

--- src/Blocks/BlockPumpkin.h
+++ src/Blocks/BlockPumpkin.h
@@ -6,11 +6,11 @@
 
 
 class cBlockPumpkinHandler :
-	public cMetaRotator<cBlockHandler, 0x07, 0x02, 0x03, 0x00, 0x01, false>
+	public cClearMetaOnDrop<cMetaRotator<cBlockHandler, 0x07, 0x02, 0x03, 0x00, 0x01, false> >
 {
 public:
 	cBlockPumpkinHandler(BLOCKTYPE a_BlockType)
-		: cMetaRotator<cBlockHandler, 0x07, 0x02, 0x03, 0x00, 0x01, false>(a_BlockType)
+		: cClearMetaOnDrop<cMetaRotator<cBlockHandler, 0x07, 0x02, 0x03, 0x00, 0x01, false> >(a_BlockType)
 	{
 	}
 	
