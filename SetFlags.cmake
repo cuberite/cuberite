431d898a813ce65eeb1d0dfca449d310b2b2811b
archshift <admin@archshift.com>
2014-07-24 11:19:49 -0700

    Made redundant semi not an error, fixed one more.

--- SetFlags.cmake
+++ SetFlags.cmake
@@ -231,7 +231,7 @@ macro(set_exe_flags)
 			add_flags_cxx("-Wno-error=covered-switch-default -Wno-error=shadow")
 			add_flags_cxx("-Wno-error=exit-time-destructors -Wno-error=missing-variable-declarations")
 			add_flags_cxx("-Wno-error=global-constructors -Wno-implicit-fallthrough")
-			add_flags_cxx("-Wno-weak-vtables -Wno-switch-enum")
+			add_flags_cxx("-Wno-error=extra-semi -Wno-weak-vtables -Wno-switch-enum")
 		endif()
 	endif()
 
