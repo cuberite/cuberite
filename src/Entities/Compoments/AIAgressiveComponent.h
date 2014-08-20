75260997ad2763c56d8877b67e94693b5c46afa7
Samuel Barney <samjbarney@gmail.com>
2014-08-20 08:28:37 -0600

    Revert "Fixed all compile errors for AIAggressiveComponent and AIComponent."
    This reverts commit 62a8403e4c61bd50680e9d8712506b951ac61b48.

--- src/Entities/Compoments/AIAgressiveComponent.h
+++ src/Entities/Compoments/AIAgressiveComponent.h
@@ -1,25 +1,22 @@
 #pragma once
 #include "AIComponent.h"
 
-class cAIAggressiveComponent : public cAIComponent {
-	typedef cAIComponent super;
+class cAIAgressiveComponent : public cAIComponent {
 protected:
 	enum MState{ATTACKING, IDLE, CHASING, ESCAPING} m_EMState;
 	cEntity * m_Target;
 public:
-	cAIAggressiveComponent(cMonster * a_Monster) : cAIComponent(a_Monster), m_Target(NULL){}
+	cAIAgressiveComponent(cMonster * a_Monster) : cAIComponent(a_Monster), m_Target(null){}
 
 	virtual void Tick(float a_Dt, cChunk & a_Chunk) override;
 	virtual void InStateChasing(float a_Dt);
 	
+	virtual void EventSeePlayer(cEntity *);
 	virtual void Attack(float a_Dt);
 
 protected:
-	virtual void CheckEventLostPlayer(void);
-	virtual void CheckEventSeePlayer(void);
 	virtual void EventLosePlayer(void);
-	virtual void EventSeePlayer(cEntity *);
+	virtual void CheckEventLostPlayer(void);
 	
 	bool IsMovingToTargetPosition();
-	bool ReachedFinalDestination();
-};
+}
--- src/Entities/Compoments/AIComponent.h
+++ src/Entities/Compoments/AIComponent.h
@@ -7,7 +7,7 @@ class cAIComponent
 protected:
 	cMonster * m_Self;
 public:
-	cAIComponent(cMonster * a_Entity) : m_Self(a_Entity){}
+	cAIComponent(cEntity * a_Entity) : m_Self(a_Entity){}
 	
 	virtual void Tick(float a_Dt, cChunk & a_Chunk){}
 };
