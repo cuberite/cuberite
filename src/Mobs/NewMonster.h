0910a3d4f8ecc92093baa3218dc5bb01d6e995c3
Samuel Barney <samjbarney@gmail.com>
2014-08-20 08:31:28 -0600

    Added start of new monster class

---
+++ src/Mobs/NewMonster.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include "../Entities/Pawn.h"
+#include "../Defines.h"
+#include "../BlockID.h"
+#include "../Item.h"
+#include "../Enchantments.h"
+#include "../Mobs/AllComponents.h"
+
+
+
+
+
+class cClientHandle;
+class cWorld;
