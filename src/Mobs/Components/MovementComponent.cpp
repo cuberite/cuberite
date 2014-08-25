95c4b00dd0c2b23a6cf85f9f68cbd378d30989ad
Samuel Barney <samjbarney@gmail.com>
2014-08-25 17:22:56 -0600

    Added some missing code

--- src/Mobs/Components/MovementComponent.cpp
+++ src/Mobs/Components/MovementComponent.cpp
@@ -2,4 +2,32 @@
 #include "MovementComponent.h"
 #include "../Monster.h"
 
+#include "../../World.h"
+
 cMovementComponent::cMovementComponent(cMonster * a_Entity) : m_Self(a_Entity){}
+
+
+int cMovementComponent::FindFirstNonAirBlockPosition(double a_PosX, double a_PosZ)
+{
+	int PosY = (int)floor(m_Self->GetPosY());
+	PosY = Clamp(PosY, 0, cChunkDef::Height);
+
+	if (!cBlockInfo::IsSolid(m_Self->GetWorld()->GetBlock((int)floor(a_PosX), PosY, (int)floor(a_PosZ))))
+	{
+		while (!cBlockInfo::IsSolid(m_Self->GetWorld()->GetBlock((int)floor(a_PosX), PosY, (int)floor(a_PosZ))) && (PosY > 0))
+		{
+			PosY--;
+		}
+
+		return PosY + 1;
+	}
+	else
+	{
+		while (cBlockInfo::IsSolid(m_Self->GetWorld()->GetBlock((int)floor(a_PosX), PosY, (int)floor(a_PosZ))) && (PosY < cChunkDef::Height))
+		{
+			PosY++;
+		}
+
+		return PosY;
+	}
+}
