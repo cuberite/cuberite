11a50854369fd9b4556ba97a68455cfdf8a9d835
Samuel Barney <samjbarney@gmail.com>
2014-08-25 17:22:45 -0600

    Added functionality to the Environment Component.

--- src/Mobs/Components/EnvironmentComponent.h
+++ src/Mobs/Components/EnvironmentComponent.h
@@ -8,9 +8,16 @@ class cEnvironmentComponent
 {
 protected:
 	cMonster * m_Self;
+	int m_SightDistance;
 public:
 	cEnvironmentComponent(cMonster * a_Entity);
 	virtual ~cEnvironmentComponent(){}
 	
 	virtual void Tick(float a_Dt, cChunk & a_Chunk){}
+
+	// Get Functions
+	int GetSightDistance() { return m_SightDistance ; }
+
+	// Set Functions
+	void SetSightDistance(int a_SightDistance) { m_SightDistance = a_SightDistance; }
 };
