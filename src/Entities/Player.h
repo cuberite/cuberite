8dedbe4db52ee434595d9bdb32b3850a6c1e175f
nesco <emmanuel@federbusch.fr>
2014-09-16 20:02:27 +0200

    Preparing 1.8 changes
    Added Spectator Gamemode

--- src/Entities/Player.h
+++ src/Entities/Player.h
@@ -171,6 +171,9 @@ public:
 	/** Returns true if the player is in Adventure mode, either explicitly, or by inheriting from current world */
 	bool IsGameModeAdventure(void) const;
 	
+	/** Returns true if the player is in Spectator mode, either explicitly, or by inheriting from current world */
+	bool IsGameModeSpectator(void) const;
+	
 	AString GetIP(void) const { return m_IP; }  // tolua_export
 
 	/** Returns the associated team, NULL if none */
