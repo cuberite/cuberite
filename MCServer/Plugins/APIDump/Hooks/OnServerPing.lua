e5873d4e890ab4c5cdff365790a368d99984944d
Alexander Harkness <me@bearbin.net>
2014-09-27 21:28:59 +0100

    Copy edit.

--- MCServer/Plugins/APIDump/Hooks/OnServerPing.lua
+++ MCServer/Plugins/APIDump/Hooks/OnServerPing.lua
@@ -7,7 +7,7 @@ return
 		Desc = [[
 			A plugin may implement an OnServerPing() function and register it as a Hook to process pings from
 			clients in the server server list. It can change the logged in players and player capacity, as well
-			as the server description and the favicon by editing the values passed in.
+			as the server description and the favicon, that are displayed to the client in the server list.
 		]],
 		Params = {
 			{ Name = "ClientHandle", Type = "{{cClientHandle}}", Notes = "The client handle that pinged the server" },
