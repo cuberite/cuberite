6127c21af97e450037daecd981cc88f40c3af423
netchip <w00tc0d3@outlook.com>
2014-09-29 17:18:20 +0200

    BlockStone: drop the right cobblestone on onBreak().
    Signed-off-by: netchip <w00tc0d3@outlook.com>

--- src/Blocks/BlockStone.h
+++ src/Blocks/BlockStone.h
@@ -2,7 +2,7 @@
 #pragma once
 
 #include "BlockHandler.h"
-
+#include "BlockID.h"
 
 
 
@@ -18,9 +18,15 @@ public:
 
 	virtual void ConvertToPickups(cItems & a_Pickups, NIBBLETYPE a_BlockMeta) override
 	{
-		a_Pickups.push_back(cItem(E_BLOCK_COBBLESTONE, 1, 0));
+		switch(a_BlockMeta) {
+			case E_META_STONE:
+				a_Pickups.push_back(cItem(E_BLOCK_COBBLESTONE, 1, 0));
+				break;
+			default:
+				a_Pickups.push_back(cItem(E_BLOCK_STONE, 1, a_BlockMeta));
+		}
 	}
-} ;
+};
 
 
 
