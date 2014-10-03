77a9868f766401d3e046992d9aeb560c91b79181
Mattes D <github@xoft.cz>
2014-10-03 19:41:42 +0200

    QtBiomeVisualiser: Added mouse hover info.

--- Tools/QtBiomeVisualiser/QtChunk.h
+++ Tools/QtBiomeVisualiser/QtChunk.h
@@ -21,8 +21,12 @@ public:
 	/** Returns the image of the chunk's biomes. Assumes that the chunk is valid. */
 	const uchar * getImage(void) const;
 
-	/** Sets the image data for this chunk. */
-	void setImage(const Image & a_Image);
+	/** Sets the biomes to m_Biomes and renders them into m_Image. */
+	void setBiomes(const cChunkDef::BiomeMap & a_Biomes);
+
+	/** Returns the biome at the specified relative coords, or biInvalidBiome if not valid.
+	Coords must be valid inside this chunk. */
+	EMCSBiome getBiome(int a_RelX, int a_RelZ);
 
 protected:
 	/** Flag that specifies if the chunk data is valid - loaded or generated. */
@@ -30,6 +34,13 @@ protected:
 
 	/** Cached rendered image of this chunk's biomes. Updated in render(). */
 	Image m_Image;
+
+	/** Biomes comprising the chunk, in the X + 16 * Z ordering. */
+	cChunkDef::BiomeMap m_Biomes;
+
+
+	/** Renders biomes from m_Biomes into m_Image. */
+	void renderBiomes();
 };
 
 typedef std::shared_ptr<Chunk> ChunkPtr;
