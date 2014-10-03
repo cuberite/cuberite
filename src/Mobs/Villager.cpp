8aaf1c45e812ca84e5492c84c3cde271f97ae09b
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2014-10-03 22:22:59 +0100

    Villagers turn to Witches on lightning
    * Fixes #1409

--- src/Mobs/Villager.cpp
+++ src/Mobs/Villager.cpp
@@ -30,6 +30,13 @@ bool cVillager::DoTakeDamage(TakeDamageInfo & a_TDI)
 		return false;
 	}
 
+	if (a_TDI.DamageType == dtLightning)
+	{
+		Destroy();
+		GetWorld()->SpawnMob(GetPosX(), GetPosY(), GetPosZ(), mtWitch);
+		return true;
+	}
+
 	if ((a_TDI.Attacker != NULL) && a_TDI.Attacker->IsPlayer())
 	{
 		if (m_World->GetTickRandomNumber(5) == 3)
