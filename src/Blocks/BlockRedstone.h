bfc88d5b01e8c0b6a074d812f07c573053578b4f
Julian Laubstein <julianlaubstein@yahoo.de>
2014-10-06 15:03:22 +0200

    Added redstone beeing placeable on upside-down-halfslabs

--- src/Blocks/BlockRedstone.h
+++ src/Blocks/BlockRedstone.h
@@ -18,11 +18,34 @@ public:
 	}
 	
 
+	inline static eBlockFace BlockMetaDataToBlockFace(NIBBLETYPE a_Meta)
+	{
+		switch (a_Meta & 0x7)
+		{
+			case 0x1: return BLOCK_FACE_XP;
+			case 0x2: return BLOCK_FACE_XM;
+			case 0x3: return BLOCK_FACE_ZP;
+			case 0x4: return BLOCK_FACE_ZM;
+			case 0x5:
+			case 0x6: return BLOCK_FACE_YP;
+			case 0x7:
+			case 0x0: return BLOCK_FACE_YM;
+			default:
+			{
+				ASSERT(!"Unhandled block meta!");
+				return BLOCK_FACE_NONE;
+			}
+		}
+	}
+
+
 	virtual bool CanBeAt(cChunkInterface & a_ChunkInterface, int a_RelX, int a_RelY, int a_RelZ, const cChunk & a_Chunk) override
 	{
 		NIBBLETYPE Meta;
 		a_Chunk.UnboundedRelGetBlockMeta(a_RelX, a_RelY, a_RelZ, Meta);
+		eBlockFace Face = BlockMetaDataToBlockFace(Meta);
 
+		AddFaceDirection(a_RelX, a_RelY, a_RelZ, Face, true);
 		BLOCKTYPE BlockIsOn;
 		a_Chunk.UnboundedRelGetBlockType(a_RelX, a_RelY, a_RelZ, BlockIsOn);
 
@@ -31,7 +54,7 @@ public:
 				((BlockIsOn == E_BLOCK_WOODEN_SLAB) && ((Meta & 0x08) == 0x08)) ||
 				((BlockIsOn == E_BLOCK_STONE_SLAB) && ((Meta & 0x08) == 0x08))
 			) &&
-			(a_RelY > 0)
+			(a_RelY > 0) && Face ==	BLOCK_FACE_TOP
 		)
 		{
 			return true;
