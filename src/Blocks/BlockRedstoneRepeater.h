0fc7ee007472f39193b1616d4f627f8bd4d075ae
Julian Laubstein <julianlaubstein@yahoo.de>
2014-10-07 11:19:34 +0200

    Derp..

--- src/Blocks/BlockRedstoneRepeater.h
+++ src/Blocks/BlockRedstoneRepeater.h
@@ -43,6 +43,25 @@ public:
 		a_WorldInterface.SendBlockTo(a_BlockX, a_BlockY, a_BlockZ, a_Player);
 	}
 
+	inline static eBlockFace BlockMetaDataToBlockFace(NIBBLETYPE a_Meta)
+	{
+		switch (a_Meta & 0x7)
+		{
+			case 0x1: return BLOCK_FACE_XP;
+			case 0x2: return BLOCK_FACE_XM;
+			case 0x3: return BLOCK_FACE_ZP;
+			case 0x4: return BLOCK_FACE_ZM;
+			case 0x5:
+			case 0x6: return BLOCK_FACE_YP;
+			case 0x7:
+			case 0x0: return BLOCK_FACE_YM;
+			default:
+			{
+				ASSERT(!"Unhandled block meta!");
+				return BLOCK_FACE_NONE;
+			}
+		}
+	}
 
 	virtual void ConvertToPickups(cItems & a_Pickups, NIBBLETYPE a_BlockMeta) override
 	{
@@ -84,8 +103,6 @@ public:
 		)
 		{
 			return true;
-		}
-		
 		}
 		else
 		{
