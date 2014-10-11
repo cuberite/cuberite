b19742fe5a0a665213e9906c697c8a25613c6442
Alexander Harkness <bearbin@gmail.com>
2014-10-11 21:31:49 +0100

    Added easy installation method for Linux.

---
+++ easyinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PLATFORM=$(uname -m)
+
+echo "Identifying platform: $PLATFORM"
+case $PLATFORM in 
+	"i686") DOWNLOADURL="http://builds.cuberite.org/job/MCServer%20Linux%20x86/lastSuccessfulBuild/artifact/MCServer.tar" ;;
+	"x86_64") DOWNLOADURL="http://builds.cuberite.org/job/MCServer%20Linux%20x64/lastSuccessfulBuild/artifact/MCServer.tar" ;;
+	# Assume that all arm devices are a raspi for now.
+	"arm*") DOWNLOADURL="http://builds.cuberite.org/job/MCServer%20Linux%20armhf/lastSuccessfulBuild/artifact/MCServer.tar"
+esac
+
+echo "Downloading precompiled binaries."
+curl -s $DOWNLOADURL | tar -xzf -
+echo "Done."
+
+echo "MCServer is now installed, run using 'cd MCServer; ./MCServer'."
