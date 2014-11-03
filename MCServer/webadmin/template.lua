8604978d5079b8646bc4227253ccb172a3569c6b
Arian <arianroeloffzen@gmail.com>
2014-11-04 00:17:14 +0100

    Update template.lua

--- MCServer/webadmin/template.lua
+++ MCServer/webadmin/template.lua
@@ -91,38 +91,46 @@ function ShowPage(WebAdmin, TemplateRequest)
 </head>
 <body>
 <div class="contention">
-<div class="pagehead">
-<div class="row1">
-<div class="wrapper">
-<img src="/logo_login.png" alt="MCServer Logo" class="logo">
-</div>
-</div>
-<div id="panel">
-<div class="upper">
-<div class="wrapper">
-<ul class="menu top_links">
-<li><a>Server Name: <strong>]] .. cRoot:Get():GetServer():GetServerID() .. [[</strong></a></li>
-<li><a>Memory: <strong>]] .. MemoryUsageKiB / 1024 .. [[MB</strong></a></li>
-<li><a>Chunks: <strong>]] .. NumChunks .. [[</strong></a></li>
-</ul>
-<div class="welcome"><strong>Welcome back, ]] .. TemplateRequest.Request.Username .. [[</strong>&nbsp;&nbsp;&nbsp;<a href=".././"><img src="/log_out.png" style="vertical-align:bottom;"> Log Out</a></div>
-</div>
-</div>
-</div>
-</div>
+	<div class="pagehead">
+		<div class="row1">
+			<div class="wrapper">
+				<img src="/logo_login.png" alt="MCServer Logo" class="logo">
+			</div>
+		</div>
+		<div id="panel">
+			<div class="upper">
+				<div class="wrapper">
+					<ul class="menu top_links">
+						<li><a>Server Name: <strong>]] .. cRoot:Get():GetServer():GetServerID() .. [[</strong></a></li>
+						<li><a>Memory: <strong>]] .. MemoryUsageKiB / 1024 .. [[MB</strong></a></li>
+						<li><a>Chunks: <strong>]] .. NumChunks .. [[</strong></a></li>
+					</ul>
+					<div class="welcome"><strong>Welcome back, ]] .. TemplateRequest.Request.Username .. [[</strong>&nbsp;&nbsp;&nbsp;<a href=".././"><img src="/log_out.png" style="vertical-align:bottom;"> Log Out</a></div>
+				</div>
+			</div>
+		</div>
+	</div>
 <div class="row2 push10">
-<div class="wrapper padtopp">
-<table width="100%" border="0" align="center"><tbody>
-<tr>
-<td width="180" valign="top">
-<table border="0" cellspacing="0" cellpadding="5" class="tborder">
-<tbody><tr>
-<td class="thead"><strong>Menu</strong></td>
-</tr>
-<tr>
-<td class="trow1 smalltext"><a href=']] .. BaseURL .. [[' class='usercp_nav_item usercp_nav_home'>Home</a></td>
-</tr>
-<tr><td class="tcat"><div><span class="smalltext"><strong><font color="#000">Server Management</font></strong></span></div></td></tr></tbody><tbody style="" id="usercppms_e"><tr><td class="trow1 smalltext">
+	<div class="wrapper padtopp">
+		<table width="100%" border="0" align="center">
+			<tbody>
+				<tr>
+					<td width="180" valign="top">
+						<table border="0" cellspacing="0" cellpadding="5" class="tborder">
+						<tbody>
+							<tr>
+								<td class="thead"><strong>Menu</strong></td>
+							</tr>
+							<tr>
+								<td class="trow1 smalltext"><a href=']] .. BaseURL .. [[' class='usercp_nav_item usercp_nav_home'>Home</a></td>
+							</tr>
+							<tr>
+								<td class="tcat"><div><span class="smalltext"><strong><font color="#000">Server Management</font></strong></span></div></td>
+							</tr>
+						</tbody>
+						<tbody style="" id="usercppms_e">
+							<tr>
+								<td class="trow1 smalltext">
 	]])
 
 
@@ -142,48 +150,51 @@ function ShowPage(WebAdmin, TemplateRequest)
 	end
 
 	
-	Output([[</td></tr></tbody>
-</table>
-</td>
-<td valign="top" style='padding-left:25px;'>
-<table border="0" cellspacing="0" cellpadding="5" class="tborder">
-<tbody><tr>
-<td class="thead" colspan="2"><strong>]] .. SubTitle .. [[</strong></td>
-</tr>
-<tr>
-<td class="trow2">
-]] .. PageContent .. [[
-</td>
-</tr>
-</tbody></table>
-</td>
-</tr>
-</tbody></table>
-</div>
-</div>
+	Output([[
+								</td>
+							</tr>
+						</tbody>
+						</table>
+					</td>
+					<td valign="top" style='padding-left:25px;'>
+						<table border="0" cellspacing="0" cellpadding="5" class="tborder">
+						<tbody>
+							<tr>
+								<td class="thead" colspan="2"><strong>]] .. SubTitle .. [[</strong></td>
+							</tr>
+							<tr>
+								<td class="trow2">]] .. PageContent .. [[</td>
+							</tr>
+						</tbody>
+						</table>
+					</td>
+				</tr>
+			</tbody>
+			</table>
+		</div>
+	</div>
 </div>
 <div id="footer">
-<div class="upper">
-<div class="wrapper">
-<ul class="menu bottom_links">
-<li><a href=http://www.mc-server.org target=_blank>MCServer</a></li>
-<li><a href=http://forum.mc-server.org target=_blank>Forums</a></li>
-<li><a href=http://builds.cuberite.org target=_blank>Buildserver</a></li>
-<li><a href=http://mc-server.xoft.cz/LuaAPI target=_blank>API Documentation</a></li>
-<li><a href=http://book.mc-server.org/ target=_blank>User's Manual</a></li>
-</ul>
+	<div class="upper">
+		<div class="wrapper">
+			<ul class="menu bottom_links">
+				<li><a href="http://www.mc-server.org" target="_blank">MCServer</a></li>
+				<li><a href="http://forum.mc-server.org" target="_blank">Forums</a></li>
+				<li><a href="http://builds.cuberite.org" target="_blank">Buildserver</a></li>
+				<li><a href="http://mc-server.xoft.cz/LuaAPI" target="_blank">API Documentation</a></li>
+				<li><a href="http://book.mc-server.org/" target="_blank">User's Manual</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="lower">
+		<div class="wrapper">
+			<span id="current_time"><strong>Current time:</strong> <script type="text/javascript">document.write('Time: <strong><span id="date-time">',new Date().toLocaleString(),"</span></strong>");if(document.getElementById){onload=function(){setInterval("document.getElementById ('date-time').firstChild.data = new Date().toLocaleString()",50)}};</script></span>
+			<span id="copyright">Copyright © <a href="http://www.mc-server.org" target="_blank">MCServer Team</a> 2014.</span>
+		</div>
+	</div>
 </div>
-</div>
-<div class=lower>
-<div class=wrapper>
-<span id=current_time><strong>Current time:</strong> <script type=text/javascript>document.write('Time: <strong><span id="date-time">',new Date().toLocaleString(),"</span></strong>");if(document.getElementById){onload=function(){setInterval("document.getElementById ('date-time').firstChild.data = new Date().toLocaleString()",50)}};</script></span>
-<span id=copyright>
-Copyright © <a href=http://www.mc-server.org target=_blank>MCServer Team</a> 2014.
-</span>
-</div>
-</div>
-</div>
-</body></html>
+</body>
+</html>
 ]])
 	
 	return table.concat(SiteContent)
