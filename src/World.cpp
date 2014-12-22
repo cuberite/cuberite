cuberite/cuberite

commit ecf778bbec2794562bf5e5b8645e2171f7cd081c

cWorld: Moved initialization into constructor.
Fixes CID 71781.

src/World.cpp
@@ -279,6 +279,8 @@ cWorld::cWorld(const AString & a_WorldName, eDimension a_Dimension, const AStrin
 	m_WorldAge(0),
 	m_TimeOfDay(0),
 	m_LastTimeUpdate(0),
+	m_LastSave(0),
+	m_LastUnload(0),
 	m_SkyDarkness(0),
 	m_GameMode(gmNotSet),
 	m_bEnabledPVP(false),
@@ -620,9 +622,6 @@ void cWorld::Start(void)
 
 	m_ChunkMap = make_unique<cChunkMap>(this);
 	
-	m_LastSave = 0;
-	m_LastUnload = 0;
-
 	// preallocate some memory for ticking blocks so we don't need to allocate that often
 	m_BlockTickQueue.reserve(1000);
 	m_BlockTickQueueCopy.reserve(1000);

commit fbd0cf74bdc3a6c3a2524691da07e4ac4ad7940d

Fix compile error on OS X introduced by commit ecf778bbec2794562bf5e5b8645e2171f7cd081c
The following error occurs on OS X with the order reversed: `MCServer/src/World.cpp:282:2: error: field 'm_LastSave' will be initialized after field 'm_LastUnload' [-Werror,-Wreorder] m_LastSave(0),`.    Reversing the order of initialization fixes this.

src/World.cpp
@@ -279,8 +279,8 @@ cWorld::cWorld(const AString & a_WorldName, eDimension a_Dimension, const AStrin
 	m_WorldAge(0),
 	m_TimeOfDay(0),
 	m_LastTimeUpdate(0),
-	m_LastSave(0),
 	m_LastUnload(0),
+	m_LastSave(0),
 	m_SkyDarkness(0),
 	m_GameMode(gmNotSet),
 	m_bEnabledPVP(false),