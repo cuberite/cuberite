9b9ce6fa3b13cb00e380664a2b51e51ffb209b7d
Howaner <franzi.moos@googlemail.com>
2015-02-06 21:40:20 +0100

    Added IsOnGround() to cEntity

--- src/Entities/Entity.h
+++ src/Entities/Entity.h
@@ -443,6 +443,9 @@ public:
 
 	/** Set the invulnerable ticks from the entity */
 	void SetInvulnerableTicks(int a_InvulnerableTicks) { m_InvulnerableTicks = a_InvulnerableTicks; }
+
+	/** Returns whether the player is on ground or not */
+	bool IsOnGround(void) const { return m_bOnGround; }
 	
 	// tolua_end
 	
