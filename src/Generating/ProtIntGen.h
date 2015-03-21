7d73168c20c1287037c070802ee499a5a213884c
worktycho <work.tycho@gmail.com>
2015-03-21 22:23:24 +0000

    Make m_BufferSize constexpr
    see 5d2b366 for reasoning.

--- src/Generating/ProtIntGen.h
+++ src/Generating/ProtIntGen.h
@@ -34,7 +34,7 @@ class cProtIntGen
 protected:
 	/** Maximum size of the generated area.
 	Adjust the constant if you need larger areas, these are just so that we can use fixed-size buffers. */
-	static const int m_BufferSize = 900;
+	static constexpr int m_BufferSize = 900;
 
 public:
 
