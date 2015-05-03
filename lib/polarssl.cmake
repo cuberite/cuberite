2f369d09b60fc576c9d01ef6830d746a8ccf7d2f
Mattes D <github@xoft.cz>
2015-05-03 10:01:19 +0200

    Fixed multiple mbedtls inclusion.
    Ref.: #1880.

--- lib/polarssl.cmake
+++ lib/polarssl.cmake
@@ -1,11 +1,10 @@
 
-if(NOT TARGET polarssl)
+# This script includes PolarSSL, if not already included.
+# It is needed for when multiple projects reference PolarSSL.
+
+if(NOT TARGET mbedtls)
 	message("including polarssl")
 	set(ENABLE_TESTING OFF CACHE BOOL "Disable tests")
 	set(ENABLE_PROGRAMS OFF CACHE BOOL "Disable programs")
-	if (SELF_TEST)
-		add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/polarssl/ ${CMAKE_CURRENT_BINARY_DIR}/lib/polarssl)
-	else()
-		add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/polarssl/ ${CMAKE_CURRENT_BINARY_DIR}/lib/polarssl EXCLUDE_FROM_ALL)
-	endif()
+	add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/polarssl/ ${CMAKE_CURRENT_BINARY_DIR}/lib/polarssl EXCLUDE_FROM_ALL)
 endif()
