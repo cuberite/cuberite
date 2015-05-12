4b97569b3a635260cc9ce71470fc9b09cfb38167
Mattes D <github@xoft.cz>
2015-05-12 09:20:54 +0200

    ToLua now generates cLuaState::Push() and GetStackValue()
    For classes exported through ToLua it generates the cLuaState::Push() and cLuaState::GetStackValue() functions, as well as the supporting forward declarations and typedefs.
    Renamed virtual_method_hooks.lua to BindingsProcessor.lua since it no longer provides virtual method hooks and instead does additional processing when generating the bindings.

--- src/Bindings/AllToLua.sh
+++ src/Bindings/AllToLua.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/tolua++ -L virtual_method_hooks.lua -o Bindings.cpp -H Bindings.h AllToLua.pkg
+/usr/bin/tolua++ -L BindingsProcessor.lua -o Bindings.cpp -H Bindings.h AllToLua.pkg
