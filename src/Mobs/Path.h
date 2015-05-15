4ffc6621a9ca976f95eeda80e7c073ee932af85f
SafwatHalaby <SafwatHalaby@users.noreply.github.com>
2015-05-15 20:25:44 +0300

    PathFinder uses UniquePtr for cell map.

--- src/Mobs/Path.h
+++ src/Mobs/Path.h
@@ -131,7 +131,7 @@ private:
 
 	/* Pathfinding fields */
 	std::priority_queue<cPathCell *,  std::vector<cPathCell *>,  compareHeuristics> m_OpenList;
-	std::unordered_map<Vector3i,  cPathCell *, VectorHasher> m_Map;
+	std::unordered_map<Vector3i,  UniquePtr<cPathCell>, VectorHasher> m_Map;
 	Vector3i m_Destination;
 	Vector3i m_Source;
 	int m_StepsLeft;
