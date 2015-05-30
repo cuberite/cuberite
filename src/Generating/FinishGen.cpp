bca13d918770d28dda143931bbac4d594a327fac
tycho <work.tycho@gmail.com>
2015-05-30 12:31:05 +0100

    Fixed minor cast warning

--- src/Generating/FinishGen.cpp
+++ src/Generating/FinishGen.cpp
@@ -1379,7 +1379,7 @@ eMonsterType cFinishGenPassiveMobs::GetRandomMob(cChunkDesc & a_ChunkDesc)
 	using diff_type =
 		std::iterator_traits<decltype(MobIter)>::difference_type;
 	diff_type RandMob = static_cast<diff_type>
-		((unsigned long)(m_Noise.IntNoise2DInt(chunkX - chunkZ + 2, chunkX + 5) / 7)
+		(static_cast<size_t>(m_Noise.IntNoise2DInt(chunkX - chunkZ + 2, chunkX + 5) / 7)
 		% ListOfSpawnables.size());
 	std::advance(MobIter, RandMob);
 
