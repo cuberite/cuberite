f163cb1ebf0b87df3e6316ecc43ef9d80c6936a2
Alexander Harkness <me@bearbin.net>
2015-06-09 16:24:36 +0100

    Update build download links.

--- easyinstall.sh
+++ easyinstall.sh
@@ -4,10 +4,10 @@ PLATFORM=$(uname -m)
 
 echo "Identifying platform: $PLATFORM"
 case $PLATFORM in 
-	"i686") DOWNLOADURL="http://builds.cuberite.org/job/MCServer%20Linux%20x86/lastSuccessfulBuild/artifact/MCServer.tar.gz" ;;
-	"x86_64") DOWNLOADURL="http://builds.cuberite.org/job/MCServer%20Linux%20x64/lastSuccessfulBuild/artifact/MCServer.tar.gz" ;;
+	"i686") DOWNLOADURL="http://builds.cuberite.org/job/Cuberite%20Linux%20x86%20Master/lastSuccessfulBuild/artifact/Cuberite.tar.gz" ;;
+	"x86_64") DOWNLOADURL="http://builds.cuberite.org/job/Cuberite%20Linux%20x64%20Master/lastSuccessfulBuild/artifact/Cuberite.tar.gz" ;;
 	# Assume that all arm devices are a raspi for now.
-	arm*) DOWNLOADURL="http://builds.cuberite.org/job/MCServer%20Linux%20armhf/lastSuccessfulBuild/artifact/MCServer/MCServer.tar.gz"
+	arm*) DOWNLOADURL="http://builds.cuberite.org/job/Cuberite%20Linux%20raspi-armhf%20Master/lastSuccessfulBuild/artifact/Cuberite.tar.gz"
 esac
 
 echo "Downloading precompiled binaries."
