10e8f7454147ac79b8710fd503d97b31336bacb9
Mattes D <github@xoft.cz>
2015-06-09 16:54:56 +0200

    NoiseTest: Use std::chrono instead of clock() to measure time.

--- tests/NoiseTest/NoiseTest.cpp
+++ tests/NoiseTest/NoiseTest.cpp
@@ -4,26 +4,31 @@
 // Implements the main app entrypoint
 
 #include "Globals.h"
-#include <time.h>
 #include "Noise/Noise.h"
 #include "OldNoise.h"
 
 
 
 
-clock_t TestCubicNoise(void)
+using namespace std::chrono;
+
+
+
+
+
+static double TestCubicNoise(void)
 {
 	cCubicNoise Cubic(0);
 	NOISE_DATATYPE Values[256 * 256];
 	
 	// Do a speed test:
-	clock_t Begin = clock();
+	auto Begin = high_resolution_clock::now();
 	for (int i = 0; i < 1000; i++)
 	{
 		Cubic.Generate2D(Values, 256, 256, 0, (NOISE_DATATYPE)25.6, 0, (NOISE_DATATYPE)25.6);
 	}
-	clock_t Ticks = clock() - Begin;
-	printf("cCubicNoise generating 1000 * 256x256 values took %d ticks (%.02f sec)\n", Ticks, (double)Ticks / CLOCKS_PER_SEC);
+	double Ticks = static_cast<double>(duration_cast<milliseconds>(high_resolution_clock::now() - Begin).count());
+	printf("cCubicNoise generating 1000 * 256x256 values took %.02f ticks (%.02f sec)\n", Ticks, Ticks / 1000);
 	
 	return Ticks;
 }
@@ -32,19 +37,19 @@ clock_t TestCubicNoise(void)
 
 
 
-clock_t TestOldNoise(void)
+static double TestOldNoise(void)
 {
 	cOldCubicNoise Cubic(0);
 	NOISE_DATATYPE Values[256 * 256];
 
 	// Do a speed test:
-	clock_t Begin = clock();
+	auto Begin = high_resolution_clock::now();
 	for (int i = 0; i < 1000; i++)
 	{
 		Cubic.Generate2D(Values, 256, 256, 0, (NOISE_DATATYPE)25.6, 0, (NOISE_DATATYPE)25.6);
 	}
-	clock_t Ticks = clock() - Begin;
-	printf("cOldCubicNoise generating 1000 * 256x256 values took %d ticks (%.02f sec)\n", Ticks, (double)Ticks / CLOCKS_PER_SEC);
+	double Ticks = static_cast<double>(duration_cast<milliseconds>(high_resolution_clock::now() - Begin).count());
+	printf("cOldCubicNoise generating 1000 * 256x256 values took %.02f ticks (%.02f sec)\n", Ticks, Ticks / 1000);
 	
 	return Ticks;
 }
@@ -57,9 +62,13 @@ int main(int argc, char * argv[])
 {
 	// new cMCLogger();  // Create a logger (will set itself as the main instance
 	
-	clock_t NewTicks = TestCubicNoise();
-	clock_t OldTicks = TestOldNoise();
-	printf("New method is %.02fx faster\n", (double)OldTicks / NewTicks);
+	auto NewTicks = TestCubicNoise();
+	auto OldTicks = TestOldNoise();
+	printf("New method is %.02fx faster\n", OldTicks / NewTicks);
 	printf("Press Enter to quit program\n");
 	getchar();
 }
+
+
+
+
