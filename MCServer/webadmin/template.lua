354503fc07ca312d98d63aa10c0d7cac5fa17099
Artur <artur9010@users.noreply.github.com>
2015-07-12 12:35:09 +0200

    Update template.lua

--- MCServer/webadmin/template.lua
+++ MCServer/webadmin/template.lua
@@ -181,7 +181,7 @@ function ShowPage(WebAdmin, TemplateRequest)
 				<li><a href="http://forum.mc-server.org" target="_blank">Forums</a></li>
 				<li><a href="http://builds.cuberite.org" target="_blank">Buildserver</a></li>
 				<li><a href="http://mc-server.xoft.cz/LuaAPI" target="_blank">API Documentation</a></li>
-				<li><a href="http://book.mc-server.org/" target="_blank">User's Manual</a></li>
+				<li><a href="http://book.cuberite.org/" target="_blank">User's Manual</a></li>
 			</ul>
 		</div>
 	</div>
