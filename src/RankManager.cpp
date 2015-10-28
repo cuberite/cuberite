d14c8fa4469168693b04891d858e670f933c9be3
Alexander Harkness <me@bearbin.net>
2015-10-28 08:24:48 +0000

    Fix wrong teleport permission. (solves #2369)

--- src/RankManager.cpp
+++ src/RankManager.cpp
@@ -2248,10 +2248,10 @@ void cRankManager::CreateDefaults(void)
 	AddGroupToRank("Everything", "Admin");
 
 	// Add permissions to groups:
-	AddPermissionToGroup("core.build", "Default");
-	AddPermissionToGroup("core.tp",    "Teleport");
-	AddPermissionToGroup("core.kick",  "Kick");
-	AddPermissionToGroup("*",          "Everything");
+	AddPermissionToGroup("core.build",    "Default");
+	AddPermissionToGroup("core.teleport", "Teleport");
+	AddPermissionToGroup("core.kick",     "Kick");
+	AddPermissionToGroup("*",             "Everything");
 
 	// Set the default rank:
 	SetDefaultRank("Default");
