837b5e0d149d98418d1eea73a1b0f7c2b337dda9
Alexander Harkness <me@bearbin.net>
2015-10-31 22:22:59 +0000

    Fixed outdated help text in easyinstall.

--- easyinstall.sh
+++ easyinstall.sh
@@ -14,4 +14,4 @@ echo "Downloading precompiled binaries."
 curl -s $DOWNLOADURL | tar -xzf -
 echo "Done."
 
-echo "Cuberite is now installed, run using 'cd MCServer; ./MCServer'."
+echo "Cuberite is now installed, run using 'cd MCServer; ./Cuberite'."
