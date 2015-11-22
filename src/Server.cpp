5fae7a0eee37f525c4703d80bde8c1a0c953bffd
Safwat Halaby <SafwatHalaby@users.noreply.github.com>
2015-11-22 16:30:33 +0200

    Revert "Changed description to "Cuberite - Minecraft in C++!""
    This reverts commit f4406c047e0d1d11631e350fa61ad04d92b950ba.

--- src/Server.cpp
+++ src/Server.cpp
@@ -189,7 +189,7 @@ void cServer::PlayerDestroying(const cPlayer * a_Player)
 
 bool cServer::InitServer(cSettingsRepositoryInterface & a_Settings, bool a_ShouldAuth)
 {
-	m_Description = a_Settings.GetValueSet("Server", "Description", "Cuberite - Minecraft in C++!");
+	m_Description = a_Settings.GetValueSet("Server", "Description", "Cuberite - in C++!");
 	m_MaxPlayers  = a_Settings.GetValueSetI("Server", "MaxPlayers", 100);
 	m_bIsHardcore = a_Settings.GetValueSetB("Server", "HardcoreEnabled", false);
 	m_bAllowMultiLogin = a_Settings.GetValueSetB("Server", "AllowMultiLogin", false);
