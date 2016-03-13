02b839bab1ee6ec0a37661f7c4daaf76689b7269
Mattes D <github@xoft.cz>
2016-03-13 18:12:33 +0100

    Moved NetworkInterfaceEnum test to a separate test project.

---
+++ tests/Network/EnumInterfaces.cpp
@@ -0,0 +1,37 @@
+
+// EnumInterfaces.cpp
+
+// Implements the main app entrypoint for the EnumInterfaces network test
+// Lists all network interfaces to the console
+
+#include "Globals.h"
+#include "OSSupport/Network.h"
+#include "OSSupport/NetworkSingleton.h"
+
+
+
+
+
+int main(int argc, char * argv[])
+{
+	// Initialize the cNetwork subsystem:
+	cNetworkSingleton::Get().Initialise();
+
+	// Enumerate all the addresses:
+	printf("Enumerating all IP addresses...\n");
+	auto IPs = cNetwork::EnumLocalIPAddresses();
+	for (auto & ip: IPs)
+	{
+		printf("  %s\n", ip.c_str());
+	}
+	printf("Done.\n");
+
+	// Terminate the cNetwork subsystem:
+	cNetworkSingleton::Get().Terminate();
+
+	return 0;
+}
+
+
+
+
