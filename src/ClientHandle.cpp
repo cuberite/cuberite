f6a1dbcbfefef16c2e76cf41b8489f6b1d31ccfa
Alexander Harkness <bearbin@gmail.com>
2016-07-21 11:19:08 +0100

    Moved function over to cRoot.
    It was not exported to the Lua API just yet but it would be possible and
    advantageous to do so in future.

--- src/ClientHandle.cpp
+++ src/ClientHandle.cpp
@@ -1831,38 +1831,24 @@ void cClientHandle::HandleUnmount(void)
 void cClientHandle::HandleTabCompletion(const AString & a_Text)
 {
 	AStringVector Results;
+	// Get player name completions.
 	if (cRoot::Get()->GetServer()->ShouldAllowMultiWorldTabCompletion())
 	{
-		class cWorldCallback : public cWorldListCallback
-		{
-		public:
-			cWorldCallback(AStringVector & a_Results, const AString & a_Search) :
-				m_Results(a_Results),
-				m_Search(a_Search)
-			{
-			}
-
-			virtual bool Item(cWorld * a_World) override
-			{
-				a_World->TabCompleteUserName(m_Search, m_Results);
-				return true;
-			}
-		private:
-			AStringVector & m_Results;
-			const AString & m_Search;
-		} WC(Results, a_Text);
-
-		cRoot::Get()->ForEachWorld(WC);
+		Results = cRoot::Get()->GetPlayerTabCompletionMultiWorld(a_Text);
 	}
 	else
 	{
 		m_Player->GetWorld()->TabCompleteUserName(a_Text, Results);
 	}
+
+	// Get command completions.
 	cRoot::Get()->GetPluginManager()->TabCompleteCommand(a_Text, Results, m_Player);
 	if (Results.empty())
 	{
 		return;
 	}
+
+	// Sort and send results.
 	std::sort(Results.begin(), Results.end());
 	SendTabCompletionResults(Results);
 }
