79d8107d16e5a6958dfcbbec8a1055e1c51b06f4
ElNounch <publi@elnounch.net>
2016-08-24 08:12:36 +0000

    Introducing backward-cpp as stacktrace beautifier under Linux
    https://github.com/bombela/backward-cpp
    Can optionnaly use library bfd for more details
    (sudo apt-get install binutils-dev)

--- src/OSSupport/StackTrace.cpp
+++ src/OSSupport/StackTrace.cpp
@@ -5,12 +5,12 @@
 
 #include "Globals.h"
 #include "StackTrace.h"
-#ifdef _WIN32
+#if defined(_WIN32)
 	#include "../StackWalker.h"
-#else
-	#ifdef __GLIBC__
-		#include <execinfo.h>
-	#endif
+#elif defined(__linux__)
+	#include "../../lib/backward-cpp/backward.hpp"
+#elif defined(__GLIBC__)
+	#include <execinfo.h>
 	#include <unistd.h>
 #endif
 
@@ -20,25 +20,34 @@
 
 void PrintStackTrace(void)
 {
-	#ifdef _WIN32
-		// Reuse the StackWalker from the LeakFinder project already bound to MCS
-		// Define a subclass of the StackWalker that outputs everything to stdout
-		class PrintingStackWalker :
-			public StackWalker
+#if defined(_WIN32)
+	// Reuse the StackWalker from the LeakFinder project already bound to MCS
+	// Define a subclass of the StackWalker that outputs everything to stdout
+	class PrintingStackWalker :
+		public StackWalker
+	{
+		virtual void OnOutput(LPCSTR szText) override
 		{
-			virtual void OnOutput(LPCSTR szText) override
-			{
-				puts(szText);
-			}
-		} sw;
-		sw.ShowCallstack();
-	#else
-		#ifdef __GLIBC__
-			// Use the backtrace() function to get and output the stackTrace:
-			// Code adapted from https://stackoverflow.com/questions/77005/how-to-generate-a-stacktrace-when-my-gcc-c-app-crashes
-			void * stackTrace[30];
-			auto numItems = backtrace(stackTrace, ARRAYCOUNT(stackTrace));
-			backtrace_symbols_fd(stackTrace, numItems, STDERR_FILENO);
-		#endif
-	#endif
+			puts(szText);
+		}
+	} sw;
+	sw.ShowCallstack();
+#elif defined(__linux__)
+	// Use bacward-cpp (https://github.com/bombela/backward-cpp)
+	backward::StackTrace st;
+	st.load_here(32);
+	backward::Printer p;
+#if defined(_DEBUG)
+	p.snippet = true;
+#endif
+	p.color = true;
+	p.address = true;
+	p.print(st, stderr);
+#elif defined(__GLIBC__)
+	// Use the backtrace() function to get and output the stackTrace:
+	// Code adapted from https://stackoverflow.com/questions/77005/how-to-generate-a-stacktrace-when-my-gcc-c-app-crashes
+	void * stackTrace[30];
+	auto numItems = backtrace(stackTrace, ARRAYCOUNT(stackTrace));
+	backtrace_symbols_fd(stackTrace, numItems, STDERR_FILENO);
+#endif
 }
