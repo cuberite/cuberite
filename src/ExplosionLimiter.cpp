64da1132c099c55352b24da178d336327b3d2c08
LogicParrot <LogicParrot@users.noreply.github.com>
2016-09-03 23:31:41 +0300

    Globals.h

--- src/ExplosionLimiter.cpp
+++ src/ExplosionLimiter.cpp
@@ -1,3 +1,6 @@
+
+#include "Globals.h"  // NOTE: MSVC stupidness requires this to be the same across all modules
+
 #include "ExplosionLimiter.h"
 cExplosionLimiter::cExplosionLimiter()
 	: m_ExplosionsAllowedPerSecond()
