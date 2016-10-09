7b043a7d52299feea2e54e5d0d44ee01a246395f
LogicParrot <LogicParrot@users.noreply.github.com>
2016-10-09 15:45:24 +0300

    Teleportation now uses QueueTask

--- src/Entities/Player.cpp
+++ src/Entities/Player.cpp
@@ -1447,13 +1447,16 @@ void cPlayer::TeleportToCoords(double a_PosX, double a_PosY, double a_PosZ)
 	//  ask plugins to allow teleport to the new position.
 	if (!cRoot::Get()->GetPluginManager()->CallHookEntityTeleport(*this, m_LastPosition, Vector3d(a_PosX, a_PosY, a_PosZ)))
 	{
-		SetPosition(a_PosX, a_PosY, a_PosZ);
-		FreezeInternal(GetPosition(), false);
-		m_LastGroundHeight = a_PosY;
-		m_bIsTeleporting = true;
-
-		m_World->BroadcastTeleportEntity(*this, GetClientHandle());
-		m_ClientHandle->SendPlayerMoveLook();
+		m_World->QueueTask([this, a_PosX, a_PosY, a_PosZ](cWorld & a_World)
+		{
+			UNUSED(a_World);
+			SetPosition(a_PosX, a_PosY, a_PosZ);
+			FreezeInternal(GetPosition(), false);
+			m_LastGroundHeight = a_PosY;
+			m_bIsTeleporting = true;
+			m_World->BroadcastTeleportEntity(*this, GetClientHandle());
+			m_ClientHandle->SendPlayerMoveLook();
+		});
 	}
 }
 
