ae61f85963c096d188f9a4f87e5cb3455e6590d7
Bond-009 <lode2willems@hotmail.com>
2017-01-23 19:57:53 +0100

    Removed spaces

--- src/World.cpp
+++ src/World.cpp
@@ -550,13 +550,13 @@ void cWorld::Start(void)
 			MyNetherName = "";
 		}
 		if (cRoot::Get()->GetWorld(MyEndName) == nullptr)
- 		{
- 			MyEndName = GetName() + "_end";
- 			if (cRoot::Get()->GetWorld(MyEndName) == nullptr)
+		{
+			MyEndName = GetName() + "_end";
+			if (cRoot::Get()->GetWorld(MyEndName) == nullptr)
 			{
 				MyEndName = "";
 			}
- 		}
+		}
 
 		m_LinkedNetherWorldName = IniFile.GetValueSet("LinkedWorlds", "NetherWorldName", MyNetherName);
 		m_LinkedEndWorldName    = IniFile.GetValueSet("LinkedWorlds", "EndWorldName",    MyEndName);
