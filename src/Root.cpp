fd7d04d56f9be58b0530202f620c3bbd39f3ff85
Gareth Nelson <gareth@garethnelson.com>
2017-02-22 08:43:53 +0000

    fix indentation

--- src/Root.cpp
+++ src/Root.cpp
@@ -144,7 +144,7 @@ void cRoot::Start(std::unique_ptr<cSettingsRepositoryInterface> a_OverridesRepo)
 	m_SettingsFile = "settings.ini";
 	if (a_OverridesRepo->HasValue("Server","ConfigFile"))
 	{
-	m_SettingsFile = a_OverridesRepo->GetValue("Server","ConfigFile");
+		m_SettingsFile = a_OverridesRepo->GetValue("Server","ConfigFile");
 	}
 
 	auto IniFile = cpp14::make_unique<cIniFile>();
