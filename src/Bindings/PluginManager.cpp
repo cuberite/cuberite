1c31cb7eecc92d2fb218da8948e3201b618a46d2
Gareth Nelson <gareth@garethnelson.com>
2017-02-22 13:10:32 +0000

    Add support for alternate config file locations (#3578)

--- src/Bindings/PluginManager.cpp
+++ src/Bindings/PluginManager.cpp
@@ -111,7 +111,7 @@ void cPluginManager::RefreshPluginList(void)
 void cPluginManager::ReloadPluginsNow(void)
 {
 	cIniFile a_SettingsIni;
-	a_SettingsIni.ReadFile("settings.ini");
+	a_SettingsIni.ReadFile(cRoot::Get()->m_SettingsFilename);
 	ReloadPluginsNow(a_SettingsIni);
 }
 
