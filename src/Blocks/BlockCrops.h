786f281ee90c91cebc2a0e8500ed9d6cda783ce0
Mattes D <github@xoft.cz>
2017-02-24 10:06:33 +0100

    BlockCrops: Fixed comments and reformatted control flow.

--- src/Blocks/BlockCrops.h
+++ src/Blocks/BlockCrops.h
@@ -14,61 +14,23 @@ class cBlockCropsHandler :
 	public cBlockPlant
 {
 	typedef cBlockPlant Super;
+
 public:
-	cBlockCropsHandler(BLOCKTYPE a_BlockType)
-		: Super(a_BlockType, true)
+
+	cBlockCropsHandler(BLOCKTYPE a_BlockType):
+		Super(a_BlockType, true)
 	{
 	}
+
+
 
 	virtual void ConvertToPickups(cItems & a_Pickups, NIBBLETYPE a_Meta) override
 	{
 		cFastRandom rand;
 
-		// Beetroots have three stages before fully grown
-		if (a_Meta >= RipeMeta)
+		// If not fully grown, drop the "seed" of whatever is growing:
+		if (a_Meta < RipeMeta)
 		{
-			// Is fully grown, drop the entire produce:
-			switch (m_BlockType)
-			{
-				case E_BLOCK_BEETROOTS:
-				{
-					char SeedCount = static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2);
-					a_Pickups.push_back(cItem(E_ITEM_BEETROOT_SEEDS, SeedCount, 0));
-					char BeetrootCount = static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2);
-					a_Pickups.push_back(cItem(E_ITEM_BEETROOT, BeetrootCount, 0));  // [1 .. 3] with high preference of 2
-					break;
-				}
-				case E_BLOCK_CROPS:
-				{
-					a_Pickups.push_back(cItem(E_ITEM_WHEAT, 1, 0));
-					a_Pickups.push_back(cItem(E_ITEM_SEEDS, static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2), 0));  // [1 .. 3] with high preference of 2
-					break;
-				}
-				case E_BLOCK_CARROTS:
-				{
-					a_Pickups.push_back(cItem(E_ITEM_CARROT, static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2), 0));  // [1 .. 3] with high preference of 2
-					break;
-				}
-				case E_BLOCK_POTATOES:
-				{
-					a_Pickups.push_back(cItem(E_ITEM_POTATO, static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2), 0));  // [1 .. 3] with high preference of 2
-					if (rand.NextInt(21) == 0)
-					{
-						// With a 5% chance, drop a poisonous potato as well
-						a_Pickups.push_back(cItem(E_ITEM_POISONOUS_POTATO, 1, 0));
-					}
-					break;
-				}
-				default:
-				{
-					ASSERT(!"Unhandled block type");
-					break;
-				}
-			}  // switch (m_BlockType)
-		}
-		else
-		{
-			// Drop 1 item of whatever is growing
 			switch (m_BlockType)
 			{
 				case E_BLOCK_BEETROOTS: a_Pickups.push_back(cItem(E_ITEM_BEETROOT_SEEDS, 1, 0)); break;
@@ -81,8 +43,50 @@ public:
 					break;
 				}
 			}
+			return;
 		}
+
+		// Fully grown, drop the crop's produce:
+		switch (m_BlockType)
+		{
+			case E_BLOCK_BEETROOTS:
+			{
+				char SeedCount = static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2);  // [1 .. 3] with high preference of 2
+				a_Pickups.push_back(cItem(E_ITEM_BEETROOT_SEEDS, SeedCount, 0));
+				char BeetrootCount = static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2);  // [1 .. 3] with high preference of 2
+				a_Pickups.push_back(cItem(E_ITEM_BEETROOT, BeetrootCount, 0));
+				break;
+			}
+			case E_BLOCK_CROPS:
+			{
+				a_Pickups.push_back(cItem(E_ITEM_WHEAT, 1, 0));
+				a_Pickups.push_back(cItem(E_ITEM_SEEDS, static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2), 0));  // [1 .. 3] with high preference of 2
+				break;
+			}
+			case E_BLOCK_CARROTS:
+			{
+				a_Pickups.push_back(cItem(E_ITEM_CARROT, static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2), 0));  // [1 .. 3] with high preference of 2
+				break;
+			}
+			case E_BLOCK_POTATOES:
+			{
+				a_Pickups.push_back(cItem(E_ITEM_POTATO, static_cast<char>(1 + (rand.NextInt(3) + rand.NextInt(3)) / 2), 0));  // [1 .. 3] with high preference of 2
+				if (rand.NextInt(21) == 0)
+				{
+					// With a 5% chance, drop a poisonous potato as well
+					a_Pickups.push_back(cItem(E_ITEM_POISONOUS_POTATO, 1, 0));
+				}
+				break;
+			}
+			default:
+			{
+				ASSERT(!"Unhandled block type");
+				break;
+			}
+		}  // switch (m_BlockType)
 	}
+
+
 
 	virtual void OnUpdate(cChunkInterface & cChunkInterface, cWorldInterface & a_WorldInterface, cBlockPluginInterface & a_PluginInterface, cChunk & a_Chunk, int a_RelX, int a_RelY, int a_RelZ) override
 	{
@@ -103,10 +107,14 @@ public:
 		}
 	}
 
+
+
 	virtual bool CanBeAt(cChunkInterface & a_ChunkInterface, int a_RelX, int a_RelY, int a_RelZ, const cChunk & a_Chunk) override
 	{
 		return ((a_RelY > 0) && (a_Chunk.GetBlock(a_RelX, a_RelY - 1, a_RelZ) == E_BLOCK_FARMLAND));
 	}
+
+
 
 	virtual ColourID GetMapBaseColourID(NIBBLETYPE a_Meta) override
 	{
