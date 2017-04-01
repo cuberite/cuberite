dbdcb853466f78e64274074e4709d6247f766059
Marvin Kopf <marvinkopf@posteo.de>
2017-04-01 23:15:08 +0200

    Added a nullptr check to cEntity::IsA (#3659)
    Fixes #3603

--- src/BlockEntities/BlockEntity.h
+++ src/BlockEntities/BlockEntity.h
@@ -9,7 +9,7 @@
 #define BLOCKENTITY_PROTODEF(classname) \
 	virtual bool IsA(const char * a_ClassName) const override \
 	{ \
-		return ((strcmp(a_ClassName, #classname) == 0) || super::IsA(a_ClassName)); \
+		return ((a_ClassName != nullptr) && ((strcmp(a_ClassName, #classname) == 0) || super::IsA(a_ClassName))); \
 	} \
 	virtual const char * GetClass(void) const override \
 	{ \
