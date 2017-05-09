7c4576a02591d8ebe41656c5cefb1eb25a086a10
Lukas Pioch <lukas@zgow.de>
2017-05-09 14:24:41 +0200

    Exported cFallingBlock and cExpOrb (#3700)

--- src/Entities/FallingBlock.h
+++ src/Entities/FallingBlock.h
@@ -13,6 +13,7 @@ class cItem;
 
 
 
+// tolua_begin
 
 class cFallingBlock :
 	public cEntity
@@ -20,13 +21,19 @@ class cFallingBlock :
 	typedef cEntity super;
 
 public:
+	// tolua_end
+
 	CLASS_PROTODEF(cFallingBlock)
 
 	/** Creates a new falling block. a_BlockPosition is expected in world coords */
 	cFallingBlock(const Vector3i & a_BlockPosition, BLOCKTYPE a_BlockType, NIBBLETYPE a_BlockMeta);
 
+	// tolua_begin
+
 	BLOCKTYPE  GetBlockType(void) const { return m_BlockType; }
 	NIBBLETYPE GetBlockMeta(void) const { return m_BlockMeta; }
+
+	// tolua_end
 
 	// cEntity overrides:
 	virtual void SpawnOn(cClientHandle & a_ClientHandle) override;
@@ -36,7 +43,7 @@ private:
 	BLOCKTYPE  m_BlockType;
 	NIBBLETYPE m_BlockMeta;
 	Vector3i   m_OriginalPosition;  // Position where the falling block has started, in world coords
-} ;
+} ;  // tolua_export
 
 
 
