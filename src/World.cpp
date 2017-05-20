401f419b1bc0e4106b3bb38b541252dc9157a0ee
Pablo Beltrán <spekdrum@gmail.com>
2017-05-20 19:09:35 +0200

    QA code fix

--- src/World.cpp
+++ src/World.cpp
@@ -1150,7 +1150,7 @@ void cWorld::TickMobs(std::chrono::milliseconds a_Dt)
 				Monster->Tick(m_Dt, *(a_Entity->GetParentChunk()));
 			}
 			// Destroy far hostile mobs except if last target was a player
-			else if ((Monster->GetMobFamily() == cMonster::eFamily::mfHostile) && !Monster->IsLastTargetWasAPlayer() )
+			else if ((Monster->GetMobFamily() == cMonster::eFamily::mfHostile) && !Monster->IsLastTargetWasAPlayer())
 			{
 				if (Monster->GetMobType() != eMonsterType::mtWolf)
 				{
