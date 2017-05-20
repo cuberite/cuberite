8efd97facc069b19587fee154c3969ca36613ecb
Pablo Beltrán <spekdrum@gmail.com>
2017-05-20 19:23:17 +0200

    Set m_WasLastTargetAPlayer in Monster constructor

--- src/Mobs/Monster.cpp
+++ src/Mobs/Monster.cpp
@@ -101,6 +101,7 @@ cMonster::cMonster(const AString & a_ConfigName, eMonsterType a_MobType, const A
 	, m_Age(1)
 	, m_AgingTimer(20 * 60 * 20)  // about 20 minutes
 	, m_Target(nullptr)
+	, m_WasLastTargetAPlayer(false)
 {
 	if (!a_ConfigName.empty())
 	{
