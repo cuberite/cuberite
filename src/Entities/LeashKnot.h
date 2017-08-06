79af20c304664dde41854c888c7acab99ccebcd1
Pablo Beltrán <spekdrum@gmail.com>
2017-08-06 10:25:27 +0200

    Change definitions order

--- src/Entities/LeashKnot.h
+++ src/Entities/LeashKnot.h
@@ -33,15 +33,15 @@ public:
 
 private:
 
+	/** When a fence is destroyed, the knot on it gets destroyed after a while. This flag turns on the countdown to self destroy. */
+	bool m_ShouldSelfDestroy;
+	int m_TicksToSelfDestroy;
+
 	virtual void OnRightClicked(cPlayer & a_Player) override;
 	virtual void KilledBy(TakeDamageInfo & a_TDI) override;
 	virtual void GetDrops(cItems & a_Items, cEntity * a_Killer) override;
 	virtual void SpawnOn(cClientHandle & a_ClientHandle) override;
 	virtual void Tick(std::chrono::milliseconds a_Dt, cChunk & a_Chunk) override;
-
-	/** When a fence is destroyed, the knot on it gets destroyed after a while. This flag turns on the countdown to self destroy. */
-	bool m_ShouldSelfDestroy;
-	int m_TicksToSelfDestroy;
 
 };  // tolua_export
 
