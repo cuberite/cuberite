b29c174e7bd9943afd3e3e141ad6ae25889d52d0
LogicParrot <LogicParrot@users.noreply.github.com>
2017-08-24 15:53:28 +0300

    whitespace cleanup

--- src/Mobs/Sheep.h
+++ src/Mobs/Sheep.h
@@ -8,44 +8,44 @@
 
 
 class cSheep :
-    public cPassiveMonster
+	public cPassiveMonster
 {
-    typedef cPassiveMonster super;
+	typedef cPassiveMonster super;
 
 public:
 
-    /** The number is the color of the sheep.
-    Use E_META_WOOL_* constants for the wool color.
-    If you type -1, the server will generate a random color
-    with the GenerateNaturalRandomColor() function. */
-    cSheep(int a_Color = -1);
+	/** The number is the color of the sheep.
+	Use E_META_WOOL_* constants for the wool color.
+	If you type -1, the server will generate a random color
+	with the GenerateNaturalRandomColor() function. */
+	cSheep(int a_Color = -1);
 
-    CLASS_PROTODEF(cSheep)
+	CLASS_PROTODEF(cSheep)
 
-    virtual void GetDrops(cItems & a_Drops, cEntity * a_Killer = nullptr) override;
-    virtual void OnRightClicked(cPlayer & a_Player) override;
-    virtual void Tick(std::chrono::milliseconds a_Dt, cChunk & a_Chunk) override;
-    virtual void InheritFromParents(cMonster * a_Parent1, cMonster * a_Parent2) override;
+	virtual void GetDrops(cItems & a_Drops, cEntity * a_Killer = nullptr) override;
+	virtual void OnRightClicked(cPlayer & a_Player) override;
+	virtual void Tick(std::chrono::milliseconds a_Dt, cChunk & a_Chunk) override;
+	virtual void InheritFromParents(cMonster * a_Parent1, cMonster * a_Parent2) override;
 
-    virtual void GetFollowedItems(cItems & a_Items) override
-    {
-        a_Items.Add(E_ITEM_WHEAT);
-    }
+	virtual void GetFollowedItems(cItems & a_Items) override
+	{
+		a_Items.Add(E_ITEM_WHEAT);
+	}
 
-    /** Generates a random color for the sheep like the vanilla server.
-    The percent's where used are from the wiki: http://minecraft.gamepedia.com/Sheep#Breeding */
-    static NIBBLETYPE GenerateNaturalRandomColor(void);
+	/** Generates a random color for the sheep like the vanilla server.
+	The percent's where used are from the wiki: http://minecraft.gamepedia.com/Sheep#Breeding */
+	static NIBBLETYPE GenerateNaturalRandomColor(void);
 
-    bool IsSheared(void) const { return m_IsSheared; }
-    void SetSheared(bool a_IsSheared) { m_IsSheared = a_IsSheared; }
+	bool IsSheared(void) const { return m_IsSheared; }
+	void SetSheared(bool a_IsSheared) { m_IsSheared = a_IsSheared; }
 
-    int GetFurColor(void) const { return m_WoolColor; }
-    void SetFurColor(int a_WoolColor) { m_WoolColor = a_WoolColor; }
+	int GetFurColor(void) const { return m_WoolColor; }
+	void SetFurColor(int a_WoolColor) { m_WoolColor = a_WoolColor; }
 
 private:
-    bool m_IsSheared;
-    int m_WoolColor;
-    int m_TimeToStopEating;
+	bool m_IsSheared;
+	int m_WoolColor;
+	int m_TimeToStopEating;
 
 } ;
 
