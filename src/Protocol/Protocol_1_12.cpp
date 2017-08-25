0737594f5c140c41d4099676bab24df1000e4b5a
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-08-24 19:55:26 -0700

    Augment server ping for 1.12.1 protocol for Forge

--- src/Protocol/Protocol_1_12.cpp
+++ src/Protocol/Protocol_1_12.cpp
@@ -1668,6 +1668,7 @@ void cProtocol_1_12_1::HandlePacketStatusRequest(cByteBuffer & a_ByteBuffer)
 	ResponseValue["version"] = Version;
 	ResponseValue["players"] = Players;
 	ResponseValue["description"] = Description;
+	m_Client->ForgeAugmentServerListPing(ResponseValue);
 	if (!Favicon.empty())
 	{
 		ResponseValue["favicon"] = Printf("data:image/png;base64,%s", Favicon.c_str());
