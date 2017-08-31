9bd939dc985be53eee675a22e91f43e3b9b6edca
LogicParrot <LogicParrot@users.noreply.github.com>
2017-08-31 13:54:57 +0300

    Pathfinder DontCare now a member variable

--- src/Mobs/Behaviors/BehaviorWanderer.cpp
+++ src/Mobs/Behaviors/BehaviorWanderer.cpp
@@ -39,7 +39,7 @@ bool cBehaviorWanderer::ControlStarting(std::chrono::milliseconds a_Dt, cChunk &
 {
 	UNUSED(a_Dt);
 	UNUSED(a_Chunk);
-	m_OldDontCare = m_Parent->GetPathFinder().getDontCare(true);
+	m_OldDontCare = m_Parent->GetPathFinder().getDontCare();
 	m_Parent->GetPathFinder().setDontCare(true); // We don't care we're we are going when
 	// wandering. If a path is not found, the pathfinder just modifies our destination.
 	return true;
