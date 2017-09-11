73accc936a30c1dc06a31c17c457cbd138c60fc6
Alexander Harkness <bearbin@gmail.com>
2017-09-11 09:22:25 +0100

    Adjust lava damage values
    Fixes isue #2925

--- src/Entities/Entity.cpp
+++ src/Entities/Entity.cpp
@@ -1293,7 +1293,7 @@ void cEntity::TickBurning(cChunk & a_Chunk)
 		m_TicksSinceLastFireDamage++;
 		if (m_TicksSinceLastFireDamage >= FIRE_TICKS_PER_DAMAGE)
 		{
-			if (!IsFireproof())
+			if (!IsFireproof() && !HasLava)
 			{
 				TakeDamage(dtFireContact, nullptr, FIRE_DAMAGE, 0);
 			}
