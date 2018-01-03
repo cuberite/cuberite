e554c0a113df78359ef813f65587e3a154f4c498
Alexander Lyons Harkness <me@bearbin.net>
2018-01-03 08:50:12 +0000

    Remove old comment and fire detection code for pickups

--- src/Entities/Pickup.cpp
+++ src/Entities/Pickup.cpp
@@ -147,13 +147,7 @@ void cPickup::Tick(std::chrono::milliseconds a_Dt, cChunk & a_Chunk)
 			// Position might have changed due to physics. So we have to make sure we have the correct chunk.
 			GET_AND_VERIFY_CURRENT_CHUNK(CurrentChunk, BlockX, BlockZ)
 
-			int RelBlockX = BlockX - (CurrentChunk->GetPosX() * cChunkDef::Width);
-			int RelBlockZ = BlockZ - (CurrentChunk->GetPosZ() * cChunkDef::Width);
-
-			// If the pickup is on the bottommost block position, make it think the void is made of air: (#131)
-			BLOCKTYPE BlockBelow = (BlockY > 0) ? CurrentChunk->GetBlock(RelBlockX, BlockY - 1, RelBlockZ) : E_BLOCK_AIR;
-			BLOCKTYPE BlockIn = CurrentChunk->GetBlock(RelBlockX, BlockY, RelBlockZ);
-
+			// Destroy the pickup if it is on fire:
 			if (IsOnFire())
 			{
 				m_bCollected = true;
