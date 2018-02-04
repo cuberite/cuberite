f784757b1cc9511bc6095c9a9424ed60fa1b21c7
peterbell10 <peterbell10@live.co.uk>
2018-02-04 22:15:31 +0000

    cChunk and cChunkData: Use vectors for block get and set functions (#4172)
    * cChunkData: Change interface to use Vector3i
    * cChunk: Add Vector3i overloads for bounded block get and set functions.

--- tests/ChunkData/ArraytoCoord.cpp
+++ tests/ChunkData/ArraytoCoord.cpp
@@ -10,19 +10,19 @@ int main(int argc, char** argv)
 
 	class cMockAllocationPool
 		: public cAllocationPool<cChunkData::sChunkSection>
- 	{
+	{
 		virtual cChunkData::sChunkSection * Allocate()
 		{
 			return new cChunkData::sChunkSection();
 		}
-		
+
 		virtual void Free(cChunkData::sChunkSection * a_Ptr)
 		{
 			delete a_Ptr;
 		}
 	} Pool;
 	{
-	
+
 		// Test first segment
 		cChunkData buffer(Pool);
 
@@ -30,25 +30,25 @@ int main(int argc, char** argv)
 		memset(SrcBlockBuffer, 0x00, sizeof(SrcBlockBuffer));
 		SrcBlockBuffer[7 + (4 * 16) + (5 * 16 * 16)] = 0xcd;
 		buffer.SetBlockTypes(SrcBlockBuffer);
-		testassert(buffer.GetBlock(7, 5, 4) == 0xcd);
-	
+		testassert(buffer.GetBlock({ 7, 5, 4 }) == 0xcd);
+
 		NIBBLETYPE SrcNibbleBuffer[16 * 16 * 256 / 2];
 		memset(SrcNibbleBuffer, 0x00, sizeof(SrcNibbleBuffer));
 		SrcNibbleBuffer[(6 + (1 * 16) + (2 * 16 * 16)) / 2] = 0xe;
 		buffer.SetMetas(SrcNibbleBuffer);
-		testassert(buffer.GetMeta(6, 2, 1) == 0xe);
-	
+		testassert(buffer.GetMeta({ 6, 2, 1 }) == 0xe);
+
 		memset(SrcNibbleBuffer, 0x00, sizeof(SrcNibbleBuffer));
 		SrcNibbleBuffer[(6 + (1 * 16) + (2 * 16 * 16)) / 2] = 0xe;
 		buffer.SetBlockLight(SrcNibbleBuffer);
-		testassert(buffer.GetBlockLight(6, 2, 1) == 0xe);
-	
+		testassert(buffer.GetBlockLight({ 6, 2, 1 }) == 0xe);
+
 		memset(SrcNibbleBuffer, 0x00, sizeof(SrcNibbleBuffer));
 		SrcNibbleBuffer[(6 + (1 * 16) + (2 * 16 * 16)) / 2] = 0xe;
 		buffer.SetSkyLight(SrcNibbleBuffer);
-		testassert(buffer.GetSkyLight(6, 2, 1) == 0xe);
+		testassert(buffer.GetSkyLight({ 6, 2, 1 }) == 0xe);
 	}
-	
+
 	{
 		// test following segment
 		cChunkData buffer(Pool);
@@ -57,25 +57,25 @@ int main(int argc, char** argv)
 		memset(SrcBlockBuffer, 0x00, sizeof(SrcBlockBuffer));
 		SrcBlockBuffer[7 + (4 * 16) + (24 * 16 * 16)] = 0xcd;
 		buffer.SetBlockTypes(SrcBlockBuffer);
-		testassert(buffer.GetBlock(7, 24, 4) == 0xcd);
-	
+		testassert(buffer.GetBlock({ 7, 24, 4 }) == 0xcd);
+
 		NIBBLETYPE SrcNibbleBuffer[16 * 16 * 256 / 2];
 		memset(SrcNibbleBuffer, 0x00, sizeof(SrcNibbleBuffer));
 		SrcNibbleBuffer[(6 + (1 * 16) + (24 * 16 * 16)) / 2] = 0xe;
 		buffer.SetMetas(SrcNibbleBuffer);
-		testassert(buffer.GetMeta(6, 24, 1) == 0xe);
-	
+		testassert(buffer.GetMeta({ 6, 24, 1 }) == 0xe);
+
 		memset(SrcNibbleBuffer, 0x00, sizeof(SrcNibbleBuffer));
 		SrcNibbleBuffer[(6 + 1 * 16 + 24 * 16 * 16) / 2] = 0xe;
 		buffer.SetBlockLight(SrcNibbleBuffer);
-		testassert(buffer.GetBlockLight(6, 24, 1) == 0xe);
-	
+		testassert(buffer.GetBlockLight({ 6, 24, 1 }) == 0xe);
+
 		memset(SrcNibbleBuffer, 0xff, sizeof(SrcNibbleBuffer));
 		SrcNibbleBuffer[(6 + (1 * 16) + (24 * 16 * 16)) / 2] = 0xe;
 		buffer.SetSkyLight(SrcNibbleBuffer);
-		testassert(buffer.GetSkyLight(6, 24, 1) == 0xe);
+		testassert(buffer.GetSkyLight({ 6, 24, 1 }) == 0xe);
 	}
-	
+
 	{
 		// test zeros
 		cChunkData buffer(Pool);
@@ -83,22 +83,22 @@ int main(int argc, char** argv)
 		BLOCKTYPE SrcBlockBuffer[16 * 16 * 256];
 		memset(SrcBlockBuffer, 0x00, sizeof(SrcBlockBuffer));
 		buffer.SetBlockTypes(SrcBlockBuffer);
-		testassert(buffer.GetBlock(7, 24, 4) == 0x00);
-	
+		testassert(buffer.GetBlock({ 7, 24, 4 }) == 0x00);
+
 		NIBBLETYPE SrcNibbleBuffer[16 * 16 * 256 / 2];
 		memset(SrcNibbleBuffer, 0x00, sizeof(SrcNibbleBuffer));
 		buffer.SetMetas(SrcNibbleBuffer);
-		testassert(buffer.GetMeta(6, 24, 1) == 0x0);
-	
+		testassert(buffer.GetMeta({ 6, 24, 1 }) == 0x0);
+
 		memset(SrcNibbleBuffer, 0x00, sizeof(SrcNibbleBuffer));
 		buffer.SetBlockLight(SrcNibbleBuffer);
-		testassert(buffer.GetBlockLight(6, 24, 1) == 0x0);
-	
+		testassert(buffer.GetBlockLight({ 6, 24, 1 }) == 0x0);
+
 		memset(SrcNibbleBuffer, 0xff, sizeof(SrcNibbleBuffer));
 		buffer.SetSkyLight(SrcNibbleBuffer);
-		testassert(buffer.GetSkyLight(6, 24, 1) == 0xf);
+		testassert(buffer.GetSkyLight({ 6, 24, 1 }) == 0xf);
 	}
-	
+
 	// All tests passed:
 	return 0;
 }
