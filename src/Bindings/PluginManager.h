4f26f653e1ae168a050f3eca3e8073eca3f10367
peterbell10 <peterbell10@live.co.uk>
2018-03-02 10:40:48 +0000

    Add cPluginManager::GenericCallHook (#4027)
    Replace repetitive CallHook functions with a generic implementation.
    Also converts some loops to range-based for.
    
    Related to #1134

--- src/Bindings/PluginManager.h
+++ src/Bindings/PluginManager.h
@@ -444,6 +444,12 @@ private:
 
 	/** Returns the folders that are specified in the settings ini to load plugins from. */
 	AStringVector GetFoldersToLoad(cSettingsRepositoryInterface & a_Settings);
+
+	/** Calls a_HookFunction on each plugin registered to the hook HookName.
+	Returns false if the action is to continue or true if the plugin wants to abort.
+	Accessible only from within PluginManager.cpp */
+	template <typename HookFunction>
+	bool GenericCallHook(PluginHook a_HookName, HookFunction a_HookFunction);
 } ;  // tolua_export
 
 
