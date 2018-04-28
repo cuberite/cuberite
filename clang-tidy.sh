1841a389e48c0d18dfe6f81ee75738e44241ce17
Bond_009 <bond.009@outlook.com>
2018-04-28 10:58:09 +0200

    Add clang-tidy to circle.yml

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
 
+FILE="tidy-fixes.yaml"
+REGEX=".+/cuberite/src/\w+/.+"
+# TODO: Add -quiet when Circle CI is updated to 2.0 with a newer version of Ubuntu
+ARGS="-header-filter $REGEX -export-fixes $FILE $REGEX"
+
 cmake --target Cuberite -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
-run-clang-tidy.py -header-filter ".+/cuberite/src/\w+/.+" -export-fixes "tidy-fixes.yaml" ".+/cuberite/src/\w+/.+"
+if [ -f ./run-clang-tidy.py ]; then
+	./run-clang-tidy.py $ARGS
+else
+	run-clang-tidy.py $ARGS
+fi
+
+if [ $? -ne 0 ]; then
+	echo "run-clang-tidy.py failed to run"
+	exit 1
+fi
+
+if [ -s $FILE ]; then
+	echo "Found violations, fixes can be found in $FILE"
+	exit 1
+else
+	echo "No violations found"
+fi
