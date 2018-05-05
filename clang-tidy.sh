8f28b69cd8ba833188ebffdd84dcc1cae4f219fa
Bond_009 <bond.009@outlook.com>
2018-05-05 23:17:22 +0200

    Fixed regex etc

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-FILE="tidy-fixes.yaml"
-REGEX=".+/cuberite/src/\w+/.+"
+set -e
+
+FIXES_FILE="tidy-fixes.yaml"
+REGEX=".+/cuberite/src/[^\.].+"
 # TODO: Add -quiet when Circle CI is updated to 2.0 with a newer version of Ubuntu
-ARGS="-header-filter $REGEX -export-fixes $FILE $REGEX"
+ARGS="-header-filter $REGEX -export-fixes $FIXES_FILE $REGEX"
+TIDY_FAILED=0
 
-cmake --target Cuberite -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
+mkdir -p tidy-build
+cd tidy-build
+cmake --target Cuberite -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 if [ -f ./run-clang-tidy.py ]; then
-	./run-clang-tidy.py $ARGS
+	../run-clang-tidy.py $ARGS || TIDY_FAILED=1
 else
-	run-clang-tidy.py $ARGS
+	run-clang-tidy.py $ARGS || TIDY_FAILED=1
 fi
 
-if [ $? -ne 0 ]; then
-	echo "run-clang-tidy.py failed to run"
-	exit 1
-fi
-
-if [ -s $FILE ]; then
-	echo "Found violations, fixes can be found in $FILE"
-	exit 1
+if [ $TIDY_FAILED -eq 0 ]; then
+	echo "clang-tidy: No violations found"
 else
-	echo "No violations found"
+	echo "clang-tidy: Found violations"
 fi
