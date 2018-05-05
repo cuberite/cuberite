647bd9653be1c999eac38a1b1fa687fe3f7e671f
Bond_009 <bond.009@outlook.com>
2018-05-06 01:38:29 +0200

    Fixes

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -6,18 +6,13 @@ FIXES_FILE="tidy-fixes.yaml"
 REGEX=".+/cuberite/src/[^\.].+"
 # TODO: Add -quiet when Circle CI is updated to 2.0 with a newer version of Ubuntu
 ARGS="-header-filter $REGEX -export-fixes $FIXES_FILE "$@" $REGEX"
-TIDY_FAILED=0
 
 mkdir -p tidy-build
 cd tidy-build
 cmake --target Cuberite -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-if [ -f ../run-clang-tidy.py ]; then
-	../run-clang-tidy.py $ARGS || TIDY_FAILED=1
-else
-	run-clang-tidy.py $ARGS || TIDY_FAILED=1
-fi
+run-clang-tidy.py $ARGS || TIDY_FAILED=1
 
-if [ $TIDY_FAILED -eq 0 ]; then
+if run-clang-tidy.py $ARGS; then
 	echo "clang-tidy: No violations found"
 else
 	echo "clang-tidy: Found violations"
