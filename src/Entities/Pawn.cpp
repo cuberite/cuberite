cuberite/cuberite

commit a4f327118b06ced1cd4510b7d20d34da83aa78a3

1.9 / 1.9.2 / 1.9.3 / 1.9.4 protocol support (#3135)
* Semistable update to 15w31a

I'm going through snapshots in a sequential order since it should make things easier, and since protocol version history is written.

* Update to 15w34b protocol

Also, fix an issue with the Entity Equipment packet from the past version.  Clients are able to connect and do stuff!

* Partially update to 15w35e

Chunk data doesn't work, but the client joins.  I'm waiting to do chunk data because chunk data has an incomplete format until 15w36d.

* Add '/blk' debug command

This command lets one see what block they are looking at, and makes figuring out what's supposed to be where in a highly broken chunk possible.

* Fix CRLF normalization in CheckBasicStyle.lua

Normally, this doesn't cause an issue, but when running from cygwin, it detects the CR as whitespace and creates thousands of violations for every single line.  Lua, when run on windows, will normalize automatically, but when run via cygwin, it won't.

The bug was simply that gsub was returning a replaced version, but not changing the parameter, so the replaced version was ignored.

* Update to 15w40b

This includes chunk serialization.  Fully functional chunk serialization for 1.9.

I'm not completely happy with the chunk serialization as-is (correct use of palettes would be great), but cuberite also doesn't skip sending empty chunks so this performance optimization should probably come later.  The creation of a full buffer is suboptimal, but it's the easiest way to implement this code.

* Write long-by-long rather than creating a buffer

This is a bit faster and should be equivalent.  However, the code still doesn't look too good.

* Update to 15w41a protocol

src/Entities/Pawn.cpp
@@ -418,8 +418,9 @@ void cPawn::HandleFalling(void)
 			TakeDamage(dtFalling, nullptr, Damage, Damage, 0);
 
 			// Fall particles
-			int ParticleSize = static_cast<int>((std::min(15, Damage) - 1.f) * ((50.f - 20.f) / (15.f - 1.f)) + 20.f);
-			GetWorld()->BroadcastSoundParticleEffect(EffectID::PARTICLE_FALL_PARTICLES, POSX_TOINT, POSY_TOINT - 1, POSZ_TOINT, ParticleSize);
+			// TODO: Re-enable this when effects in 1.9 aren't broken (right now this uses the wrong effect ID in 1.9 and the right one in 1.8 and 1.7)
+			// int ParticleSize = static_cast<int>((std::min(15, Damage) - 1.f) * ((50.f - 20.f) / (15.f - 1.f)) + 20.f);
+			// GetWorld()->BroadcastSoundParticleEffect(EffectID::PARTICLE_FALL_PARTICLES, POSX_TOINT, POSY_TOINT - 1, POSZ_TOINT, ParticleSize);
 		}
 
 		m_bTouchGround = true;

CONTRIBUTORS
@@ -30,6 +30,7 @@ mtilden
 nesco
 NiLSPACE (formerly STR_Warrior)
 p-mcgowan
+pokechu22
 rs2k
 SamJBarney
 Schwertspize

Server/Plugins/APIDump/APIDesc.lua
@@ -2965,6 +2965,28 @@ end
 			},
 			Constants =
 			{
+				BLOCK_FACE_XM = { Notes = "Interacting with the X- face of the block" },
+				BLOCK_FACE_XP = { Notes = "Interacting with the X+ face of the block" },
+				BLOCK_FACE_YM = { Notes = "Interacting with the Y- face of the block" },
+				BLOCK_FACE_YP = { Notes = "Interacting with the Y+ face of the block" },
+				BLOCK_FACE_ZM = { Notes = "Interacting with the Z- face of the block" },
+				BLOCK_FACE_ZP = { Notes = "Interacting with the Z+ face of the block" },
+				BLOCK_FACE_NONE = { Notes = "Interacting with no block face - swinging the item in the air" },
+				BLOCK_FACE_EAST = { Notes = "(<b>DEPRECATED!</b>) Please use BLOCK_FACE_XM instead. Interacting with the eastern face of the block." },
+				BLOCK_FACE_WEST = { Notes = "(<b>DEPRECATED!</b>) Please use BLOCK_FACE_XP instead. Interacting with the western face of the block." },
+				BLOCK_FACE_BOTTOM = { Notes = "(<b>DEPRECATED!</b>) Please use BLOCK_FACE_YM instead. Interacting with the bottom face of the block." },
+				BLOCK_FACE_TOP = { Notes = "(<b>DEPRECATED!</b>) Please use BLOCK_FACE_YP instead. Interacting with the top face of the block." },
+				BLOCK_FACE_NORTH = { Notes = "(<b>DEPRECATED!</b>) Please use BLOCK_FACE_ZM instead. Interacting with the northern face of the block." },
+				BLOCK_FACE_SOUTH = { Notes = "(<b>DEPRECATED!</b>) Please use BLOCK_FACE_ZP instead. Interacting with the southern face of the block." },
+				BLOCK_FACE_MAX = { Notes = "Used for range checking - highest legal value for an {{Globals#BlockFaces|eBlockFace}}" },
+				BLOCK_FACE_MIN = { Notes = "Used for range checking - lowest legal value for an {{Globals#BlockFaces|eBlockFace}}" },
+				DIG_STATUS_STARTED = { Notes = "The player has started digging" },
+				DIG_STATUS_CANCELLED = { Notes = "The player has let go of the mine block key before finishing mining the block" },
+				DIG_STATUS_FINISHED = { Notes = "The player thinks that it has finished mining a block" },
+				DIG_STATUS_DROP_HELD = { Notes = "The player has dropped a single item using the Drop Item key (default: Q)" },
+				DIG_STATUS_DROP_STACK = { Notes = "The player has dropped a full stack of items using the Drop Item key (default: Q) while holding down a specific modifier key (in windows, control)" },
+				DIG_STATUS_SHOOT_EAT = { Notes = "The player has finished shooting a bow or finished eating" },
+				DIG_STATUS_SWAP_ITEM_IN_HAND = { Notes = "The player has swapped their held item with the item in their offhand slot (1.9)" },
 				esBed = { Notes = "A bed explosion. The SourceData param is the {{Vector3i|position}} of the bed." },
 				esEnderCrystal = { Notes = "An ender crystal entity explosion. The SourceData param is the {{cEntity|ender crystal entity}} object." },
 				esGhastFireball = { Notes = "A ghast fireball explosion. The SourceData param is the {{cGhastFireballEntity|ghast fireball entity}} object." },
@@ -3046,6 +3068,14 @@ end
 					TextBefore = [[
 						These constants are used for specifying the cause of damage to entities. They are used in the
 						{{TakeDamageInfo}} structure, as well as in {{cEntity}}'s damage-related API functions.
+					]],
+				},
+				DigStatuses =
+				{
+					Include = "^DIG_STATUS_.*",
+					TextBefore = [[
+						These constants are used to describe digging statuses, but in reality cover several more cases.
+						They are used with {{OnPlayerLeftClick|HOOK_PLAYER_LEFT_CLICK}}.
 					]],
 				},
 				GameMode =

Server/Plugins/APIDump/Classes/Projectiles.lua
@@ -131,6 +131,7 @@ return
 				GetEntityEffect = { Params = "", Return = "{{cEntityEffect}}", Notes = "Returns the entity effect in this potion" },
 				GetEntityEffectType = { Params = "", Return = "{{cEntityEffect|Entity effect type}}", Notes = "Returns the effect type of this potion" },
 				GetPotionColor = { Params = "", Return = "number", Notes = "Returns the color index of the particles emitted by this potion" },
+				GetItem  = { Params = "", Return = "{{cItem}}", Notes = "Gets the potion item that was thrown." },
 				SetEntityEffect = { Params = "{{cEntityEffect}}", Return = "", Notes = "Sets the entity effect for this potion" },
 				SetEntityEffectType = { Params = "{{cEntityEffect|Entity effect type}}", Return = "", Notes = "Sets the effect type of this potion" },
 				SetPotionColor = { Params = "number", Return = "", Notes = "Sets the color index of the particles for this potion" },

Server/Plugins/Debuggers/Debuggers.lua
@@ -2156,3 +2156,31 @@ end
 
 
 
+
+function HandleBlkCmd(a_Split, a_Player)
+	-- Gets info about the block the player is looking at.
+	local World = a_Player:GetWorld();
+
+	local Callbacks = {
+		OnNextBlock = function(a_BlockX, a_BlockY, a_BlockZ, a_BlockType, a_BlockMeta)
+			if (a_BlockType ~= E_BLOCK_AIR) then
+				a_Player:SendMessage("Block at " .. a_BlockX .. ", " .. a_BlockY .. ", " .. a_BlockZ .. " is " .. a_BlockType .. ":" .. a_BlockMeta)
+				return true;
+			end
+		end
+	};
+	
+	local EyePos = a_Player:GetEyePosition();
+	local LookVector = a_Player:GetLookVector();
+	LookVector:Normalize();
+	
+	local End = EyePos + LookVector * 50;
+	
+	cLineBlockTracer.Trace(World, Callbacks, EyePos.x, EyePos.y, EyePos.z, End.x, End.y, End.z);
+	
+	return true;
+end
+
+
+
+

Server/Plugins/Debuggers/Info.lua
@@ -202,6 +202,12 @@ g_PluginInfo =
 			Handler = HandleRemoveXp,
 			HelpString = "Remove all xp"
 		},
+		["/blk"] =
+		{
+			Permission = "debuggers",
+			Handler = HandleBlkCmd,
+			HelpString = "Gets info about the block you are looking at"
+		},
 	},  -- Commands
 	
 	ConsoleCommands =

src/ByteBuffer.cpp
@@ -1034,3 +1034,21 @@ void cByteBuffer::CheckValid(void) const
 
 
 
+size_t cByteBuffer::GetVarIntSize(UInt32 a_Value)
+{
+	size_t Count = 0;
+
+	do
+	{
+		// If the value cannot be expressed in 7 bits, it needs to take up another byte
+		Count++;
+		a_Value >>= 7;
+	} while (a_Value != 0);
+
+	return Count;
+}
+
+
+
+
+

src/ByteBuffer.h
@@ -129,6 +129,9 @@ public:
 	/** Checks if the internal state is valid (read and write positions in the correct bounds) using ASSERTs */
 	void CheckValid(void) const;
 
+	/** Gets the number of bytes that are needed to represent the given VarInt */
+	static size_t GetVarIntSize(UInt32 a_Value);
+
 protected:
 	char * m_Buffer;
 	size_t m_BufferSize;  // Total size of the ringbuffer

commit 5ccd77955a21fab9553bf8fdb2c9ca4c86c72bfd

Reimplemented fall particles
Regression when 1.9.x support introduced in #3135, originally implemented in #461.

src/Entities/Pawn.cpp
@@ -428,9 +428,14 @@ void cPawn::HandleFalling(void)
 			TakeDamage(dtFalling, nullptr, Damage, Damage, 0);
 
 			// Fall particles
-			// TODO: Re-enable this when effects in 1.9 aren't broken (right now this uses the wrong effect ID in 1.9 and the right one in 1.8)
-			// int ParticleSize = static_cast<int>((std::min(15, Damage) - 1.f) * ((50.f - 20.f) / (15.f - 1.f)) + 20.f);
-			// GetWorld()->BroadcastSoundParticleEffect(EffectID::PARTICLE_FALL_PARTICLES, POSX_TOINT, POSY_TOINT - 1, POSZ_TOINT, ParticleSize);
+			GetWorld()->BroadcastParticleEffect(
+				"blockdust",
+				GetPosition(),
+				{ 0, 0, 0 },
+				(Damage - 1.f) * ((0.3f - 0.1f) / (15.f - 1.f)) + 0.1f,  // Map damage (1 - 15) to particle speed (0.1 - 0.3)
+				static_cast<int>((Damage - 1.f) * ((50.f - 20.f) / (15.f - 1.f)) + 20.f),  // Map damage (1 - 15) to particle quantity (20 - 50)
+				{ { GetWorld()->GetBlock(POS_TOINT - Vector3i(0, 1, 0)), 0 } }
+			);
 		}
 
 		m_bTouchGround = true;