7bc50a332a0cf7e4abe3ead0f741f1b4ff12d36c
Bastien ADAM <buom_01@openmailbox.org>
2019-04-19 21:17:30 +0200

    Now place armor stand when used

--- src/Items/ItemArmorStand.h
+++ src/Items/ItemArmorStand.h
@@ -25,6 +25,8 @@ public:
 		int a_BlockX, int a_BlockY, int a_BlockZ, eBlockFace a_BlockFace
 	) override
 	{
+		// TODO: Should verify that there are no armor stand or other mob before placing
+
 		if (a_BlockFace < 0)
 		{
 			return false;
@@ -32,13 +34,10 @@ public:
 
 		AddFaceDirection(a_BlockX, a_BlockY, a_BlockZ, a_BlockFace);
 
-		if (a_BlockFace == BLOCK_FACE_YM)
-		{
-			a_BlockY--;
-		}
+		double Rotation = FloorC((a_Player->GetYaw() + 22.5f) / 45.0f) * 45.0f - 180.0f;
 
 		if (
-			(a_World->SpawnArmorStand(Vector3d(a_BlockX + 0.5, a_BlockY, a_BlockZ + 0.5)) != cEntity::INVALID_ID))  // Spawning succeeded
+			(a_World->SpawnArmorStand(Vector3d(a_BlockX + 0.5, a_BlockY, a_BlockZ + 0.5), Rotation) != cEntity::INVALID_ID))  // Spawning succeeded
 		{
 			if (!a_Player->IsGameModeCreative())
 			{
