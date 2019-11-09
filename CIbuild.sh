915daca71ccef3ba6507bea3ab4467cbe7e6d5a1
Peter Bell <peterbell10@live.co.uk>
2019-11-09 17:59:02 +0000

    Add ccache to build

--- CIbuild.sh
+++ CIbuild.sh
@@ -6,7 +6,14 @@ export CUBERITE_BUILD_SERIES_NAME="Travis $CC $TRAVIS_CUBERITE_BUILD_TYPE"
 export CUBERITE_BUILD_ID=$TRAVIS_JOB_NUMBER
 export CUBERITE_BUILD_DATETIME=`date`
 
-cmake . -DBUILD_TOOLS=1 -DSELF_TEST=1;
+# Use ccache if available
+if [ `which ccache` ]; then
+	export CCACHE_CPP2=yes
+	CACHE_ARGS="-DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+	echo "Using ccache installed at $(which ccache)"
+fi
+
+cmake . -DBUILD_TOOLS=1 -DSELF_TEST=1 ${CACHE_ARGS};
 
 echo "Building..."
 cmake --build . -- -j 2;
