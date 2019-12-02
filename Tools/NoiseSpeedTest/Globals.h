03cc206969582a4eb6d86f8fdf0b3f22299c2857
Mattes D <github@xoft.cz>
2019-12-02 21:43:42 +0100

    Added Clamp() to NoiseSpeedTest globals.

--- Tools/NoiseSpeedTest/Globals.h
+++ Tools/NoiseSpeedTest/Globals.h
@@ -233,3 +233,10 @@ public:
 
 
 
+
+/** Clamps the value into the specified range. */
+template <typename T>
+T Clamp(T a_Value, T a_Min, T a_Max)
+{
+	return (a_Value < a_Min) ? a_Min : ((a_Value > a_Max) ? a_Max : a_Value);
+}
