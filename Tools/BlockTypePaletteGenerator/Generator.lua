e276988ec744eff0e72e710eda45d8ab31b213fe
Mattes D <github@xoft.cz>
2019-12-20 22:10:24 +0100

    BlockTypePalette: Load from TSV or original reports' JSON.

--- Tools/BlockTypePaletteGenerator/Generator.lua
+++ Tools/BlockTypePaletteGenerator/Generator.lua
@@ -1,14 +1,33 @@
--- lib/lunajson/src/ is not in default Lua package paths
+-- Generator.lua
+
+--[[
+Crafts an intermediate block palette format to be read by Cuberite.
+It processes the blocks.json report file (https://wiki.vg/Data_Generators)
+into a file that can be loaded into a BlockTypePalette (and is to be stored
+as Server/Protocol/<version>/base.btp.txt).
+
+The output format is the regular TSV BlockTypePalette, described in the
+$/src/BlockTypePalette.h file.
+--]]
+
+
+
+
+-- Allow Lua to load libraries in our subfolder:
 package.path = 'lib/lunajson/src/?.lua;' .. package.path;
 
 
+
+
+
+
 --- Prints usage instructions to stdout.
--- If the optional `message` is passed, output is prepended by message _and_
+-- If the optional `aMessage` is passed, output is prepended by message _and_
 -- redirected to stderr.
-function usage(message)
-	if message then
+local function usage(aMessage)
+	if aMessage then
 		io.output(io.stderr);
-		io.write(message, "\n\n");
+		io.write(aMessage, "\n\n");
 	end
 	io.write(
 		"Usage: lua Generator.lua INPUTFILE OUTPUTFILE\n"..
@@ -22,70 +41,125 @@ function usage(message)
 end
 
 
--- Test whether the script is run in a path where it can load it's libraries
-if not pcall(function() require("lunajson.decoder") end) then
-	usage("Could not load required libraries, please run `Generator.lua` "..
-		"within its directory and make sure to run `git submodule update`.");
+
+
+
+--- Parses the JSON registry into a Lua table
+--[[ The returned array-table has the following format:
+{
+	{ id = 1, blockTypeName = "minecraft:stone", properties = {key = value, ...} },
+	...
+}
+--]]
+local function parseRegistry(aBlockRegistryJsonStr)
+	assert(type(aBlockRegistryJsonStr) == "string")
+
+	local lj = require("lunajson")
+	local input = lj.decode(aBlockRegistryJsonStr)
+	local registry = {}
+	local idx = 1
+	for blockTypeName, blockData in pairs(input) do
+		for _, state in pairs(blockData.states) do
+			registry[idx] = {
+				id = state.id,
+				blockTypeName = blockTypeName,
+				properties = state.properties,
+			}
+			idx = idx + 1
+		end
+	end
+	return registry
 end
 
+
+
+
+
+--- Serializes the properties from the JSON / array table format into a single output string
+-- Concatenates all properties with \t as the delimiting character
+local function serializeProperties(aProperties)
+	local res = {}
+	local idx = 1
+	for k, v in pairs(aProperties or {}) do
+		res[idx] = k
+		res[idx + 1] = v
+		idx = idx + 2
+	end
+	return table.concat(res, "\t")
+end
+
+
+
+
+
+--- Returns the prefix that is common for all block type names in the registry
+-- aRegistry is the parsed registry, as returned from parseRegistry()
+local function findCommonPrefix(aRegistryTable)
+	local prefix = aRegistryTable[1].blockTypeName
+	local len = string.len(prefix)
+	local sub = string.sub
+	for _, block in ipairs(aRegistryTable) do
+		while (sub(block.blockTypeName, 1, len) ~= prefix) do
+			len = len - 1
+			if (len == 0) then
+				return ""
+			end
+			prefix = sub(prefix, 1, len)
+		end
+	end
+	return prefix
+end
+
+
+
+
+
+-- Test whether the script is run in a path where it can load it's libraries
+if not(pcall(function() require("lunajson") end)) then
+	usage(
+		"Could not load required libraries, please run `Generator.lua` " ..
+		"within its directory and make sure to run `git submodule update`."
+	)
+end
 
 -- Check/Prepare CLI arguments
 local inpath, outpath = ...;
-io.input(io.stdin);
-io.output(io.stdout);
-
-if select("#", ...) ~= 2 then
-	usage("Incorrect number of arguments.");
+inpath = inpath or "blocks.json"
+outpath = outpath or "base.btp.txt"
+if (inpath ~= "-") then
+	local handle, err = io.open(inpath, "r")
+	io.input(handle or usage(err))
+end
+if (outpath ~= "-") then
+	local handle, err = io.open(outpath, "w")
+	io.output(handle or usage(err))
 end
 
-if inpath ~= "-" then
-	local handle, err = io.open(inpath, "r");
-	io.input(handle or usage(err));
-end
+-- Parse the registry:
+local registry = parseRegistry(io.input():read("*a"))
+local commonPrefix = findCommonPrefix(registry)
 
-if outpath ~= "-" then
-	local handle, err = io.open(outpath, "w");
-	io.output(handle or usage(err));
-end
-
-
--- Main program starts here
-local decode = (require("lunajson.decoder"))();
-local encode = (require("lunajson.encoder"))();
-
-local input = decode(io.input():read("*a"));
-local registry = {};
-local max_id = -1;
-
-
-for blockname, blockdata in pairs(input) do
-	for i = 1, #(blockdata.states or {}) do
-		local state = blockdata.states[i];
-		assert(registry[state.id + 1] == nil, "Ensure no duplicate IDs");
-
-		-- needed in the end to verify we got no holes in the array:
-		max_id = math.max(max_id, state.id);
-
-		registry[state.id + 1] = {
-			id = assert(state.id, "id is required."),
-			name = assert(blockname, "Block type name is required."),
-			-- default = state.default or nil,  -- may need this later
-			props = state.properties,
-		};
+-- Sort the entries:
+table.sort(registry,
+	function (entry1, entry2)
+		return (entry1.id < entry2.id)
 	end
+)
+
+-- Write out the output format:
+io.write("BlockTypePalette\n")
+io.write("FileVersion\t1\n")
+io.write("CommonPrefix\t", commonPrefix, "\n")
+io.write("\n")
+local prefixLen = string.len(commonPrefix) + 1
+for _, entry in ipairs(registry) do
+	local props = serializeProperties(entry.properties)
+	if (props ~= "") then
+		props = "\t" .. props
+	end
+	io.write(
+		entry.id, "\t",
+		string.sub(entry.blockTypeName, prefixLen),
+		props, "\n"
+	)
 end
-
-
--- The following assertion is not necessary by the current spec, but is required
--- by how lunajson distinguishes objects from arrays. Also if this fails, it is
--- _very_ likely that the input file is faulty.
-assert(#registry == max_id + 1, "Ensure that registry has contiguous keys");
-
-local out = {
-	Metadata = {
-		ProtocolBlockTypePaletteVersion = 1
-	},
-	Palette = registry
-};
-
-io.write(encode(out), "\n");
--- src/BlockTypePalette.cpp
+++ src/BlockTypePalette.cpp
@@ -123,7 +123,18 @@ std::map<UInt32, UInt32> BlockTypePalette::createTransformMapWithFallback(const 
 
 void BlockTypePalette::loadFromString(const AString & aString)
 {
-	// TODO: Detect format (Json vs Lua)
+	static const AString hdrTsvRegular = "BlockTypePalette";
+	static const AString hdrTsvUpgrade = "UpgradeBlockTypePalette";
+
+	// Detect format by checking the header line (none -> JSON):
+	if (aString.substr(0, hdrTsvRegular.length()) == hdrTsvRegular)
+	{
+		return loadFromTsv(aString, false);
+	}
+	else if (aString.substr(0, hdrTsvUpgrade.length()) == hdrTsvUpgrade)
+	{
+		return loadFromTsv(aString, true);
+	}
 	return loadFromJsonString(aString);
 }
 
@@ -143,55 +154,144 @@ void BlockTypePalette::loadFromJsonString(const AString & aJsonPalette)
 		throw LoadFailedException(errs);
 	}
 
-	// Check the JSON's metadata + version:
-	if (!root.isObject() ||
-		!root.isMember("Metadata") ||
-		!root["Metadata"].isMember("ProtocolBlockTypePaletteVersion") ||
-		!root.isMember("Palette") ||
-		!root["Palette"].isArray())
+	// Sanity-check the JSON's structure:
+	if (!root.isObject())
 	{
-		throw LoadFailedException("Incorrect palette format, wrong or missing metadata.");
-	}
-	auto version = root["Metadata"]["ProtocolBlockTypePaletteVersion"].asUInt();
-	if (version != 1)
-	{
-		throw(Printf("Palette format version %d not supported.", version));
+		throw LoadFailedException("Incorrect palette format, expected an object at root.");
 	}
 
 	// Load the palette:
-	auto len = root["Palette"].size();
-	for (decltype(len) i = 0; i < len; ++i)
+	for (auto itr = root.begin(), end = root.end(); itr != end; ++itr)
 	{
-		const auto & record = root["Palette"][i];
-		if (!record.isObject())
+		const auto & blockTypeName = itr.name();
+		const auto & states = (*itr)["states"];
+		if (states == Json::Value())
 		{
-			throw LoadFailedException(Printf("Palette record #%u is not a JSON object.", i));
+			throw LoadFailedException(Printf("Missing \"states\" for block type \"%s\"", blockTypeName));
 		}
+		for (const auto & state: states)
+		{
+			auto id = static_cast<UInt32>(std::stoul(state["id"].asString()));
+			std::map<AString, AString> props;
+			if (state.isMember("properties"))
+			{
+				const auto & properties = state["properties"];
+				if (!properties.isObject())
+				{
+					throw LoadFailedException(Printf("Member \"properties\" is not a JSON object (block type \"%s\", id %u).", blockTypeName, id));
+				}
+				for (const auto & key: properties.getMemberNames())
+				{
+					props[key] = properties[key].asString();
+				}
+			}
+			addMapping(id, blockTypeName, props);
+		}
+	}
+}
 
-		auto blockTypeName = record["name"].asString();
-		auto id = static_cast<UInt32>(std::stoul(record["id"].asString()));
+
+
+
+
+void BlockTypePalette::loadFromTsv(const AString & aTsvPalette, bool aIsUpgrade)
+{
+	auto lines = StringSplitAndTrim(aTsvPalette, "\n");
+
+	// Parse the header:
+	int fileVersion = 0;
+	AString commonPrefix;
+	auto numLines = lines.size();
+	for (size_t idx = 1; idx < numLines; ++idx)
+	{
+		const auto & line = lines[idx];
+		if (line.empty())
+		{
+			// End of headers, erase them from lines[] and go parse the data
+			lines.erase(lines.begin(), lines.begin() + static_cast<AStringVector::difference_type>(idx) + 1);
+			break;
+		}
+		auto s = StringSplit(line, "\t");
+		if (s.size() != 2)
+		{
+			throw LoadFailedException(Printf("Invalid header format on line %u", idx + 1));
+		}
+		if (s[0] == "FileVersion")
+		{
+			try
+			{
+				fileVersion = std::stoi(s[1]);
+			}
+			catch (const std::exception & exc)
+			{
+				throw LoadFailedException(Printf("Invalid file version: \"%d\" (%s)", s[1], exc.what()));
+			}
+		}
+		else if (s[0] == "CommonPrefix")
+		{
+			commonPrefix = s[1];
+		}
+	}
+	if (fileVersion != 1)
+	{
+		throw LoadFailedException(Printf("Unknown file version (%d), only version 1 is supported", fileVersion));
+	}
+
+	// Parse the data:
+	size_t minSplit = aIsUpgrade ? 3 : 2;
+	for (const auto & line: lines)
+	{
+		auto s = StringSplit(line, "\t");
+		auto numSplit = s.size();
+		if (numSplit < minSplit)
+		{
+			throw LoadFailedException(Printf("Not enough values on data line: \"%s\"", line));
+		}
+		UInt32 id;
+		try
+		{
+			id = static_cast<UInt32>(std::stoi(s[0]));
+		}
+		catch (const std::exception & exc)
+		{
+			throw LoadFailedException(Printf("Invalid block ID: \"%s\" (%s)", s[0], exc.what()));
+		}
+		size_t idx = 1;
+		if (aIsUpgrade)
+		{
+			id = id * 16;
+			try
+			{
+				id = id + static_cast<UInt32>(Clamp(std::stoi(s[1]), 0, 15));
+			}
+			catch (const std::exception & exc)
+			{
+				throw LoadFailedException(Printf("Invalid block meta: \"%s\" (%s)", s[1], exc.what()));
+			}
+			idx = 2;
+		}
+		const auto & blockTypeName = s[idx];
+		idx += 1;
 		std::map<AString, AString> state;
-
-		if (record.isMember("props"))
+		while (idx + 1 < numSplit)
 		{
-			const auto & props = record["props"];
-			if (!props.isObject())
-			{
-				throw LoadFailedException(Printf("Palette record #%u: \"props\" value is not a JSON object.", i));
-			}
-			for (const auto & key: props.getMemberNames())
-			{
-				state[key] = props[key].asString();
-			}
+			state[s[idx]] = s[idx + 1];
+			idx += 2;
 		}
-		BlockState blockState(state);
+		addMapping(id, commonPrefix + blockTypeName, state);
+	}
+}
 
-		// Insert / update in the maps:
-		mNumberToBlock[id] = {blockTypeName, blockState};
-		mBlockToNumber[blockTypeName][blockState] = id;
-		if (id > mMaxIndex)
-		{
-			mMaxIndex = id;
-		}
-	}  // for i - Palette[]
+
+
+
+
+void BlockTypePalette::addMapping(UInt32 aID, const AString & aBlockTypeName, const BlockState & aBlockState)
+{
+	mNumberToBlock[aID] = {aBlockTypeName, aBlockState};
+	mBlockToNumber[aBlockTypeName][aBlockState] = aID;
+	if (aID > mMaxIndex)
+	{
+		mMaxIndex = aID;
+	}
 }
