8d085966ada263f4346e9589dd5837dd8c8a0dd9
Mat <mail@mathias.is>
2020-03-19 22:41:50 +0200

    CI test

--- src/World.cpp
+++ src/World.cpp
@@ -1408,7 +1408,7 @@ void cWorld::DoExplosionAt(double a_ExplosionSize, double a_BlockX, double a_Blo
 	cVector3iArray BlocksAffected;
 	m_ChunkMap->DoExplosionAt(a_ExplosionSize, a_BlockX, a_BlockY, a_BlockZ, BlocksAffected);
 
-	float SoundPitchMultiplier = 1.0f + (Random.RandReal(1.0f) - Random.RandReal(1.0f)) * 0.2f;
+	auto SoundPitchMultiplier = 1.0f + (Random.RandReal(1.0f) - Random.RandReal(1.0f)) * 0.2f;
 	BroadcastSoundEffect("entity.generic.explode", Vector3d(a_BlockX, a_BlockY, a_BlockZ), 4.0f, SoundPitchMultiplier * 0.7f);
 
 	{
@@ -1425,16 +1425,17 @@ void cWorld::DoExplosionAt(double a_ExplosionSize, double a_BlockX, double a_Blo
 		}
 	}
 
-	float ParticleFormula = static_cast<float>(a_ExplosionSize) * 0.33f;
-	float Spread = ParticleFormula * 0.5f;
-	float ParticleCount = std::min((ParticleFormula * 125), 600.0f);
+	auto Position = Vector3d(a_BlockX, a_BlockY - 0.5f, a_BlockZ);
+	auto ParticleFormula = a_ExplosionSize * 0.33f;
+	auto Spread = ParticleFormula * 0.5f;
+	auto ParticleCount = std::min((ParticleFormula * 125), 600.0);
 
-	BroadcastParticleEffect("largesmoke", Vector3f(a_BlockX, a_BlockY - 0.5f, a_BlockZ), Vector3f{}, Spread, ParticleCount);
+	BroadcastParticleEffect("largesmoke", Position, Vector3f{}, Spread, ParticleCount);
 
 	Spread = ParticleFormula * 0.35f;
-	ParticleCount = std::min((ParticleFormula * 550), 1800.0f);
+	ParticleCount = std::min((ParticleFormula * 125), 1800.0);
 
-	BroadcastParticleEffect("explode", Vector3f(a_BlockX, a_BlockY - 0.5f, a_BlockZ), Vector3f{}, Spread, ParticleCount);
+	BroadcastParticleEffect("explode", Position, Vector3f{}, Spread, ParticleCount);
 
 	cPluginManager::Get()->CallHookExploded(*this, a_ExplosionSize, a_CanCauseFire, a_BlockX, a_BlockY, a_BlockZ, a_Source, a_SourceData);
 }
