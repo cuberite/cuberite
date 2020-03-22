3d5a29542374c92bfcaa134d0b3a54ea8bc5dee0
Mat <mail@mathias.is>
2020-03-22 02:18:49 +0200

    Ender chest is not affected by piston (#4526)

--- src/Blocks/BlockPiston.h
+++ src/Blocks/BlockPiston.h
@@ -141,7 +141,7 @@ private:
 			case E_BLOCK_END_GATEWAY:
 			case E_BLOCK_END_PORTAL:
 			case E_BLOCK_END_PORTAL_FRAME:
-			// Notice the lack of an E_BLOCK_ENDER_CHEST here; its because ender chests can totally be pushed / pulled in MCS :)
+			case E_BLOCK_ENDER_CHEST:
 			case E_BLOCK_FURNACE:
 			case E_BLOCK_LIT_FURNACE:
 			case E_BLOCK_INVERTED_DAYLIGHT_SENSOR:
