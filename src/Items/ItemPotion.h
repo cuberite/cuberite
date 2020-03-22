076749bd3638becf41a2627370fd6853508b333b
Mat <mail@mathias.is>
2020-03-22 17:44:24 +0200

    Increase speed of splash potion and expbottle (#4513)

--- src/Items/ItemPotion.h
+++ src/Items/ItemPotion.h
@@ -40,7 +40,10 @@ public:
 		}
 
 		Vector3d Pos = a_Player->GetThrowStartPos();
-		Vector3d Speed = a_Player->GetLookVector() * 7;
+		Vector3d Speed = a_Player->GetLookVector() * 14;
+
+		// Play sound
+		a_World->BroadcastSoundEffect("entity.arrow.shoot", a_Player->GetPosition() - Vector3d(0, a_Player->GetHeight(), 0), 0.5f, 0.4f / GetRandomProvider().RandReal(0.8f, 1.2f));
 
 		if (a_World->CreateProjectile(Pos.x, Pos.y, Pos.z, cProjectileEntity::pkSplashPotion, a_Player, &a_Player->GetEquippedItem(), &Speed) == cEntity::INVALID_ID)
 		{
