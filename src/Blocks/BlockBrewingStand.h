3145c13a07b4fd57a389c64b6e33bd4377eb933d
Mat <mail@mathias.is>
2020-03-23 15:03:32 +0200

    Fix brewing stand drop

--- src/Blocks/BlockBrewingStand.h
+++ src/Blocks/BlockBrewingStand.h
@@ -1,7 +1,7 @@
 
 #pragma once
 
-#include "BlockEntity.h"
+#include "../BlockEntities/BrewingstandEntity.h"
 #include "Mixins.h"
 
 
@@ -9,15 +9,30 @@
 
 
 class cBlockBrewingStandHandler :
-	public cMetaRotator<cContainerEntityHandler<cBlockEntityHandler>, 0x07, 0x02, 0x05, 0x03, 0x04>
+	public cMetaRotator<cBlockEntityHandler, 0x07, 0x02, 0x05, 0x03, 0x04>
 {
-	using super = cMetaRotator<cContainerEntityHandler<cBlockEntityHandler>, 0x07, 0x02, 0x05, 0x03, 0x04>;
+	using super = cMetaRotator<cBlockEntityHandler, 0x07, 0x02, 0x05, 0x03, 0x04>;
 
 public:
 
 	cBlockBrewingStandHandler(BLOCKTYPE a_BlockType):
 		super(a_BlockType)
 	{
+	}
+
+
+
+
+
+	virtual cItems ConvertToPickups(NIBBLETYPE a_BlockMeta, cBlockEntity * a_BlockEntity, const cEntity * a_Digger, const cItem * a_Tool) override
+	{
+		cItems res(cItem(E_ITEM_BREWING_STAND, 1)); // We have to drop the item form of a brewing stand
+		if (a_BlockEntity != nullptr)
+		{
+			auto be = static_cast<cBrewingstandEntity *>(a_BlockEntity);
+			res.AddItemGrid(be->GetContents());
+		}
+		return res;
 	}
 
 
