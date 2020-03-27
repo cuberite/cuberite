535d29e4df3aba3b9b9456699ef9d8f9f672e128
mathiascode <mail@mathias.is>
2020-03-27 20:34:16 +0200

    Revert another change

--- src/Mobs/Monster.cpp
+++ src/Mobs/Monster.cpp
@@ -1095,10 +1095,10 @@ int cMonster::GetSpawnDelay(cMonster::eFamily a_MobFamily)
 {
 	switch (a_MobFamily)
 	{
-		case mfHostile:   return 80;
+		case mfHostile:   return 40;
 		case mfPassive:   return 40;
 		case mfAmbient:   return 40;
-		case mfWater:     return 100;
+		case mfWater:     return 400;
 		case mfNoSpawn:   return -1;
 		default:
 		{
