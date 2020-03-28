276da7e39830f07fa73fb9a4c4497fce42a7ed23
IKDC <ikdc.services@gmail.com>
2020-03-28 10:51:55 +0100

    Updated src\Blocks\BlockBookShelf.h again
    From madmaxoft requests

--- src/Blocks/BlockBookShelf.h
+++ src/Blocks/BlockBookShelf.h
@@ -10,11 +10,11 @@ class cBlockBookShelfHandler :
 	public cBlockHandler
 {
 	public:
-		cBlockBookShelfHandler(BLOCKTYPE a_BlockType)
-			: cBlockHandler(a_BlockType)
+		cBlockBookShelfHandler(BLOCKTYPE a_BlockType) :
+			cBlockHandler(a_BlockType)
 		{
 		}
-		
+
 		virtual cItems ConvertToPickups(NIBBLETYPE a_BlockMeta, cBlockEntity * a_BlockEntity, const cEntity * a_Digger, const cItem * a_Tool) override
 		{
 			if ((ItemCategory::IsTool(a_Tool->m_ItemType)) && (ToolHasSilkTouch(a_Tool)))
