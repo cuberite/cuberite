e6e258ad79e90bf091393d8e78c156f70f7cfbe8
GefaketHD <JoBad@online.de>
2020-04-11 21:01:25 +0200

    pressure plates make sounds and shall act better

--- src/Simulator/IncrementalRedstoneSimulator/PressurePlateHandler.h
+++ src/Simulator/IncrementalRedstoneSimulator/PressurePlateHandler.h
@@ -72,15 +72,101 @@ public:
 		UNUSED(a_PoweringData.PowerLevel);
 		// LOGD("Evaluating clicky the pressure plate (%d %d %d)", a_Position.x, a_Position.y, a_Position.z);
 
-		auto Power = GetPowerLevel(a_World, a_Position, a_BlockType, a_Meta);
-		auto PreviousPower = static_cast<cIncrementalRedstoneSimulator *>(a_World.GetRedstoneSimulator())->GetChunkData()->ExchangeUpdateOncePowerData(a_Position, PoweringData(a_BlockType, Power));
+		auto Power = GetPowerLevel(a_World, a_Position, a_BlockType, a_Meta);  // Get the current power of the plate
+		cIncrementalRedstoneSimulatorChunkData *ChunkData = static_cast<cIncrementalRedstoneSimulator *>(a_World.GetRedstoneSimulator())->GetChunkData();
+		auto PreviousPower = ChunkData->GetCachedPowerData(a_Position);
+		auto PreviousPlateState = ChunkData->GetCachedPressurePlateState(a_Position);
+
+		auto NextPressurePlateState = E_PRESSURE_PLATE_RAISED;
+
+		if (Power != 0) && (PreviousPlateState == E_PRESSURE_PLATE_RAISED)  // Plate is pressed initially
+		{
+			NextPressurePlateState = E_PRESSURE_PLATE_INITIALLY_PRESSED;
+
+			ChunkData->SetCachedPressurePlateState(a_Position, NextPressurePlateState);
+			/* a_World.ScheduleTask(20, [a_Position](cWorld & a_World)
+			{
+				static_cast<cIncrementalRedstoneSimulator *>(a_World.GetRedstoneSimulator())->GetChunkData()->SetCachedPressurePlateState(a_Position, E_PRESSURE_PLATE_WANTS_TO_RELEASE);
+			}); */
+
+			// manage on-sounds
+			AString soundToPlay = "";
+			switch (a_BlockType)
+			{
+				case E_BLOCK_STONE_PRESSURE_PLATE:
+					soundToPlay = "block.wood_pressureplate.click_on";
+					break;
+				case E_BLOCK_WOODEN_PRESSURE_PLATE:
+					soundToPlay = "block.wood_pressureplate.click_on";
+					break;
+				case E_BLOCK_HEAVY_WEIGHTED_PRESSURE_PLATE:
+				case E_BLOCK_LIGHT_WEIGHTED_PRESSURE_PLATE:
+					soundToPlay = "block.metal_pressureplate.click_on";
+					break;
+				default:
+				{
+					ASSERT(!"Unhandled/unimplemented block in pressure plate handler!");
+					return {};
+				}
+			}
+			a_World.BroadcastSoundEffect(soundToPlay, a_Position, 0.5f, 0.5f);
+		}
+
+		if (Power != 0) && (PreviousPlateState == E_PRESSURE_PLATE_WANTS_TO_RELEASE)  // Plate is still pressed, even though it COULD release now
+		{
+			NextPressurePlateState = E_PRESSURE_PLATE_HELD_DOWN;
+		}
+
+		if (Power == 0) && (PreviousPlateState == E_PRESSURE_PLATE_HELD_DOWN)  // Schedule release
+		{
+			/* a_World.ScheduleTask(10, [a_Position](cWorld & a_World)
+			{
+				static_cast<cIncrementalRedstoneSimulator *>(a_World.GetRedstoneSimulator())->GetChunkData()->SetCachedPressurePlateState(a_Position, E_PRESSURE_PLATE_WANTS_TO_RELEASE);
+			}); */
+		}
+
+		if (Power == 0) && (PreviousPlateState == E_PRESSURE_PLATE_WANTS_TO_RELEASE)
+		{
+			NextPressurePlateState = E_PRESSURE_PLATE_RAISED;
+
+			// manage off-sounds
+			AString soundToPlay = "";
+			switch (a_BlockType)
+			{
+				case E_BLOCK_STONE_PRESSURE_PLATE:
+					soundToPlay = "block.wood_pressureplate.click_off";
+					break;
+				case E_BLOCK_WOODEN_PRESSURE_PLATE:
+					soundToPlay = "block.wood_pressureplate.click_off";
+					break;
+				case E_BLOCK_HEAVY_WEIGHTED_PRESSURE_PLATE:
+				case E_BLOCK_LIGHT_WEIGHTED_PRESSURE_PLATE:
+					soundToPlay = "block.metal_pressureplate.click_off";
+					break;
+				default:
+				{
+					ASSERT(!"Unhandled/unimplemented block in pressure plate handler!");
+					return {};
+				}
+			}
+			a_World.BroadcastSoundEffect(soundToPlay, a_Position, 0.5f, 0.5f);
+		}
+
+		if (NextPressurePlateState != E_PRESSURE_PLATE_RAISED) && (a_Meta != E_META_PRESSURE_PLATE_DEPRESSED)
+		{
+			a_World.SetBlockMeta(a_Position, E_META_PRESSURE_PLATE_DEPRESSED);
+		}
+
 
 		if (Power != PreviousPower.PowerLevel)
 		{
-			a_World.SetBlockMeta(a_Position, (Power == 0) ? E_META_PRESSURE_PLATE_RAISED : E_META_PRESSURE_PLATE_DEPRESSED);
 			return GetAdjustedRelatives(a_Position, StaticAppend(GetRelativeLaterals(), cVector3iArray{ OffsetYM() }));
 		}
 
+		if (NextPressurePlateState == E_PRESSURE_PLATE_RAISED) && (a_Meta != E_META_PRESSURE_PLATE_RAISED)
+		{
+			a_World.SetBlockMeta(a_Position, E_META_PRESSURE_PLATE_RAISED);
+		}
 		return {};
 	}
 
--- src/Simulator/IncrementalRedstoneSimulator/RedstoneSimulatorChunkData.h
+++ src/Simulator/IncrementalRedstoneSimulator/RedstoneSimulatorChunkData.h
@@ -23,6 +23,17 @@ public:
 		return m_ActiveBlocks;
 	}
 
+	cRedstoneHandler::ENUM_PRESSURE_PLATE_STATE GetCachedPressurePlateState(const Vector3i & a_Position)
+	{
+		auto Result = m_CachedPressurePlateStates.find(a_Position);
+		return (Result == m_CachedPressurePlateStates.end()) ? cRedstoneHandler::E_PRESSURE_PLATE_RAISED : Result->second;
+	}
+
+	void SetCachedPressurePlateState(const Vector3i & a_Position, cRedstoneHandler::ENUM_PRESSURE_PLATE_STATE a_PressurePlateState)
+	{
+		m_CachedPressurePlateStates[a_Position] = a_PressurePlateState;
+	}
+
 	const cRedstoneHandler::PoweringData GetCachedPowerData(const Vector3i & a_Position) const
 	{
 		auto Result = m_CachedPowerLevels.find(a_Position);
@@ -67,6 +78,7 @@ private:
 
 	// TODO: map<Vector3i, int> -> Position of torch + it's heat level
 
+	std::unordered_map<Vector3i, cRedstoneHandler::ENUM_PRESSURE_PLATE_STATE, VectorHasher<int>> m_CachedPressurePlateStates;
 	std::unordered_map<Vector3i, cRedstoneHandler::PoweringData, VectorHasher<int>> m_CachedPowerLevels;
 
 	friend class cRedstoneHandlerFactory;
