43525c5df8ec537d8ce83278795962a3af09f858
Peter Bell <peterbell10@live.co.uk>
2020-04-12 23:04:30 +0100

    Acquire chunk map CS when accessing player entities

--- src/World.h
+++ src/World.h
@@ -1205,7 +1205,7 @@ private:
 	std::unique_ptr<cFireSimulator>      m_FireSimulator;
 	cRedstoneSimulator * m_RedstoneSimulator;
 
-	cCriticalSection m_CSPlayers;
+	// Protect with chunk map CS
 	cPlayerList      m_Players;
 
 	cWorldStorage    m_Storage;
