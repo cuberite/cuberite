31788a7fae6c5425a6e06f29fdd0c5e6a6e74c19
12xx12 <12xx12100@gmail.com>
2020-08-02 23:08:28 +0200

    added to item handler

--- src/Items/ItemBanner.h
+++ src/Items/ItemBanner.h
@@ -44,14 +44,8 @@ class cItemBannerHandler: public cItemHandler
 	) override
 	{
 		/*
-			may either be placed on top of a block or on the side (may overlap)
 			may have 16 colors
-			has no collisions
-			hitbox is one block tall
 			can be placed in item frame - just shows the item model
-			water and lava flow around banners (water goes through the bottom part if placed onto a wall)
-			lava can spawn fire blocks next to the banner but doesn't burn the banner
-			cannot be moved by pistons
 			If renamed retains its name on adding/removing pattern
 			keeps its name on building/breaking
 
@@ -59,20 +53,6 @@ class cItemBannerHandler: public cItemHandler
 			may be crated together with shield to display the banner
 
 			can be used for 1.5 items as fuel
-
-		 	sounds:
-		 		Description                            |sound             | Volume | Pitch |dist
-		 		Breaking the block                      block.wood.break    1.0     0.8     16
-				Falling on the block with fall damage   block.wood.fall	    0.5     0.75    16
-				Mining the block                        block.wood.hit      0.25    0.5     16
-				Placing the block                       block.wood.place    1.0     0.8     16
-				Walking on the block                    block.wood.step	    0.15    1.0     16
-
-			Block data:
-		 		Standing:
-		 			direction
-				Wall:
-					direction
 		*/
 
 		if (a_ClickedBlockFace == BLOCK_FACE_BOTTOM)
