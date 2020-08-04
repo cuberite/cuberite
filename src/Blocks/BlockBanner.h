0760c91aae9a7f6e0f0e355120389fc47ca44a78
12xx12 <12xx12100@gmail.com>
2020-08-04 22:35:17 +0200

    fixed bad check

--- src/Blocks/BlockBanner.h
+++ src/Blocks/BlockBanner.h
@@ -62,14 +62,19 @@ public:
 		const cItem * a_Tool
 	) override
 	{
-		if ((a_BlockEntity == nullptr) || (a_BlockEntity->GetBlockType() != E_BLOCK_STANDING_BANNER) ||(a_BlockEntity->GetBlockType() != E_BLOCK_WALL_BANNER))
+		LOG("Destroying Banner queued");
+		LOG(std::to_string(a_BlockEntity->GetBlockType()));
+		LOG(std::to_string(E_BLOCK_STANDING_BANNER));
+		LOG(std::to_string(E_BLOCK_WALL_BANNER));
+		if ((a_BlockEntity == nullptr) || ((a_BlockEntity->GetBlockType() != E_BLOCK_STANDING_BANNER) && (a_BlockEntity->GetBlockType() != E_BLOCK_WALL_BANNER)))
 		{
 			return {};
 		}
+		LOG("Destroying Banner");
 		// Todo: transfer cBannerPatternContainer back to the item
 		auto BannerEntity = static_cast<cBannerEntity *>(a_BlockEntity);
 		NIBBLETYPE BlockMeta = static_cast<NIBBLETYPE>(BannerEntity->GetBaseColor());
-		return cItem(E_ITEM_BANNER, 1, BlockMeta);
+		return cItem(BannerEntity->GetBlockType(), 1, BlockMeta);
 	}
 
 
