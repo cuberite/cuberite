64e06a4b84bbf526b47fe3bbaf771807061f3316
12xx12 <12xx12100@gmail.com>
2020-08-28 20:36:50 +0200

    added loot table lookup for mobs and chests

--- src/LootTableProvider.cpp
+++ src/LootTableProvider.cpp
@@ -2,18 +2,66 @@
 // cLootTableProvider.cpp
 
 #include "LootTableProvider.h"
-// #include "Registries/LootTables.cpp"
+// #include "Registries/LootTables.h"
 
-cLootTableProvider::cLootTableProvider()
+cLootTableProvider::cLootTableProvider(cWorld * a_World)
 {
+	m_World = a_World;
+	auto Path = m_World->GetDataPath();
+	// Check for custom tables
 
+	// Load if available
+}
+
+
+
+
+/*
+Format:    Type|FurtherInfo
+example:   Chest|AbandonedMineshaft
+example:   Monster|Skeleton
+example:   Block|Stone
+This is not case sensitive, and removes all spaces
+*/
+std::shared_ptr<cLootTable> cLootTableProvider::GetLootTable(const AString & a_Name) const
+{
+	auto Data = StringSplitAndTrim(a_Name,"|");
+
+	if ((Data.size() > 0) || (Data.size() > 2))
+	{
+		LOGWARNING("Got ill formatted string to look up a loot table. Please use Type|Subtype");
+		return std::make_shared<cLootTable>(cEmptyLootTable());
+	}
+
+	auto Type = LootTable::eType(Data[0]);
+
+	switch (Type)
+	{
+		case LootTable::eType::Chest:
+		{
+			auto ChestType = LootTable::eChestType(Data[1]);
+			return GetLootTable(ChestType);
+		}
+		case LootTable::eType::Entity:
+		{
+			auto MobType = LootTable::eMonsterType(Data[1]);
+			return GetLootTable(MobType);
+		}
+		case LootTable::eType::Block:
+		{
+			// TODO: add block meta
+			const BLOCKTYPE BlockType = static_cast<ENUM_BLOCK_TYPE>(BlockStringToType(Data[1]));
+			return GetLootTable(BlockType);
+		}
+		default: return std::make_shared<cLootTable>(cEmptyLootTable());
+	}
 }
 
 
 
 
 
-std::shared_ptr<cLootTable> cLootTableProvider::GetLootTableFromString(AString a_Name)
+std::shared_ptr<cLootTable> cLootTableProvider::GetLootTable(const BLOCKTYPE a_Block) const
 {
 	return nullptr;
 }
@@ -22,7 +70,7 @@ std::shared_ptr<cLootTable> cLootTableProvider::GetLootTableFromString(AString a
 
 
 
-std::shared_ptr<cLootTable> cLootTableProvider::GetLootTableFromBlock()
+std::shared_ptr<cLootTable> cLootTableProvider::GetLootTable(const BLOCKTYPE a_Block, const ENUM_BLOCK_META) const
 {
 	return nullptr;
 }
@@ -31,8 +79,75 @@ std::shared_ptr<cLootTable> cLootTableProvider::GetLootTableFromBlock()
 
 
 
-std::shared_ptr<cLootTable> cLootTableProvider::GetLooTableFromMob(eMonsterType a_Type)
+std::shared_ptr<cLootTable> cLootTableProvider::GetLootTable(const enum LootTable::eChestType a_Type) const
 {
+	// Checks if there's a custom Loot table available for this type else returns the default one
+	auto CustomTable = m_CustomChestLootTables.find(a_Type);
+
+	if (CustomTable != m_CustomChestLootTables.end())
+	{
+		return CustomTable->second;
+	}
+
+	switch (a_Type)
+	{
+		case LootTable::eChestType::AbandonedMineshaft:     return std::make_shared<cLootTable>();
+		case LootTable::eChestType::BuriedTreasure:         return std::make_shared<cLootTable>();
+		case LootTable::eChestType::DesertPyramid:          return std::make_shared<cLootTable>();
+		case LootTable::eChestType::EndCityTreasure:        return std::make_shared<cLootTable>();
+		case LootTable::eChestType::IglooChest:             return std::make_shared<cLootTable>();
+		case LootTable::eChestType::JungleTemple:           return std::make_shared<cLootTable>();
+		case LootTable::eChestType::JungleTempleDispenser:  return std::make_shared<cLootTable>();
+		case LootTable::eChestType::NetherBridge:           return std::make_shared<cLootTable>();
+		case LootTable::eChestType::PillagerOutpost:        return std::make_shared<cLootTable>();
+		case LootTable::eChestType::ShipwreckMap:           return std::make_shared<cLootTable>();
+		case LootTable::eChestType::ShipwreckSupply:        return std::make_shared<cLootTable>();
+		case LootTable::eChestType::ShipwreckTreasure:      return std::make_shared<cLootTable>();
+		case LootTable::eChestType::SimpleDungeon:          return std::make_shared<cLootTable>();
+		case LootTable::eChestType::SpawnBonusChest:        return std::make_shared<cLootTable>();
+		case LootTable::eChestType::StrongholdCorridor:     return std::make_shared<cLootTable>();
+		case LootTable::eChestType::StrongholdCrossing:     return std::make_shared<cLootTable>();
+		case LootTable::eChestType::StrongholdLibrary:      return std::make_shared<cLootTable>();
+		case LootTable::eChestType::UnderwaterRuinBig:      return std::make_shared<cLootTable>();
+		case LootTable::eChestType::UnderwaterRuinSmall:    return std::make_shared<cLootTable>();
+
+			/* Village chest types */
+		case LootTable::eChestType::VillageArmorer:         return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageButcher:         return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageCartographer:    return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageDesertHouse:     return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageFisher:          return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageFletcher:        return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageMason:           return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillagePlainsHouse:     return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageSavannaHouse:    return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageShepherd:        return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageSnowyHouse:      return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageTaigaHouse:      return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageTannery:         return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageTemple:          return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageToolsmith:       return std::make_shared<cLootTable>();
+		case LootTable::eChestType::VillageWeaponsmith:     return std::make_shared<cLootTable>();
+
+		case LootTable::eChestType::WoodlandMansion:        return std::make_shared<cLootTable>();
+		default:                                            return nullptr;
+	}
+}
+
+
+
+
+
+std::shared_ptr<cLootTable> cLootTableProvider::GetLootTable(const eMonsterType a_Type) const
+{
+	// Checks if there's a custom Loot table available for this type else returns the default one
+	auto CustomTable = m_CustomMonsterLootTables.find(a_Type);
+
+	if (CustomTable != m_CustomMonsterLootTables.end())
+	{
+		return CustomTable->second;
+	}
+
 	switch (a_Type)
 	{
 		case mtBat:            return std::make_shared<cLootTable>();
@@ -68,11 +183,33 @@ std::shared_ptr<cLootTable> cLootTableProvider::GetLooTableFromMob(eMonsterType 
 		case mtZombie:         return std::make_shared<cLootTable>();
 		case mtZombiePigman:   return std::make_shared<cLootTable>();
 		case mtZombieVillager: return std::make_shared<cLootTable>();
-		default: return std::make_shared<cLootTable>();
+		default: return std::make_shared<cLootTable>(cLootTable());
 	}
 }
 
+
+
+
+
+void cLootTableProvider::DropLoot(const cMonster & a_Monster) const
+{
+	auto LootTable = GetLootTable(a_Monster.GetMobType());
+
+
+}
+
+
+
+
+
 cLootTable::cLootTable() {}
-cLootTable::cLootTable(AString a_Description) {}
-bool cLootTable::ReadFromString(AString a_Description) { return false; }
-bool cLootTable::FillWithLoot(Vector3i a_Pos) { return false; }
+cLootTable::cLootTable(const AString & a_Description) {}
+cLootTable::cLootTable(const cLootTable & a_Other)
+{
+	m_Type = a_Other.m_Type;
+	m_LootTablePools = a_Other.m_LootTablePools;
+	m_LootTableFunctions = a_Other.m_LootTableFunctions;
+}
+cLootTable::~cLootTable() {}
+bool cLootTable::ReadFromString(const AString & a_Description) { return false; }
+bool cLootTable::FillWithLoot(cBlockEntityWithItems & a_BlockEntity) { return false; }
--- src/LootTableProvider.h
+++ src/LootTableProvider.h
@@ -2,55 +2,470 @@
 // cLootTableProvider.h
 
 #pragma once
+
 #include "Globals.h"  // NOTE: MSVC stupidness requires this to be the same across all modules
 #include "BlockType.h"
 #include "Mobs/MonsterTypes.h"
+#include "BlockEntities/BlockEntityWithItems.h"
 
-
-/** Any available function type as of 1.16.2 */
-enum class cLootTableFunctionType
+namespace LootTable
 {
-	ApplyBonus,
-	CopyName,
-	CopyNbt,
-	CopyState,
-	EnchantRandomly,
-	EnchantWithLevels,
-	ExplorationMap,
-	ExplosionDecay,
-	FurnaceSmelt,
-	FillPlayerHead,
-	LimitCount,
-	LootingEnchant,
-	SetAttributes,
-	SetContents,
-	SetCount,
-	SetDamage,
-	SetLootTable,
-	SetLore,
-	SetName,
-	SetNbt,
-	SetStewEffect,
-	None
-};
+	/** Any available function type as of 1.16.2 */
+	enum class eFunctionType
+	{
+		ApplyBonus,
+		CopyName,
+		CopyNbt,
+		CopyState,
+		EnchantRandomly,
+		EnchantWithLevels,
+		ExplorationMap,
+		ExplosionDecay,
+		FurnaceSmelt,
+		FillPlayerHead,
+		LimitCount,
+		LootingEnchant,
+		SetAttributes,
+		SetContents,
+		SetCount,
+		SetDamage,
+		SetLootTable,
+		SetLore,
+		SetName,
+		SetNbt,
+		SetStewEffect,
+		None
+	};
 
 
 
 
-enum class LootTableType
-{
-	Empty,
-	Block,
-	Chest,
-	Fishing,
-	Gift,
-	AdvancementReward,
-	Barter,
-	Command,
-	Selector,
-	AdvancementEntity,
-	Generic
-};
+	/** Any available type of loot table Type as of 1.16.2*/
+	enum class eType
+	{
+		Empty,                         // doesnt generate any Loot
+		Entity,                        // Loot from any entity
+		Block,                         // Loot from mined blocks
+		Chest,                         // Loot from chests
+		Fishing,                       // Loot from fishing
+		Gift,                          // Loot gifted to the player from NPCs
+		AdvancementReward,             // Rewards from advancements
+		Barter,                        // Loot from "trading" with Piglins
+		Command,                       // Customizable loot tables for the loot command
+		Selector,                      // ?
+		AdvancementEntity,             // ?
+		Generic                        // ?
+	};
+
+
+
+
+	/** Gets the eType from String. Defaults to generic */
+	enum eType eType(const AString & a_Type)
+	{
+		if (NoCaseCompare(a_Type, "Empty") == 0)
+		{
+			return eType::Empty;
+		}
+		else if (NoCaseCompare(a_Type, "Entity") == 0)
+		{
+			return eType::Entity;
+		}
+		else if (NoCaseCompare(a_Type, "Block") == 0)
+		{
+			return eType::Block;
+		}
+		else if (NoCaseCompare(a_Type, "Chest") == 0)
+		{
+			return eType::Chest;
+		}
+		else if (NoCaseCompare(a_Type, "Fishing") == 0)
+		{
+			return eType::Fishing;
+		}
+		else if (NoCaseCompare(a_Type, "Gift") == 0)
+		{
+			return eType::Gift;
+		}
+		else if (NoCaseCompare(a_Type, "AdvancementReward") == 0)
+		{
+			return eType::AdvancementReward;
+		}
+		else if (NoCaseCompare(a_Type, "Barter") == 0)
+		{
+			return eType::Barter;
+		}
+		else if (NoCaseCompare(a_Type, "Command") == 0)
+		{
+			return eType::Command;
+		}
+		else if (NoCaseCompare(a_Type, "Selector") == 0)
+		{
+			return eType::Selector;
+		}
+		else if (NoCaseCompare(a_Type, "AdvancementEntity") == 0)
+		{
+			return eType::AdvancementEntity;
+		}
+		else
+		{
+			return eType::Generic;
+		}
+	}
+
+
+
+	/** Any available types of chest loots as of 1.16.2 */
+	enum class eChestType
+	{
+		AbandonedMineshaft,
+		BuriedTreasure,
+		DesertPyramid,
+		EndCityTreasure,
+		IglooChest,
+		JungleTemple,
+		JungleTempleDispenser,
+		NetherBridge,
+		PillagerOutpost,
+		ShipwreckMap,
+		ShipwreckSupply,
+		ShipwreckTreasure,
+		SimpleDungeon,
+		SpawnBonusChest,
+		StrongholdCorridor,
+		StrongholdCrossing,
+		StrongholdLibrary,
+		UnderwaterRuinBig,
+		UnderwaterRuinSmall,
+
+		/* Village chest types */
+		VillageArmorer,
+		VillageButcher,
+		VillageCartographer,
+		VillageDesertHouse,
+		VillageFisher,
+		VillageFletcher,
+		VillageMason,
+		VillagePlainsHouse,
+		VillageSavannaHouse,
+		VillageShepherd,
+		VillageSnowyHouse,
+		VillageTaigaHouse,
+		VillageTannery,
+		VillageTemple,
+		VillageToolsmith,
+		VillageWeaponsmith,
+
+		WoodlandMansion,
+		None
+	};
+
+
+
+
+	/** Gets the eChestType from String as of 1.16.2 */
+	enum eChestType eChestType(const AString & a_Type)
+	{
+		if (NoCaseCompare(a_Type, "AbandonedMineshaft") == 0)
+		{
+			return eChestType::AbandonedMineshaft;
+		}
+		else if (NoCaseCompare(a_Type, "BuriedTreasure") == 0)
+		{
+			return eChestType::BuriedTreasure;
+		}
+		else if (NoCaseCompare(a_Type, "DesertPyramid") == 0)
+		{
+			return eChestType::DesertPyramid;
+		}
+		else if (NoCaseCompare(a_Type, "EndCityTreasure") == 0)
+		{
+			return eChestType::EndCityTreasure;
+		}
+		else if (NoCaseCompare(a_Type, "IglooChest") == 0)
+		{
+			return eChestType::IglooChest;
+		}
+		else if (NoCaseCompare(a_Type, "JungleTemple") == 0)
+		{
+			return eChestType::JungleTemple;
+		}
+		else if (NoCaseCompare(a_Type, "JungleTempleDispenser") == 0)
+		{
+			return eChestType::JungleTempleDispenser;
+		}
+		else if (NoCaseCompare(a_Type, "NetherBridge") == 0)
+		{
+			return eChestType::NetherBridge;
+		}
+		else if (NoCaseCompare(a_Type, "PillagerOutpost") == 0)
+		{
+			return eChestType::PillagerOutpost;
+		}
+		else if (NoCaseCompare(a_Type, "ShipwreckMap") == 0)
+		{
+			return eChestType::ShipwreckMap;
+		}
+		else if (NoCaseCompare(a_Type, "ShipwreckSupply") == 0)
+		{
+			return eChestType::ShipwreckSupply;
+		}
+		else if (NoCaseCompare(a_Type, "ShipwreckTreasure") == 0)
+		{
+			return eChestType::ShipwreckTreasure;
+		}
+		else if (NoCaseCompare(a_Type, "SimpleDungeon") == 0)
+		{
+			return eChestType::SimpleDungeon;
+		}
+		else if (NoCaseCompare(a_Type, "SpawnBonusChest") == 0)
+		{
+			return eChestType::SpawnBonusChest;
+		}
+		else if (NoCaseCompare(a_Type, "StrongholdCorridor") == 0)
+		{
+			return eChestType::StrongholdCorridor;
+		}
+		else if (NoCaseCompare(a_Type, "StrongholdCrossing") == 0)
+		{
+			return eChestType::StrongholdCrossing;
+		}
+		else if (NoCaseCompare(a_Type, "StrongholdLibrary") == 0)
+		{
+			return eChestType::StrongholdLibrary;
+		}
+		else if (NoCaseCompare(a_Type, "UnderwaterRuinBig") == 0)
+		{
+			return eChestType::UnderwaterRuinBig;
+		}
+		else if (NoCaseCompare(a_Type, "UnderwaterRuinSmall") == 0)
+		{
+			return eChestType::UnderwaterRuinSmall;
+		}
+
+		/* Village chest types */
+
+		else if (NoCaseCompare(a_Type, "VillageArmorer") == 0)
+		{
+			return eChestType::VillageArmorer;
+		}
+		else if (NoCaseCompare(a_Type, "VillageButcher") == 0)
+		{
+			return eChestType::VillageButcher;
+		}
+		else if (NoCaseCompare(a_Type, "VillageCartographer") == 0)
+		{
+			return eChestType::VillageCartographer;
+		}
+		else if (NoCaseCompare(a_Type, "VillageDesertHouse") == 0)
+		{
+			return eChestType::VillageDesertHouse;
+		}
+		else if (NoCaseCompare(a_Type, "VillageFisher") == 0)
+		{
+			return eChestType::VillageFisher;
+		}
+		else if (NoCaseCompare(a_Type, "VillageFletcher") == 0)
+		{
+			return eChestType::VillageFletcher;
+		}
+		else if (NoCaseCompare(a_Type, "VillageMason") == 0)
+		{
+			return eChestType::VillageMason;
+		}
+		else if (NoCaseCompare(a_Type, "VillagePlainsHouse") == 0)
+		{
+			return eChestType::VillagePlainsHouse;
+		}
+		else if (NoCaseCompare(a_Type, "VillageSavannaHouse") == 0)
+		{
+			return eChestType::VillageSavannaHouse;
+		}
+		else if (NoCaseCompare(a_Type, "VillageShepherd") == 0)
+		{
+			return eChestType::VillageShepherd;
+		}
+		else if (NoCaseCompare(a_Type, "VillageSnowyHouse") == 0)
+		{
+			return eChestType::VillageSnowyHouse;
+		}
+		else if (NoCaseCompare(a_Type, "VillageTaigaHouse") == 0)
+		{
+			return eChestType::VillageTaigaHouse;
+		}
+		else if (NoCaseCompare(a_Type, "VillageTannery") == 0)
+		{
+			return eChestType::VillageTannery;
+		}
+		else if (NoCaseCompare(a_Type, "VillageTemple") == 0)
+		{
+			return eChestType::VillageTemple;
+		}
+		else if (NoCaseCompare(a_Type, "VillageToolsmith") == 0)
+		{
+			return eChestType::VillageToolsmith;
+		}
+		else if (NoCaseCompare(a_Type, "VillageWeaponsmith") == 0)
+		{
+			return eChestType::VillageWeaponsmith;
+		}
+
+		/* Village chest types end */
+
+		else if (NoCaseCompare(a_Type, "WoodlandMansion") == 0)
+		{
+			return eChestType::WoodlandMansion;
+		}
+		else
+		{
+			return eChestType::None;
+		}
+	}
+
+
+
+
+	/** Gets the eMonsterType from String. Defaults to Giant -> no loot */
+	eMonsterType eMonsterType(const AString & a_Type)
+	{
+		if (NoCaseCompare(a_Type, "Bat") == 0)
+		{
+			return mtBat;
+		}
+		else if (NoCaseCompare(a_Type, "Blaze") == 0)
+		{
+			return mtBlaze;
+		}
+		else if (NoCaseCompare(a_Type, "CaveSpider") == 0)
+		{
+			return mtCaveSpider;
+		}
+		else if (NoCaseCompare(a_Type, "mtChicken") == 0)
+		{
+			return mtChicken;
+		}
+		else if (NoCaseCompare(a_Type, "Cow") == 0)
+		{
+			return mtCow;
+		}
+		else if (NoCaseCompare(a_Type, "Creeper") == 0)
+		{
+			return mtCreeper;
+		}
+		else if (NoCaseCompare(a_Type, "EnderDragon") == 0)
+		{
+			return mtEnderDragon;
+		}
+		else if (NoCaseCompare(a_Type, "Enderman") == 0)
+		{
+			return mtEnderman;
+		}
+		else if (NoCaseCompare(a_Type, "Ghast") == 0)
+		{
+			return mtGhast;
+		}
+		else if (NoCaseCompare(a_Type, "Giant") == 0)
+		{
+			return mtGiant;
+		}
+		else if (NoCaseCompare(a_Type, "Guardian") == 0)
+		{
+			return mtGuardian;
+		}
+			else if (NoCaseCompare(a_Type, "Horse") == 0)
+		{
+			return mtHorse;
+		}
+		else if (NoCaseCompare(a_Type, "IronGolem") == 0)
+		{
+			return mtIronGolem;
+		}
+		else if (NoCaseCompare(a_Type, "MagmaCube") == 0)
+		{
+			return mtMagmaCube;
+		}
+		else if (NoCaseCompare(a_Type, "Mooshroom") == 0)
+		{
+			return mtMooshroom;
+		}
+		else if (NoCaseCompare(a_Type, "Ocelot") == 0)
+		{
+			return mtOcelot;
+		}
+		else if (NoCaseCompare(a_Type, "Pig") == 0)
+		{
+		return mtPig;
+		}
+		else if (NoCaseCompare(a_Type, "Rabbit") == 0)
+		{
+			return mtRabbit;
+		}
+		else if (NoCaseCompare(a_Type, "Sheep") == 0)
+		{
+			return mtSheep;
+		}
+		else if (NoCaseCompare(a_Type, "Silverfish") == 0)
+		{
+			return mtSilverfish;
+		}
+		else if (NoCaseCompare(a_Type, "Skeleton") == 0)
+		{
+			return mtSkeleton;
+		}
+		else if (NoCaseCompare(a_Type, "mtSlime") == 0)
+		{
+			return mtSlime;
+		}
+		else if (NoCaseCompare(a_Type, "SnowGolem") == 0)
+		{
+			return mtSnowGolem;
+		}
+		else if (NoCaseCompare(a_Type, "Spider") == 0)
+		{
+			return mtSpider;
+		}
+		else if (NoCaseCompare(a_Type, "Squid") == 0)
+		{
+			return mtSquid;
+		}
+		else if (NoCaseCompare(a_Type, "Villager") == 0)
+		{
+			return mtVillager;
+		}
+		else if (NoCaseCompare(a_Type, "mtWitch") == 0)
+		{
+			return mtWitch;
+		}
+		else if (NoCaseCompare(a_Type, "Wither") == 0)
+		{
+			return mtWither;
+		}
+		else if (NoCaseCompare(a_Type, "WitherSkeleton") == 0)
+		{
+			return mtWitherSkeleton;
+		}
+		else if (NoCaseCompare(a_Type, "Wolf") == 0)
+		{
+			return mtWolf;
+		}
+		else if (NoCaseCompare(a_Type, "Zombie") == 0)
+		{
+			return mtZombie;
+		}
+		else if (NoCaseCompare(a_Type, "ZombiePigman") == 0)
+		{
+			return mtZombiePigman;
+		}
+		else if (NoCaseCompare(a_Type, "ZombieVillager") == 0)
+		{
+			return mtZombieVillager;
+		}
+		else
+		{
+			return mtGiant;
+		}
+	}
+}
 
 
 
@@ -58,8 +473,8 @@ enum class LootTableType
 /** Represents a function for a pool item */
 typedef struct LootTableFunction
 {
-	cLootTableFunctionType m_Type;
-	AStringVector m_Parameter;
+	LootTable::eFunctionType m_Type;
+	AStringMap m_Parameter;
 } cLootTableFunction;
 
 typedef std::vector<cLootTableFunction> cLootTableFunctionVector;
@@ -101,40 +516,72 @@ typedef std::vector<cLootTablePool> cLootTablePoolVector;
 class cLootTable
 {
   public:
-
 	cLootTable();
 
 	/** Creates new loot table from string describing the loot table */
-	cLootTable(AString a_Description);
+	cLootTable(const AString & a_Description);
+
+	cLootTable(const cLootTable & a_Other);
+
+	~cLootTable();
+
+	// Todo: add json interpreter
 
 	/** Reads the string and generates a filled loot table from that and returns the success */
-	bool ReadFromString(AString a_Description);
+	bool ReadFromString(const AString & a_Description);
 
 	/** Fills the specified block entity at the position with loot and returns the success */
-	bool FillWithLoot(Vector3i a_Pos);
+	bool FillWithLoot(cBlockEntityWithItems & a_BlockEntity);
 
 protected:
-	LootTableType m_Type;
+	/** Type of loot table */
+	enum LootTable::eType m_Type;
+
+	/** Vector of loot pools */
 	cLootTablePoolVector m_LootTablePools;
-	cLootTableFunctionVector m_LootTableFunctions;
+
+	/** Vector of functions applied to all pools */
+	cLootTableFunctionVector m_LootTableFunctions = {};
 };
 
-
-
+typedef std::map<enum eMonsterType,          std::shared_ptr<cLootTable>> cCustomMonsterLootTableMap;
+typedef std::map<enum LootTable::eChestType, std::shared_ptr<cLootTable>> cCustomChestLootTableMap;
 
 /** The LootTableProvider is used per world to ask for loot tables */
 class cLootTableProvider
 {
 public:
-	cLootTableProvider();
+	cLootTableProvider(cWorld * a_World);
 
-	std::shared_ptr<cLootTable> GetLootTableFromString(AString a_Name);
+	/** Function to load a custom loot tables from specified path */
+	void LoadCustomLootTable(AString a_Path);
 
-	std::shared_ptr<cLootTable> GetLootTableFromBlock();
+	/** Functions to load loot tables. Custom loot tables are also checked */
+	std::shared_ptr<cLootTable> GetLootTable(const AString & a_Name) const;
+	std::shared_ptr<cLootTable> GetLootTable(const BLOCKTYPE a_Block) const;
+	std::shared_ptr<cLootTable> GetLootTable(const BLOCKTYPE a_Block, const ENUM_BLOCK_META) const;
+	std::shared_ptr<cLootTable> GetLootTable(const enum LootTable::eChestType a_Type) const;
+	std::shared_ptr<cLootTable> GetLootTable(const eMonsterType a_Type) const;
 
-	std::shared_ptr<cLootTable> GetLooTableFromMob(eMonsterType a_Type);
+	/** Drops the loot of a_Monster specified by the loot tables */
+	void DropLoot(const cMonster & a_Monster) const;  // Todo: move to unique id of monster
+
 private:
-	// m_CustomLootTables
+
+	/** Maps containing custom loot tables */
+	cCustomChestLootTableMap   m_CustomChestLootTables   = cCustomChestLootTableMap();
+	cCustomMonsterLootTableMap m_CustomMonsterLootTables = cCustomMonsterLootTableMap();
+
+	cWorld * m_World;
+};
+
+
+
+
+
+class cEmptyLootTable: public cLootTable
+{
+	enum LootTable::eType m_Type = LootTable::eType::Empty;
 };
 
 
@@ -143,24 +590,22 @@ private:
 
 class cChestLootTable: public cLootTable
 {
-	LootTableType m_Type = LootTableType::Chest;
+	enum LootTable::eType m_Type = LootTable::eType::Chest;
 	cLootTablePoolVector m_LootTablePools =
 	{
-		{-1, {1, 3}, 0 , {0, 0},
+		{
+			-1, {1, 3},  // Rolls
+			0 , {0, 0},  // Bonus rolls
 			{
 				{{}, E_ITEM_LEAD, 20},
 				{{}, E_ITEM_GOLDEN_APPLE, 15},
-				{{}, E_ITEM_GOLDEN_APPLE, 2}, // Todo: interpret apple
+				{{}, E_ITEM_GOLDEN_APPLE, 2}, // Todo: interpret golden apple
 				{{}, E_ITEM_13_DISC, 15},
 				{{}, E_ITEM_CAT_DISC, 15},
 				{{}, E_ITEM_NAME_TAG, 20},
 				{{}, E_ITEM_CHAIN_CHESTPLATE, 10}
 			}
 		}
-	};
-	cLootTableFunctionVector m_LootTableFunctions =
-	{
-
 	};
 };
 
