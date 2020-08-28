d2e92440eb3c4b3b9af42451d6a7406d384307b6
Tiger Wang <ziwei.tiger@outlook.com>
2020-08-28 21:43:12 +0100

    Remove redundant DoWithChunkAt in chests

--- src/BlockEntities/BlockEntityWithItems.cpp
+++ src/BlockEntities/BlockEntityWithItems.cpp
@@ -53,10 +53,7 @@ void cBlockEntityWithItems::OnSlotChanged(cItemGrid * a_Grid, int a_SlotNum)
 	}
 
 	m_World->MarkChunkDirty(GetChunkX(), GetChunkZ());
-	m_World->DoWithChunkAt(m_Pos, [&](cChunk & a_Chunk)
-	{
-		// Notify comparators:
-		m_World->WakeUpSimulators(m_Pos);
-		return true;
-	});
+
+	// Notify comparators:
+	m_World->WakeUpSimulators(m_Pos);
 }
