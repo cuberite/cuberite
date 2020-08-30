02d4daf4f59e20522a6c5250fd47866a42d5eba3
Alexander Harkness <me@bearbin.net>
2020-08-30 19:25:51 +0100

    Implemented TNT entity interaction
    * Fixed misaligned destruction tracing

--- src/Physics/Explodinator.cpp
+++ src/Physics/Explodinator.cpp
@@ -1,12 +1,12 @@
 
 #include "Globals.h"
-#include "Chunk.h"
-#include "ClientHandle.h"
 #include "BlockArea.h"
 #include "BlockInfo.h"
+#include "Chunk.h"
+#include "ClientHandle.h"
+#include "LineBlockTracer.h"
 #include "Simulator/SandSimulator.h"
 #include "World.h"
-
 
 
 
@@ -19,7 +19,51 @@ namespace Explodinator
 
 	inline void DamageEntities(cWorld & World, const Vector3f Position, const unsigned Power)
 	{
+		World.ForEachEntityInBox(
+			cBoundingBox(Position, 2*Power),
+			[&](cEntity & Entity)
+			{
+				auto bb = Entity.GetBoundingBox();
 
+				int unobstructed = 0;
+				int obstructed = 0;
+				for (float x = bb.GetMinX(); x < bb.GetMaxX(); x += 0.5f)
+				{
+					for (float y = bb.GetMinY(); y < bb.GetMaxY(); y += 0.5f)
+					{
+						for (float z = bb.GetMinZ(); z < bb.GetMaxZ(); z += 0.5f)
+						{
+							auto Start = Vector3d(x, y, z);
+							cLineBlockTracer::LineOfSightTrace(World, Start, Position, cLineBlockTracer::losAir) ? unobstructed++ : obstructed++;
+						}
+					}
+				}
+
+				auto exposure = static_cast<float>(unobstructed) / (unobstructed + obstructed);
+
+				auto direction = Entity.GetPosition() - Position;
+				auto distance = direction.Length();
+
+				auto impact = (1 - (distance / (2 * Power))) * exposure;
+
+				auto damage = (impact * impact + impact) * 7 * Power + 1;
+				auto roundedDamage = FloorC(damage);
+
+				Entity.TakeDamage(dtExplosion, nullptr, roundedDamage, 0);
+
+				auto knockbackAmount = exposure - exposure * Entity.GetEnchantmentBlastKnockbackReduction();
+				Entity.SetSpeed(direction.NormalizeCopy() * knockbackAmount * 50);
+
+				return true;
+			}
+		);
+		// Do with entities in box:
+
+		//  1. Calculate exposure
+		//  2. Calculate distance from explosion
+		//  3. Calcualte damage, and apply
+		//  4. Calculate blast protection reduction to exposure
+		//  5. Knockback
 	}
 
 	inline float GetAbsorption(const BLOCKTYPE Block)
@@ -57,11 +101,12 @@ namespace Explodinator
 		}
 		else if (Fiery && Random.RandBool(1.f / 3.f))  // 33% chance of starting fires if it can start fires
 		{
+			// TODO: Check for opaque block below (or fire simulator does so)
 			// Start a fire, depend on the fire simulator to extinguish invalid positions:
 			Area.SetBlockTypeMeta(Position.x, Position.y, Position.z, E_BLOCK_FIRE, 0);
 			return;
 		}
-		else if (const auto Shrapnel = World.GetTNTShrapnelLevel(); (Shrapnel > slNone) && Random.RandBool(0.20))  // 20% chance of flinging stuff around
+		else if (const auto Shrapnel = World.GetTNTShrapnelLevel(); (Shrapnel > slNone) && Random.RandBool(0.2))  // 20% chance of flinging stuff around
 		{
 			// If the block is shrapnel-able, make a falling block entity out of it:
 			if (
@@ -83,7 +128,7 @@ namespace Explodinator
 		while ((Destination - Origin).SqrLength() > 0)
 		{
 			Origin += Step;
-			Intensity -= 0.225f;
+			Intensity -= StepAttenuation;
 
 			const auto Position = Origin.Floor();
 			Intensity -= 0.3f * (0.3f + GetAbsorption(Area.GetBlockType(Position.x, Position.y, Position.z)));
@@ -117,11 +162,11 @@ namespace Explodinator
 		}
 
 		// Oh boy... Better hope you have a hot cache, 'cos this little manoeuvre's gonna cost us 1352 raytraces in one tick...
-		const auto HalfSide = TraceCubeSideLength / 2.f;
-
-		for (float OffsetX = -HalfSide; OffsetX < HalfSide; OffsetX++)
+		const int HalfSide = TraceCubeSideLength / 2;
+		
+		for (int OffsetX = -HalfSide; OffsetX < HalfSide; OffsetX++)
 		{
-			for (float OffsetZ = -HalfSide; OffsetZ < HalfSide; OffsetZ++)
+			for (int OffsetZ = -HalfSide; OffsetZ < HalfSide; OffsetZ++)
 			{
 				DestructionTrace(World, Area, Random, Position, Position + Vector3f(OffsetX, +ExplosionRadius, OffsetZ), Power, Fiery, Intensity);
 				DestructionTrace(World, Area, Random, Position, Position + Vector3f(OffsetX, -ExplosionRadius, OffsetZ), Power, Fiery, Intensity);
@@ -129,14 +174,14 @@ namespace Explodinator
 		}
 
 		// TODO: not symmetric
-		for (float Offset = -HalfSide; Offset < HalfSide - 1; Offset++)
+		for (int Offset = -HalfSide; Offset < HalfSide - 1; Offset++)
 		{
-			for (float OffsetY = -HalfSide + 1; OffsetY < HalfSide - 1; OffsetY++)
+			for (int OffsetY = -HalfSide + 1; OffsetY < HalfSide - 1; OffsetY++)
 			{
-				DestructionTrace(World, Area, Random, Position, Position + Vector3f(+ExplosionRadius, OffsetY, Offset), Power, Fiery, Intensity);
+				DestructionTrace(World, Area, Random, Position, Position + Vector3f(+ExplosionRadius, OffsetY, Offset + 1), Power, Fiery, Intensity);
 				DestructionTrace(World, Area, Random, Position, Position + Vector3f(-ExplosionRadius, OffsetY, Offset), Power, Fiery, Intensity);
 				DestructionTrace(World, Area, Random, Position, Position + Vector3f(Offset, OffsetY, +ExplosionRadius), Power, Fiery, Intensity);
-				DestructionTrace(World, Area, Random, Position, Position + Vector3f(Offset, OffsetY, -ExplosionRadius), Power, Fiery, Intensity);
+				DestructionTrace(World, Area, Random, Position, Position + Vector3f(Offset + 1, OffsetY, -ExplosionRadius), Power, Fiery, Intensity);
 			}
 		}
 
@@ -162,7 +207,7 @@ namespace Explodinator
 	void Kaboom(cWorld & World, const Vector3f Position, const unsigned Power, const bool Fiery)
 	{
 		LagTheClient(World, Position, Power);
-		DamageEntities(World, Position, Power);
 		DamageBlocks(World, Position, Power, Fiery);
+		DamageEntities(World, Position, Power);
 	}
 }
