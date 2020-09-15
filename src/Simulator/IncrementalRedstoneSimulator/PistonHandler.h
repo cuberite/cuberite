46bf273773ab0597365bdc388719d4da834b35a9
Elias Thomson <dev@eliasthomson.com>
2020-09-15 17:10:36 +0400

    [WIP] Fixed code nonconformity issues

--- src/Simulator/IncrementalRedstoneSimulator/PistonHandler.h
+++ src/Simulator/IncrementalRedstoneSimulator/PistonHandler.h
@@ -34,18 +34,12 @@ namespace PistonHandler
 		// Needs to be editable
 		auto EffectivePower = (PowerLevel) Power;
 
-		// If isn't powered directly, try querying blocks adjacent to QC site
+		// If isn't powered directly, try get power using quasi-connectivity
 		if (Power == 0)
 		{
-			// Try get power using Quasi-connectivity
-			// @see https://minecraft.gamepedia.com/Tutorials/Quasi-connectivity
-
-			// Get the QC site
 			const auto QCSocketPos  = a_Position.addedY(1);
 			const auto QCSocketType = a_Chunk.GetBlock(QCSocketPos);
 
-			// Then query each of it's surrounding blocks,
-			//  for whether they can provide QC site with power
 			for (const auto SourceOffset : RelativeAdjacents)
 			{
 				// Don't check the piston itself
@@ -54,38 +48,30 @@ namespace PistonHandler
 					continue;
 				}
 
-				// Get source block data
 				const auto SourceBlockPos  = QCSocketPos + SourceOffset;
 				const auto SourceBlockType = a_Chunk.GetBlock(SourceBlockPos);
 
 				// Solid blocks get power from further away and thus require block update
 				if (cBlockInfo::IsSolid(SourceBlockType))
 				{
-					// Except for one (maybe more, it's just what I found with testing)
+					// Except for one (maybe more, this is just what I found with testing)
 					if (SourceBlockType != E_BLOCK_TRAPPED_CHEST)
 					{
-						// Skip block of redstone as it uses BUD
+						// Skip block of redstone as it uses block update detection
 						if (SourceBlockType == E_BLOCK_TRAPPED_CHEST)
 						{
 							continue;
 						}
 
-						// Check if this solid has an attached component, 
-						//  capable of activating&updating 2 blocks away
-						//  those include: Button, Detector Rail, Lever, Pressure Plates, Tripwire Hook
-						//  of which we only interested in those attached from above, 
-						//  since diagonally they can power up to 2 block taxicab away,
-						//  unlike those attached vertically, that can do only 1 block
-						//  @see: 
-
 						// Query block above for attachments
 						const auto SourceBlockAttachmentPos  = SourceBlockPos.addedY(1);
 						const auto SourceBlockAttachmentType = a_Chunk.GetBlock(SourceBlockAttachmentPos);
 
+						// Attachments must be diagonal to QC Socket in order to provide block update
 						switch (SourceBlockAttachmentType)
 						{
 							// Detector rails - can only be put on top
-							case E_BLOCK_DETECTOR_RAIL:	break;
+							case E_BLOCK_DETECTOR_RAIL: break;
 
 							// Levers
 							case E_BLOCK_LEVER:
@@ -116,32 +102,35 @@ namespace PistonHandler
 							case E_BLOCK_HEAVY_WEIGHTED_PRESSURE_PLATE:
 							case E_BLOCK_LIGHT_WEIGHTED_PRESSURE_PLATE:
 							case E_BLOCK_STONE_PRESSURE_PLATE:
-							case E_BLOCK_WOODEN_PRESSURE_PLATE:	break;
+							case E_BLOCK_WOODEN_PRESSURE_PLATE: break;
 
 							// No suitable attachments on this source - look further
 							default: continue;
 						}
 					}
 				}
-				else // if (!cBlockInfo::IsSolid(QCSocketType))	
+				else
 				{
-					// Create a helper function, that otherwise doesn't belong in this file
 					auto BlockFaceToOffset = [](eBlockFace a_BlockFace)
 					{
+						// Axial vector of length 0
+						const auto O = Vector3i({0, 0, 0});
+
 						switch (a_BlockFace)
 						{
-							case BLOCK_FACE_XP: return Vector3i({1,0,0});
-							case BLOCK_FACE_XM: return Vector3i({-1,0,0});
-							case BLOCK_FACE_YP: return Vector3i({0,1,0});
-							case BLOCK_FACE_YM: return Vector3i({1,-1,0});
-							case BLOCK_FACE_ZP: return Vector3i({1,0,0});
-							case BLOCK_FACE_ZM: return Vector3i({-1,0,0});
+							case BLOCK_FACE_XP: return O.addedX(1);
+							case BLOCK_FACE_XM: return O.addedX(-1);
+							case BLOCK_FACE_YP: return O.addedY(1);
+							case BLOCK_FACE_YM: return O.addedY(-1);
+							case BLOCK_FACE_ZP: return O.addedZ(1);
+							case BLOCK_FACE_ZM: return O.addedZ(-1);
 
-							// Only look for adjacent block
-							default: return Vector3i({0,0,0});
+							// Only evaluate for adjacent blocks
+							default: return O;
 						}
 					};
 
+					// If the source block is itself attacheable, it must be attached to QC socket
 					switch (SourceBlockType)
 					{
 						// Levers
@@ -175,14 +164,13 @@ namespace PistonHandler
 
 				// Query how much power the source can give
 				EffectivePower = RedstoneHandler::GetPowerDeliveredToPosition(
-														a_Chunk,
-														SourceBlockPos,
-														SourceBlockType,
-														QCSocketPos,
-														QCSocketType,
-														false);
+					a_Chunk,
+					SourceBlockPos,
+					SourceBlockType,
+					QCSocketPos,
+					QCSocketType,
+					false);
 
-				// If viable source is found, bail out
 				if (EffectivePower != 0)
 				{
 					break;
