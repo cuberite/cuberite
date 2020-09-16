d6b6186b5077a9c45c6c62c60976afb25dca150e
12xx12 <12xx12100@gmail.com>
2020-09-16 20:44:32 +0200

    added armor and shulker box cleaning

--- src/Blocks/BlockCauldron.h
+++ src/Blocks/BlockCauldron.h
@@ -54,7 +54,6 @@ public:
 					if (!a_Player.IsGameModeCreative())
 					{
 						a_Player.ReplaceOneEquippedItemTossRest(cItem(E_ITEM_WATER_BUCKET));
-						a_Player.GetStatManager().AddValue(Statistic::UseCauldron);
 					}
 				}
 				break;
@@ -83,6 +82,7 @@ public:
 					{
 						a_Player.ReplaceOneEquippedItemTossRest(cItem(E_ITEM_POTION));
 					}
+					a_Player.GetStatManager().AddValue(Statistic::UseCauldron);
 				}
 				break;
 			}
@@ -98,6 +98,49 @@ public:
 						a_Player.ReplaceOneEquippedItemTossRest(cItem(E_ITEM_GLASS_BOTTLE));
 					}
 				}
+				break;
+			}
+			// Resets any color to default
+			case E_ITEM_LEATHER_BOOTS:
+			case E_ITEM_LEATHER_CAP:
+			case E_ITEM_LEATHER_PANTS:
+			case E_ITEM_LEATHER_TUNIC:
+			{
+				if ((Meta > 0) && ((EquippedItem.m_ItemColor.GetRed() != 255) || (EquippedItem.m_ItemColor.GetBlue() != 255) || (EquippedItem.m_ItemColor.GetGreen() != 255)))
+				{
+					a_ChunkInterface.SetBlockMeta(a_BlockPos, --Meta);
+					LOG("%d", Meta);
+					auto NewItem = cItem(EquippedItem);
+					NewItem.m_ItemColor.Clear();
+					a_Player.ReplaceOneEquippedItemTossRest(NewItem);
+				}
+				break;
+			}
+			// Resets shulker box color
+			case E_BLOCK_BLACK_SHULKER_BOX:
+			case E_BLOCK_BLUE_SHULKER_BOX:
+			case E_BLOCK_BROWN_SHULKER_BOX:
+			case E_BLOCK_CYAN_SHULKER_BOX:
+			case E_BLOCK_GRAY_SHULKER_BOX:
+			case E_BLOCK_GREEN_SHULKER_BOX:
+			case E_BLOCK_LIGHT_BLUE_SHULKER_BOX:
+			case E_BLOCK_LIGHT_GRAY_SHULKER_BOX:
+			case E_BLOCK_LIME_SHULKER_BOX:
+			case E_BLOCK_MAGENTA_SHULKER_BOX:
+			case E_BLOCK_ORANGE_SHULKER_BOX:
+			case E_BLOCK_PINK_SHULKER_BOX:
+			case E_BLOCK_RED_SHULKER_BOX:
+			case E_BLOCK_YELLOW_SHULKER_BOX:
+			{
+				if (Meta > 0)
+				{
+					a_ChunkInterface.SetBlockMeta(a_BlockPos, --Meta);
+					// Give new potion when the gamemode is not creative:
+					auto NewShulker = cItem(EquippedItem);
+					NewShulker.m_ItemType = E_BLOCK_PURPLE_SHULKER_BOX;
+					a_Player.ReplaceOneEquippedItemTossRest(NewShulker);
+				}
+				break;
 			}
 		}
 		return true;
