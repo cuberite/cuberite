ae752a3860743acde1cab8f6a0cdfbad4e305fa9
12xx12 <12xx12100@gmail.com>
2020-09-24 13:45:52 +0200

    removed debug log

--- src/WorldStorage/WSSAnvil.cpp
+++ src/WorldStorage/WSSAnvil.cpp
@@ -1343,7 +1343,6 @@ OwnedBlockEntity cWSSAnvil::LoadMobSpawnerFromNBT(const cParsedNBT & a_NBT, int 
 	if ((Type >= 0) && (a_NBT.GetType(Type) == TAG_Short))
 	{
 		short MonsterType = a_NBT.GetShort(Type);
-		LOG("%d", MonsterType);
 		if (MonsterType != eMonsterType::mtInvalidType)
 		{
 			MobSpawner->SetEntity(static_cast<eMonsterType>(MonsterType));
