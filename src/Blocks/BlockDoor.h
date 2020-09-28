429117c1f6131df407246a46bc75a724eca0da54
Tiger Wang <ziwei.tiger@outlook.com>
2020-09-28 22:04:57 +0100

    Doors: check power & toggle correctly
    * Fixed upper half ignoring its updates
    * Fixes #4945
    * Fixed doors playing sound effects when they didn't actually toggle

--- src/Blocks/BlockDoor.h
+++ src/Blocks/BlockDoor.h
@@ -96,6 +96,14 @@ public:
 		}
 	}
 
+	/** Returns true iff the door at the specified coords is open.
+	The coords may point to either the top part or the bottom part of the door. */
+	static bool IsOpen(cChunkInterface & a_ChunkInterface, const Vector3i a_BlockPos)
+	{
+		const auto Meta = GetCompleteDoorMeta(a_ChunkInterface, a_BlockPos);
+		return (Meta & 0x04) != 0;
+	}
+
 	/** Sets the door to the specified state. If the door is already in that state, does nothing. */
 	static void SetOpen(cChunkInterface & a_ChunkInterface, const Vector3i a_BlockPos, bool a_Open)
 	{
@@ -231,18 +239,6 @@ private:
 	virtual bool CanBeAt(cChunkInterface & a_ChunkInterface, const Vector3i a_RelPos, const cChunk & a_Chunk) const override
 	{
 		return ((a_RelPos.y > 0) && CanBeOn(a_Chunk.GetBlock(a_RelPos.addedY(-1)), a_Chunk.GetMeta(a_RelPos.addedY(-1))));
-	}
-
-
-
-
-
-	/** Returns true iff the door at the specified coords is open.
-	The coords may point to either the top part or the bottom part of the door. */
-	static NIBBLETYPE IsOpen(cChunkInterface & a_ChunkInterface, const Vector3i a_BlockPos)
-	{
-		NIBBLETYPE Meta = GetCompleteDoorMeta(a_ChunkInterface, a_BlockPos);
-		return ((Meta & 0x04) != 0);
 	}
 
 
