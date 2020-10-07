93490241d2a7513514b6ee5e20f51666a320ed80
12xx12 <12xx12100@gmail.com>
2020-10-07 13:13:34 +0200

    checkstyle

--- src/Physics/Explodinator.h
+++ src/Physics/Explodinator.h
@@ -16,6 +16,6 @@ namespace Explodinator
 	/** Creates an explosion of Power, centred at Position, with ability to set fires as provided.
 	For maximum efficiency, Position should be in the centre of the entity or block that exploded.
 	Kaboom indeed, you drunken wretch.
-	The entity pointer is used to trigger OnBreak in the destroyed blocks*/
+	The entity pointer is used while trigger OnBreak in the destroyed blocks. */
 	void Kaboom(cWorld & World, Vector3f Position, unsigned Power, bool Fiery, const cEntity * a_ExplodingEntity);
 }
