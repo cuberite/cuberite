a4fe7b9bc185689b755d7c0e95f37d9f17492baa
12xx12 <12xx12100@gmail.com>
2020-10-16 16:28:27 +0200

    added item tags

---
+++ src/Registries/ItemTags.cpp
@@ -0,0 +1,91 @@
+
+// ItemTags.cpp
+
+#include "Globals.h"
+
+#include "ItemTags.h"
+#include "../IniFile.h"
+
+cItemTags::cItemTags()
+{
+	cIniFile Ini;
+	if (!Ini.ReadFile("items.ini"))
+	{
+		UNREACHABLE("Missing Items.ini file, please download with the newest version of cuberite!");
+	}
+	int KeyID = Ini.FindKey("ItemTags");
+	if (KeyID == cIniFile::noID)
+	{
+		UNREACHABLE("Missing Item tags in item.ini, please download with the newest version of cuberite!");
+	}
+	int NumValues = Ini.GetNumValues(KeyID);
+	for (int i = 0; i < NumValues; i++)
+	{
+		AString Name = Ini.GetValueName(KeyID, i);
+		if (Name.empty())
+		{
+			continue;
+		}
+		AString Value = Ini.GetValue(KeyID, i);
+		AddToMap(Name, Value);
+	}  // for i - Ini.Values[]
+}
+
+
+
+
+
+const cItems cItemTags::Resolve(const AString & a_Name) const
+{
+	auto It = m_Map.find(a_Name);
+	if (It != m_Map.end())
+	{
+		cItems Result;
+		const auto & Items = It->second;
+		for (const auto & Item : Items)
+		{
+			std::visit(
+				overloaded
+				{
+						[&] (const AString & a_ItemTagName)
+						{
+							// Just to prevent infinite recursion
+							if (NoCaseCompare(a_Name, a_ItemTagName) == 0)
+							{
+								return;
+							}
+							auto SubResult = Resolve(a_ItemTagName);
+							Result.insert(Result.end(), SubResult.begin(), SubResult.end());
+						},
+						[&] (const cItem & a_Item) { Result.Add(a_Item); }
+				}
+				, Item);
+		}
+		return Result;
+	}
+	return cItems();
+}
+
+
+
+
+
+void cItemTags::AddToMap(const AString & a_Name, const AString & a_Value)
+{
+	const auto Items = StringSplitAndTrim(a_Value, "|");
+	cItemTag Value;
+	for (const auto & ItemString : Items)
+	{
+		cItem Item;
+		// If there is no item present save it as a string to map
+		if (!StringToItem(ItemString, Item))
+		{
+			Value.emplace_back(ItemString);
+		}
+		else
+		{
+			Value.emplace_back(Item);
+		}
+	}
+	m_Map[a_Name] = Value;
+}
---
+++ src/Registries/ItemTags.h
@@ -0,0 +1,40 @@
+
+// ItemTags.h
+
+#pragma once
+
+#include "../Item.h"
+#include "variant"
+
+/*
+Item tags are loaded from the item.ini
+You can export them from the minecraft sever with data generators
+
+This list is up to date with 1.16.2 if there are new added please add below:
+https://minecraft.gamepedia.com/Tag#Items
+*/
+
+using cItemTag = std::vector<std::variant<cItem, AString>>;
+
+class cItemTags
+{
+public:
+	cItemTags();
+
+	/** Resolves the given string to a cItems */
+	const cItems Resolve(const AString & a_Name) const;
+
+protected:
+	void AddToMap(const AString & a_Name, const AString & a_Value);
+
+	std::unordered_map<AString, cItemTag> m_Map;
+};
+
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+static cItemTags & GetItemTags()
+{
+	static cItemTags ItemTags;
+	return ItemTags;
+}
