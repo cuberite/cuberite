2882eb820a3a35f1566237dcf3bc3e847e923622
partypyro <aidenneill@gmail.com>
2020-11-27 20:53:22 -0700

    Daylight sensor now updates properly

--- src/Blocks/BlockDaylightSensor.h
+++ src/Blocks/BlockDaylightSensor.h
@@ -8,53 +8,52 @@
 
 
 class cBlockDaylightSensorHandler final :
-  public cBlockHandler
+	public cBlockHandler
 {
-  using Super = cBlockHandler;
-
+	using Super = cBlockHandler;
 public:
 
 	using Super::Super;
 
 private:
 
-  virtual bool OnUse(
-    cChunkInterface & a_ChunkInterface,
+	virtual bool OnUse(
+		cChunkInterface & a_ChunkInterface,
 		cWorldInterface & a_WorldInterface,
 		cPlayer & a_Player,
 		const Vector3i a_BlockPos,
 		eBlockFace a_BlockFace,
 		const Vector3i a_CursorPos
-  ) const override
-  {
-    BLOCKTYPE BlockType = a_ChunkInterface.GetBlock(a_BlockPos);
-    if(BlockType == E_BLOCK_DAYLIGHT_SENSOR)
-    {
-      a_ChunkInterface.FastSetBlock(a_BlockPos, E_BLOCK_INVERTED_DAYLIGHT_SENSOR, 0);
-    }
-    else
-    {
-      a_ChunkInterface.FastSetBlock(a_BlockPos, E_BLOCK_DAYLIGHT_SENSOR, 0);
-    }
+	) const override
+	{
+		BLOCKTYPE BlockType = a_ChunkInterface.GetBlock(a_BlockPos);
+		if (BlockType == E_BLOCK_DAYLIGHT_SENSOR)
+		{
+			a_ChunkInterface.FastSetBlock(a_BlockPos, E_BLOCK_INVERTED_DAYLIGHT_SENSOR, 0);
+		}
+		else
+		{
+			a_ChunkInterface.FastSetBlock(a_BlockPos, E_BLOCK_DAYLIGHT_SENSOR, 0);
+		}
 
-    return true;
-  }
-
-
-
-
-
-  virtual cItems ConvertToPickups(NIBBLETYPE a_BlockMeta, const cEntity * a_Digger, const cItem * a_Tool) const override
-  {
-    // Always drop the regular daylight sensor:
-    return(cItem(E_BLOCK_DAYLIGHT_SENSOR, 1, 0));
-  }
+		return true;
+	}
 
 
 
 
 
-  virtual bool IsUseable(void) const override
+	virtual cItems ConvertToPickups(NIBBLETYPE a_BlockMeta, const cEntity * a_Digger, const cItem * a_Tool) const override
+	{
+		// Always drop the regular daylight sensor:
+		return(cItem(E_BLOCK_DAYLIGHT_SENSOR, 1, 0));
+	}
+
+
+
+
+
+	virtual bool IsUseable(void) const override
 	{
 		return true;
 	}
--- src/Simulator/IncrementalRedstoneSimulator/DaylightSensorHandler.h
+++ src/Simulator/IncrementalRedstoneSimulator/DaylightSensorHandler.h
@@ -1,6 +1,5 @@
 
 #pragma Once
-#include <cmath>
 
 
 
@@ -8,30 +7,51 @@
 
 namespace DaylightSensorHandler
 {
+	inline unsigned char GetPowerLevel(const cChunk & Chunk, const Vector3i Position)
+	{
+		float PI = 3.14159;
+		int TimeOfDay = Chunk.GetWorld()->GetTimeOfDay();
+		bool IsInverted = (Chunk.GetBlock(Position) == E_BLOCK_DAYLIGHT_SENSOR) ? 0 : 1;
+		int Output = Clamp(FloorC(14 * std::sin((TimeOfDay * PI) / 12000) + 8), 0, 15);
+
+		if (IsInverted)
+		{
+			Output = 15 - Output;
+		}
+
+		return static_cast<unsigned char>(Output);
+	}
+
 	inline PowerLevel GetPowerDeliveredToPosition(const cChunk & a_Chunk, Vector3i a_Position, BLOCKTYPE a_BlockType, Vector3i a_QueryPosition, BLOCKTYPE a_QueryBlockType, bool IsLinked)
 	{
 		UNUSED(a_Chunk);
-		UNUSED(a_Position);
 		UNUSED(a_BlockType);
 		UNUSED(a_QueryPosition);
-		UNUSED(IsLinked);
 
-    float PI = 3.14159;
-    int TimeOfDay = a_Chunk.GetWorld()->GetTimeOfDay();
-    bool IsInverted = (a_Chunk.GetBlock(a_Position) == E_BLOCK_DAYLIGHT_SENSOR) ? 1 : 0;
-    int Output = Clamp(FloorC(8 * std::sin((TimeOfDay * PI) / 12000) + 8), 0, 15);
-
-    if(IsInverted)
-    {
-      Output = 15 - Output;
-    }
-
-    return static_cast<int>(Output);
+		// Daylight sensors output to all surrounding blocks
+		// Retrieve and return the cached power calculated by Update for performance
+		return DataForChunk(a_Chunk).GetCachedPowerData(a_Position);
 	}
 
 	inline void Update(cChunk & a_Chunk, cChunk & CurrentlyTicking, Vector3i a_Position, BLOCKTYPE a_BlockType, NIBBLETYPE a_Meta, const PowerLevel Power)
 	{
-    
+		LOGD("Evaluating Darryl the daylight sensor (%d %d %d)", a_Position.x, a_Position.y, a_Position.z);
+
+		auto & ChunkData = DataForChunk(a_Chunk);
+
+		const auto PreviousPower = ChunkData.GetCachedPowerData(a_Position);
+		const auto PowerLevel = GetPowerLevel(a_Chunk, a_Position);
+
+		// We don't need to update the daylight sensor often
+		ChunkData.m_MechanismDelays[a_Position] = std::make_pair(30, true);
+
+		// Only update the power level if the power level has changed - it has gone
+		// up a 'step' on the time-power function
+		if (PowerLevel != PreviousPower)
+		{
+			ChunkData.SetCachedPowerData(a_Position, PowerLevel);
+			UpdateAdjustedRelatives(a_Chunk, CurrentlyTicking, a_Position, RelativeAdjacents);
+		}
 	}
 
 	inline void ForValidSourcePositions(const cChunk & a_Chunk, Vector3i a_Position, BLOCKTYPE a_BlockType, NIBBLETYPE a_Meta, ForEachSourceCallback & Callback)
