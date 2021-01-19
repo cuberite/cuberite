7a2923f5184f0a107298f2db90b6af56e10f4dff
dImrich <damian@haze.sk>
2021-01-19 01:40:56 +0100

    End crystal placement

---
+++ src/Items/ItemEndCrystal.h
@@ -0,0 +1,82 @@
+
+#pragma once
+
+#include "ItemHandler.h"
+#include "../World.h"
+#include "../Entities/Player.h"
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <string>
+
+
+
+class cItemEndCrystalHandler : public cItemHandler
+{
+	using Super = cItemHandler;
+
+  public:
+	cItemEndCrystalHandler(int a_ItemType) : Super(a_ItemType) {}
+
+
+
+
+
+	virtual bool OnItemUse(
+		cWorld * a_World, cPlayer * a_Player,
+		cBlockPluginInterface & a_PluginInterface, const cItem & a_HeldItem,
+		const Vector3i a_BlockPos,
+		eBlockFace a_ClickedBlockFace) override
+	{
+		// Must click a valid block:
+		if (a_ClickedBlockFace < 0)
+		{
+			return false;
+		}
+
+		auto * a_ChunkMap = a_World->GetChunkMap();
+
+		cChunkInterface ChunkInterface(a_ChunkMap);
+
+		if (ChunkInterface.GetBlock(a_BlockPos + Vector3i(0, 1, 0)) != 0 ||
+			ChunkInterface.GetBlock(a_BlockPos + Vector3i(0, 2, 0)) != 0)
+		{
+			return false;
+		}
+
+		auto a_BlockType = ChunkInterface.GetBlock(a_BlockPos);
+
+		if (a_BlockType == E_BLOCK_OBSIDIAN || a_BlockType == E_BLOCK_BEDROCK)
+		{
+
+			bool CanBePlaced = true;
+
+			a_ChunkMap->ForEachEntityInBox(cBoundingBox(a_BlockPos,Vector3d(a_BlockPos.x + 1.0, a_BlockPos.y + 2.0, a_BlockPos.z + 1.0)),
+				[&CanBePlaced](cEntity & a_Entity) -> bool
+				{
+					if (a_Entity.GetEntityType() == esEnderCrystal)
+					{
+						CanBePlaced = false;
+					}
+
+					return false;
+				}
+			);
+
+			if (CanBePlaced)
+			{
+				a_World->SpawnEnderCrystal(Vector3d(a_BlockPos.x + 0.5, a_BlockPos.y + 1.0, a_BlockPos.z + 0.5), false);
+
+				if (!a_Player->IsGameModeCreative())
+				{
+					a_Player->GetInventory().RemoveOneEquippedItem();
+				}
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+};
