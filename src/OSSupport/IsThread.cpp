222d9957a1a566464d6a824caaf9a56539eb3234
Tiger Wang <ziwei.tiger@outlook.com>
2021-03-28 22:33:24 +0100

    cIsThread cleanup
    + Semi-gracefully handle unexpected exceptions
    * No-one cared about the return values, remove them

--- src/OSSupport/IsThread.cpp
+++ src/OSSupport/IsThread.cpp
@@ -32,12 +32,76 @@ cIsThread::~cIsThread()
 
 
 
-void cIsThread::DoExecute(void)
+void cIsThread::Start(void)
+{
+	// Initialize the thread:
+	m_Thread = std::thread(&cIsThread::Entrypoint, this);
+
+	// Notify the thread that initialization is complete and it can run its code safely:
+	m_Initialisation.Set();
+}
+
+
+
+
+
+void cIsThread::Stop(void)
+{
+	m_ShouldTerminate = true;
+	{
+		LOGD("Waiting for the %s thread to finish", m_ThreadName.c_str());
+		if (m_Thread.joinable())
+		{
+			m_Thread.join();
+		}
+		LOGD("The %s thread finished", m_ThreadName.c_str());
+	}
+	m_ShouldTerminate = false;
+}
+
+
+
+
+
+void cIsThread::Entrypoint(void)
+{
+	// Apply thread naming:
+	SetThreadName();
+
+	// Wait for initialisation:
+	m_Initialisation.Wait();
+
+	try
+	{
+		Execute();
+	}
+	catch (const std::exception & Oops)
+	{
+		LOGERROR("Thread %s faulted with standard exception: %s", m_ThreadName.c_str(), Oops.what());
+		std::abort();
+	}
+	catch (...)
+	{
+		LOGERROR("Thread %s faulted with unknown exception!", m_ThreadName.c_str());
+		std::abort();
+	}
+}
+
+
+
+
+
+void cIsThread::SetThreadName() const
 {
 #if defined(_MSC_VER) && !defined(NDEBUG)
 	/* Sets the name of this thread.
 	(When in MSVC, the debugger provides "thread naming" by catching special exceptions)
 	Code adapted from MSDN: https://msdn.microsoft.com/en-us/library/xcb2z8hs.aspx */
+
+	if (m_ThreadName.empty())
+	{
+		return;
+	}
 
 #pragma pack(push, 8)
 	struct THREADNAME_INFO
@@ -49,80 +113,15 @@ void cIsThread::DoExecute(void)
 	};
 #pragma pack(pop)
 
-	if (!m_ThreadName.empty())
-	{
-		const DWORD NAME_EXCEPTION = 0x406D1388;
-		const THREADNAME_INFO Name = { 0x1000, m_ThreadName.c_str(), -1, 0 };
+	const DWORD NAME_EXCEPTION = 0x406D1388;
+	const THREADNAME_INFO Name = { 0x1000, m_ThreadName.c_str(), -1, 0 };
 
-		__try
-		{
-			RaiseException(NAME_EXCEPTION, 0, sizeof(Name) / sizeof(ULONG_PTR), reinterpret_cast<const ULONG_PTR *>(&Name));
-		}
-		__except (EXCEPTION_EXECUTE_HANDLER)
-		{
-		}
+	__try
+	{
+		RaiseException(NAME_EXCEPTION, 0, sizeof(Name) / sizeof(ULONG_PTR), reinterpret_cast<const ULONG_PTR *>(&Name));
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
 	}
 #endif
-
-	m_evtStart.Wait();
-	Execute();
-}
-
-
-
-
-
-bool cIsThread::Start(void)
-{
-	try
-	{
-		// Initialize the thread:
-		m_Thread = std::thread(&cIsThread::DoExecute, this);
-
-		// Notify the thread that initialization is complete and it can run its code safely:
-		m_evtStart.Set();
-
-		return true;
-	}
-	catch (const std::system_error & a_Exception)
-	{
-		LOGERROR("cIsThread::Start error %i: could not construct thread %s; %s", a_Exception.code().value(), m_ThreadName.c_str(), a_Exception.code().message().c_str());
-		return false;
-	}
-}
-
-
-
-
-
-void cIsThread::Stop(void)
-{
-	m_ShouldTerminate = true;
-	Wait();
-	m_ShouldTerminate = false;
-}
-
-
-
-
-
-bool cIsThread::Wait(void)
-{
-	LOGD("Waiting for the %s thread to finish", m_ThreadName.c_str());
-	if (m_Thread.joinable())
-	{
-		try
-		{
-			m_Thread.join();
-			return true;
-		}
-		catch (const std::system_error & a_Exception)
-		{
-			LOGERROR("%s error %i: could not join the %s thread; %s", __FUNCTION__, a_Exception.code().value(), m_ThreadName.c_str(), a_Exception.code().message().c_str());
-			return false;
-		}
-	}
-
-	LOGD("The %s thread finished", m_ThreadName.c_str());
-	return true;
 }
