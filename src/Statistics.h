35e15572e12cd81f5245570ee6d023dcaca60dcb
nshah25 <68348722+nshah25@users.noreply.github.com>
2021-04-12 20:29:26 -0400

    Fixed issue #5166

--- src/Statistics.h
+++ src/Statistics.h
@@ -1,4 +1,5 @@
 
+
 // Statistics.h
 
 #pragma once
@@ -6,7 +7,8 @@
 #include "Registries/Statistics.h"
 
 /* Hello fellow developer !
-In case you are trying to add new statistics to Cuberite you need to do a few things:
+In case you are trying to add new statistics to Cuberite you need to do a few
+things:
 ---------------------------------------------------------------------------
 1. add a new entry to the enum class Statistic in Registries\Statistics.h file
 2. add this to serialization functions in WorldStorage\NamespaceSerializer.cpp
@@ -14,9 +16,10 @@ In case you are trying to add new statistics to Cuberite you need to do a few th
 	so use the same string!
 
 In case you want to add a mapping of network IDs to the used stats
-you will find a lua script in ../Tools/BlockTypePaletteGenerator/ExportStatMapping.lua
-it will provide you with information how to use it. you need a registries.json
-exported from the server https://wiki.vg/Data_Generators
+you will find a lua script in
+../Tools/BlockTypePaletteGenerator/ExportStatMapping.lua it will provide you
+with information how to use it. you need a registries.json exported from the
+server https://wiki.vg/Data_Generators
 
 		Greetings 12xx12 */
 
@@ -25,10 +28,11 @@ exported from the server https://wiki.vg/Data_Generators
 
 
 /** Class that manages the statistics and achievements of a single player. */
+
+// tolua_begin
 class cStatManager
 {
-public:
-
+  public:
 	typedef unsigned StatValue;
 	typedef std::unordered_map<Statistic, StatValue> CustomStore;
 
@@ -38,8 +42,11 @@ public:
 	/** Increments the specified statistic. Returns the new value. */
 	StatValue AddValue(Statistic a_Stat, StatValue a_Delta = 1);
 
-	/** Returns whether the prerequisite for awarding an achievement are satisfied. */
+	/** Returns whether the prerequisite for awarding an achievement are
+	 * satisfied. */
 	bool SatisfiesPrerequisite(Statistic a_Stat);
+
+	// tolua_end
 
 	/** Invokes the given callbacks for each category of tracked statistics. */
 	template <class CustomCallback>
@@ -48,8 +55,7 @@ public:
 		a_Custom(m_CustomStatistics);
 	}
 
-private:
-
+  private:
 	/** Returns if a statistic is both present and has nonzero value. */
 	bool IsStatisticPresent(Statistic a_Stat) const;
 
@@ -57,3 +63,4 @@ private:
 
 	CustomStore m_CustomStatistics;
 };
+
