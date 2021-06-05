2d75b671941c914803c3b9b9a6b37450a1683600
12xx12 <44411062+12xx12@users.noreply.github.com>
2021-06-05 13:31:38 +0200

    fixed build

--- src/Blocks/BlockDaylightSensor.h
+++ src/Blocks/BlockDaylightSensor.h
@@ -38,7 +38,6 @@ private:
 
 	virtual cItems ConvertToPickups(BlockState, const cEntity * a_Digger, const cItem * a_Tool) const override
 	{
-		UNUSED(a_Digger);
 		UNUSED(a_Tool);
 		// Always drop the regular daylight sensor:
 		return { Item::DaylightDetector };
