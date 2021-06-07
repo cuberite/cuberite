8b62245dfbcb610fd1aead1889c349afc0db8e97
Tiger Wang <ziwei.tiger@outlook.com>
2021-06-07 10:11:41 +0100

    Remove LUA_PLUGIN_NAME_VAR_NAME

--- src/Bindings/PluginLua.cpp
+++ src/Bindings/PluginLua.cpp
@@ -87,8 +87,6 @@ bool cPluginLua::Load(void)
 		// Inject the identification global variables into the state:
 		lua_pushlightuserdata(m_LuaState, this);
 		lua_setglobal(m_LuaState, LUA_PLUGIN_INSTANCE_VAR_NAME);
-		lua_pushstring(m_LuaState, GetName().c_str());
-		lua_setglobal(m_LuaState, LUA_PLUGIN_NAME_VAR_NAME);
 
 		// Add the plugin's folder to the package.path and package.cpath variables (#693):
 		m_LuaState.AddPackagePath("path", GetLocalFolder() + "/?.lua");
