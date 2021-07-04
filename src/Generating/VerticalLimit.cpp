4134b4b84a6ca40fd5f7f93a3d532c6c41d7d5c4
x12xx12x <44411062+12xx12@users.noreply.github.com>
2021-07-04 16:48:47 +0200

    removed duplicate macro

--- src/Generating/VerticalLimit.cpp
+++ src/Generating/VerticalLimit.cpp
@@ -8,19 +8,6 @@
 
 
 
-// Emit a warning if the first param is true
-#define CONDWARNING(ShouldLog, Fmt, ...) \
-	do { \
-		if (ShouldLog) \
-		{ \
-			LOGWARNING(Fmt, __VA_ARGS__); \
-		} \
-	} while (false)
-
-
-
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // Globals:
 
