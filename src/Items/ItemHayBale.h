5b9f3d8acbe04b886049df7a078da4485649b94b
x12xx12x <44411062+12xx12@users.noreply.github.com>
2021-11-26 10:48:14 +0100

    Added HayBale handler

--- src/Items/ItemHayBale.h
+++ src/Items/ItemHayBale.h
@@ -1,10 +1,53 @@
 
 #pragma once
 
+#include "Registries/BlockStates.h"
+
 class cItemHayBaleHandler : public cItemHandler
 {
-public:
 	using Super = cItemHandler;
 
-	cItemHayBaleHandler() : cItemHandler(Item::HayBale) {}
+public:
+
+	cItemHayBaleHandler() : Super(Item::HayBale) {}
+
+	virtual bool IsPlaceable(void) override
+	{
+		return true;
+	}
+
+protected:
+	virtual bool CommitPlacement(cPlayer & a_Player, const cItem & a_HeldItem, Vector3i a_PlacePosition, eBlockFace a_ClickedBlockFace, Vector3i a_CursorPosition) override
+	{
+		auto Facing = DisplacementYawToFacing(a_PlacePosition, a_Player.GetEyePosition(), a_Player.GetYaw());
+		auto Axis = Block::HayBale::Axis::X;
+		switch (Facing)
+		{
+			case BLOCK_FACE_NONE:
+			{
+				ASSERT(!"Facing is NONE");
+				break;
+			}
+			case BLOCK_FACE_XM:
+			case BLOCK_FACE_XP:
+			{
+				Axis = Block::HayBale::Axis::X;
+				break;
+			}
+			case BLOCK_FACE_YM:
+			case BLOCK_FACE_YP:
+			{
+				Axis = Block::HayBale::Axis::Y;
+				break;
+			}
+			case BLOCK_FACE_ZM:
+			case BLOCK_FACE_ZP:
+			{
+				Axis = Block::HayBale::Axis::Z;
+				break;
+			}
+		}
+		a_Player.PlaceBlock(a_PlacePosition, Block::HayBale::HayBale(Axis));
+		return true;
+	}
 };
