9172076e213d1419bc86b0d24924ac7836794193
Persson-dev <sim16.prib@gmail.com>
2022-01-15 11:24:24 +0100

    Add POI definition

---
+++ src/PointOfInterest.cpp
@@ -0,0 +1,156 @@
+
+#include "Globals.h"  // NOTE: MSVC stupidness requires this to be the same across all modules
+
+#include "BlockType.h"
+#include "PointOfInterest.h"
+
+
+
+
+
+ePointOfInterestType cPointOfInterest::GetPointOnInterestType(BLOCKTYPE a_BlockType)
+{
+	switch (a_BlockType)
+	{
+		/*
+		case E_BLOCK_BLAST_FURNACE:
+		{
+			return poiArmorer;
+		}
+
+		case E_BLOCK_SMOKER:
+		{
+			return poiButcher;
+		}
+
+		case E_BLOCK_CARTOGRAPHY_TABLE:
+		{
+			return poiCartographer;
+		}
+		*/
+
+		case E_BLOCK_BREWING_STAND:
+		{
+			return poiCleric;
+		}
+
+		/*
+		case E_BLOCK_COMPOSTER:
+		{
+			return poiFarmer;
+		}
+
+		case E_BLOCK_BARREL:
+		{
+			return poiFisherman;
+		}
+
+		case E_BLOCK_FLETCHING_TABLE:
+		{
+			return poiFletcher;
+		}
+		*/
+
+		case E_BLOCK_CAULDRON:
+		{
+			return poiLetherworker;
+		}
+
+		/*
+		case E_BLOCK_LECTERN:
+		{
+			return poiLibrairian;
+		}
+
+		case E_BLOCK_STONECUTTER:
+		{
+			return poiMason;
+		}
+
+		case E_BLOCK_LOOM:
+		{
+			return poiShepherd;
+		}
+
+		case E_BLOCK_SMITHING_TABLE:
+		{
+			return poiToolsmith;
+		}
+
+		case E_BLOCK_GRINDSTONE:
+		{
+			return poiWeaponsmith;
+		}
+
+		case E_BLOCK_BEEHIVE:
+		{
+			return poiBeehive;
+		}
+
+		case E_BLOCK_BEE_NEST:
+		{
+			return poiBeeNest;
+		}
+		*/
+
+		case E_BLOCK_NETHER_PORTAL:
+		{
+			return poiNetherPortal;
+		}
+
+		/*
+		case E_BLOCK_LODESTONE:
+		{
+			return poiLodestone;
+		}
+
+		case E_BLOCK_LIGHTNING_ROD:
+		{
+			return poiLightningRod;
+		}
+		*/
+
+		default:
+		{
+			return poiNone;
+		}
+	}
+}
+
+
+
+
+
+int cPointOfInterest::GetMaxFreeTickets(ePointOfInterestType a_Type)
+{
+	switch (a_Type)
+	{
+		case poiArmorer:
+		case poiButcher:
+		case poiCartographer:
+		case poiCleric:
+		case poiFarmer:
+		case poiFisherman:
+		case poiFletcher:
+		case poiLetherworker:
+		case poiLibrairian:
+		case poiMason:
+		case poiShepherd:
+		case poiToolsmith:
+		case poiWeaponsmith:
+		case poiHome:
+		{
+			return 1;
+		}
+
+		case poiMeeting:
+		{
+			return 32;
+		}
+
+	default:
+		{
+			return 0;
+		}
+	}
+}
---
+++ src/PointOfInterest.h
@@ -0,0 +1,76 @@
+
+// PointOfInterest.h
+
+// Points Of Interest (POI) are used to store special blocks in memory to interact with entities. See https://minecraft.fandom.com/wiki/Java_Edition_level_format#poi_format
+
+
+
+
+
+#pragma once
+
+
+
+
+
+enum ePointOfInterestType
+{
+	// Villagers
+	poiArmorer,  // Blast furnace
+	poiButcher,  // Smoker
+	poiCartographer,  // Cartography table
+	poiCleric,  // Brewing stand
+	poiFarmer,  // Composter
+	poiFisherman,  // Barrel
+	poiFletcher,  // Fletching table
+	poiLetherworker,  // Cauldron
+	poiLibrairian,  // Lectern
+	poiMason,  // Stonecutter
+	poiShepherd,  // Loom
+	poiToolsmith,  // Smithing table
+	poiWeaponsmith,  // Grindstone
+	poiHome,  // Bed
+	poiMeeting,  // Bell
+
+	// Bees
+	poiBeehive,  // Beehive
+	poiBeeNest,  // Bee nest
+
+	// Others
+	poiNetherPortal,  // Nether portal (used for faster portal travel)
+	poiLodestone,  // Lodestone (used for player respawn)
+	poiLightningRod,  // Lightning rod (used for lightnings)
+
+	poiNone
+};
+
+
+
+
+
+class cPointOfInterest
+{
+public:
+
+	cPointOfInterest(Vector3i a_BlockPosition, BLOCKTYPE a_BlockType) :
+		m_BlockPosition(a_BlockPosition), m_Type(GetPointOnInterestType(a_BlockType)),
+		m_FreeTickets(GetMaxFreeTickets(m_Type)) {}
+
+	static ePointOfInterestType GetPointOnInterestType(BLOCKTYPE a_BlockType);
+	static int GetMaxFreeTickets(ePointOfInterestType a_Type);
+
+	// Getters
+	const Vector3i & GetBlockPosition() const { return m_BlockPosition; }
+	ePointOfInterestType GetPoiType() const { return m_Type; }
+	int GetTicketsRemaining() const { return m_FreeTickets; }
+
+	void RemoveTicket() { m_FreeTickets = std::max(0, m_FreeTickets - 1); }
+	void AddTicket() { m_FreeTickets = std::min(GetMaxFreeTickets(m_Type), m_FreeTickets + 1); }
+
+private:
+
+	Vector3i m_BlockPosition;
+	ePointOfInterestType m_Type;
+	int m_FreeTickets;
+
+};
