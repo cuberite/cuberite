b04a30129ca24f598844cc2592fee5173a543fe7
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2022-04-27 00:19:55 +0200

    Remove code that's no longer needed

--- src/Mobs/SnowGolem.cpp
+++ src/Mobs/SnowGolem.cpp
@@ -20,43 +20,6 @@ cSnowGolem::cSnowGolem(void) :
 
 
 
-bool cSnowGolem::DoTakeDamage(TakeDamageInfo & a_TDI)
-{
-	// Make sure that snow golems will never target players and other snow golems
-	auto OldTarget = GetTarget();
-	auto OldState = m_EMState;
-
-	if (!Super::DoTakeDamage(a_TDI))
-	{
-		return false;
-	}
-
-	// Quick and dirty fix: just revert changes made by Super::DoTakeDamage...
-	// unless the new target is a hostile and it isn't a ghast
-	if (OldTarget != GetTarget())
-	{
-		if (
-			(GetTarget() != nullptr) && (GetTarget()->IsMob()) &&
-			(static_cast<cMonster *>(GetTarget())->GetMobFamily() == mfHostile) &&
-			(static_cast<cMonster *>(GetTarget())->GetMobType() != mtGhast)
-		)
-		{
-			m_EMState = CHASING;
-		}
-		else
-		{
-			SetTarget(OldTarget);
-			m_EMState = OldState;
-		}
-	}
-
-	return true;
-}
-
-
-
-
-
 void cSnowGolem::GetDrops(cItems & a_Drops, cEntity * a_Killer)
 {
 	UNUSED(a_Killer);
--- src/Mobs/SnowGolem.h
+++ src/Mobs/SnowGolem.h
@@ -18,7 +18,6 @@ public:
 
 	CLASS_PROTODEF(cSnowGolem)
 
-	virtual bool DoTakeDamage(TakeDamageInfo & a_TDI) override;
 	virtual void Tick(std::chrono::milliseconds a_Dt, cChunk & a_Chunk) override;
 	virtual void GetDrops(cItems & a_Drops, cEntity * a_Killer = nullptr) override;
 	virtual bool Attack(std::chrono::milliseconds a_Dt) override;
