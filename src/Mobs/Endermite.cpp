f408725db9ee9ceb41e92464b221041cbf74547d
Debucquoy <debucqquoy.anthony@gmail.com>
2022-11-15 15:16:40 +0100

    destroy endermite if 2 min, not if name is set

--- src/Mobs/Endermite.cpp
+++ src/Mobs/Endermite.cpp
@@ -7,13 +7,30 @@
 #include "../Chunk.h"
 #include "../Blocks/BlockHandler.h"
 #include "../Blocks/BlockInfested.h"
+#include <string>
 
 
 
 
 
 cEndermite::cEndermite() :
-	Super("Endermite", mtEndermite, "entity.endermite.hurt", "entity.endermite.death", "entity.endermite.ambient", 0.4f, 0.3f)
+	Super("Endermite", mtEndermite, "entity.endermite.hurt", "entity.endermite.death", "entity.endermite.ambient", 0.4f, 0.3f),
+	m_Timer(0),
+	m_Lifetime(2 * 1000 * 60) // 2 minutes (2 * 1000 (mili to sec) * 60 (sec to min) * 2 because tick = 1/2 sec)
 {
 }
 
+
+void cEndermite::Tick(std::chrono::milliseconds a_Dt, cChunk & a_Chunk){
+	Super::Tick(a_Dt, a_Chunk);
+
+	// Not destroying the endermite if a name is set
+	if(m_CustomName.empty()){
+		m_Timer += a_Dt;
+		//Destroy the endermite after 2 minutes
+		if(m_Timer > m_Lifetime){
+			Destroy();
+		}
+
+	}
+}
--- src/Mobs/Endermite.h
+++ src/Mobs/Endermite.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "AggressiveMonster.h"
+#include <chrono>
 
 
 
@@ -12,9 +13,15 @@ class cEndermite:
 {
 	using Super = cAggressiveMonster;
 
+	//Endermite should despawn in two minutes
+	std::chrono::milliseconds m_Lifetime;
+	std::chrono::milliseconds m_Timer;
+
 public:
 
 	cEndermite();
 
 	CLASS_PROTODEF(cEndermite)
+
+	void Tick(std::chrono::milliseconds a_Dt, cChunk & a_Chunk);
 } ;
