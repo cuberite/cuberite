366a61bd99ca09029f36eddfc16b4824e24c4c11
Niels Breuker <niels.breuker@hotmail.nl>
2023-03-12 23:43:27 +0100

    Added article explaining how to configure lua-language-server

--- Server/Plugins/APIDump/APIDesc.lua
+++ Server/Plugins/APIDump/APIDesc.lua
@@ -19239,6 +19239,10 @@ end
 			Title = "Setting up the ZeroBrane Studio Lua IDE",
 		},
 		{
+			FileName = "SettingUpLuaLanguageServer.html",
+			Title = "Setting up Lua-Language-Server (VSCode/Emacs)"
+		},
+		{
 			FileName = "UsingChunkStays.html",
 			Title = "Using ChunkStays",
 		},
