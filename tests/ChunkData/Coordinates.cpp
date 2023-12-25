a9c5afb4c2bbea85e234dc39997fd11be94e7b3b
x12xx12x <44411062+12xx12@users.noreply.github.com>
2023-12-25 15:48:25 +0100

    Added test for ChunkDataStore

--- tests/ChunkData/Coordinates.cpp
+++ tests/ChunkData/Coordinates.cpp
@@ -1,142 +1,132 @@
 
 #include "Globals.h"
-#include "../TestHelpers.h"
+#include <gtest/gtest.h>
 #include "ChunkData.h"
 
 
 
 
 /** Performs the entire cChunkData coordinates test. */
-static void test()
-{
-	{
-		ChunkBlockData buffer;
+TEST(ChunkDataCoordinates, SetAndGetOutOfBounds){
+    ChunkBlockData buffer;
 
-		// Empty chunks
-		buffer.SetBlock({ 0, 0, 0 }, 0xAB);
-		TEST_EQUAL(buffer.GetBlock({ 0, 0, 0 }), 0xAB);
-		buffer.SetMeta({ 0, 16, 0 }, 0xC);
-		TEST_EQUAL(buffer.GetMeta({ 0, 16, 0 }), 0xC);
+    // Empty chunks
+    buffer.SetBlock({ 0, 0, 0 }, 0xAB);
+    EXPECT_EQ(buffer.GetBlock({ 0, 0, 0 }), 0xAB);
+    buffer.SetMeta({ 0, 16, 0 }, 0xC);
+    EXPECT_EQ(buffer.GetMeta({ 0, 16, 0 }), 0xC);
 
-		// loaded but not written segments
-		TEST_EQUAL(buffer.GetBlock({ 1, 0, 0 }), 0x0);
-		TEST_EQUAL(buffer.GetMeta({ 1, 16, 0 }), 0x0);
+    // loaded but not written segments
+    EXPECT_EQ(buffer.GetBlock({ 1, 0, 0 }), 0x0);
+    EXPECT_EQ(buffer.GetMeta({ 1, 16, 0 }), 0x0);
 
-		// Notloaded segments
-		TEST_EQUAL(buffer.GetBlock({ 0, 32, 0 }), 0x0);
-		TEST_EQUAL(buffer.GetMeta({ 0, 48, 0 }), 0x0);
+    // Notloaded segments
+    EXPECT_EQ(buffer.GetBlock({ 0, 32, 0 }), 0x0);
+    EXPECT_EQ(buffer.GetMeta({ 0, 48, 0 }), 0x0);
 
-		// Out of range GetBlock
-		TEST_ASSERTS(
-			buffer.GetBlock({ -1, 0, 0 });
-		);
-		TEST_ASSERTS(
-			buffer.GetBlock({ 0, -1, 0 });
-		);
-		TEST_ASSERTS(
-			buffer.GetBlock({ 0, 0, -1 });
-		);
-		TEST_ASSERTS(
-			buffer.GetBlock({ 256, 0, 0 });
-		);
-		TEST_ASSERTS(
-			buffer.GetBlock({ 0, 256, 0 });
-		);
-		TEST_ASSERTS(
-			buffer.GetBlock({ 0, 0, 256 });
-		);
+    // Out of range GetBlock
+    EXPECT_ANY_THROW(
+        buffer.GetBlock({ -1, 0, 0 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetBlock({ 0, -1, 0 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetBlock({ 0, 0, -1 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetBlock({ 256, 0, 0 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetBlock({ 0, 256, 0 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetBlock({ 0, 0, 256 });
+    );
 
-		// Out of range SetBlock
-		TEST_ASSERTS(
-			buffer.SetBlock({ -1, 0, 0 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetBlock({ 0, -1, 0 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetBlock({ 0, 0, -1 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetBlock({ 256, 0, 0 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetBlock({ 0, 256, 0 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetBlock({ 0, 0, 256 }, 0)
-		);
+    // Out of range SetBlock
+    EXPECT_ANY_THROW(
+        buffer.SetBlock({ -1, 0, 0 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetBlock({ 0, -1, 0 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetBlock({ 0, 0, -1 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetBlock({ 256, 0, 0 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetBlock({ 0, 256, 0 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetBlock({ 0, 0, 256 }, 0)
+    );
 
-		// Out of range GetMeta
-		TEST_ASSERTS(
-			buffer.GetMeta({ -1, 0, 0 });
-		);
-		TEST_ASSERTS(
-			buffer.GetMeta({ 0, -1, 0 });
-		);
-		TEST_ASSERTS(
-			buffer.GetMeta({ 0, 0, -1 });
-		);
-		TEST_ASSERTS(
-			buffer.GetMeta({ 256, 0, 0 });
-		);
-		TEST_ASSERTS(
-			buffer.GetMeta({ 0, 256, 0 });
-		);
-		TEST_ASSERTS(
-			buffer.GetMeta({ 0, 0, 256 });
-		);
+    // Out of range GetMeta
+    EXPECT_ANY_THROW(
+        buffer.GetMeta({ -1, 0, 0 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetMeta({ 0, -1, 0 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetMeta({ 0, 0, -1 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetMeta({ 256, 0, 0 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetMeta({ 0, 256, 0 });
+    );
+    EXPECT_ANY_THROW(
+        buffer.GetMeta({ 0, 0, 256 });
+    );
 
-		// Out of range SetMeta
-		TEST_ASSERTS(
-			buffer.SetMeta({ -1, 0, 0 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetMeta({ 0, -1, 0 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetMeta({ 0, 0, -1 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetMeta({ 256, 0, 0 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetMeta({ 0, 256, 0 }, 0)
-		);
-		TEST_ASSERTS(
-			buffer.SetMeta({ 0, 0, 256 }, 0)
-		);
-	}
-
-	{
-		ChunkBlockData buffer;
-
-		// Zero's
-		buffer.SetBlock({ 0, 0, 0 }, 0x0);
-		buffer.SetBlock({ 0, 0, 1 }, 0xab);
-		TEST_EQUAL(buffer.GetBlock({ 0, 0, 0 }), 0x0);
-		TEST_EQUAL(buffer.GetBlock({ 0, 0, 1 }), 0xab);
-
-		buffer.SetMeta({ 0, 16, 0 }, 0x0);
-		buffer.SetMeta({ 0, 16, 1 }, 0xc);
-		TEST_EQUAL(buffer.GetMeta({ 0, 16, 0 }), 0x0);
-		TEST_EQUAL(buffer.GetMeta({ 0, 16, 1 }), 0xc);
-	}
-
-
-	{
-		// Operator =
-		ChunkBlockData buffer;
-		buffer.SetBlock({ 0, 0, 0 }, 0x42);
-		ChunkBlockData copy;
-		copy = std::move(buffer);
-		TEST_EQUAL(copy.GetBlock({ 0, 0, 0 }), 0x42);
-	}
+    // Out of range SetMeta
+    EXPECT_ANY_THROW(
+        buffer.SetMeta({ -1, 0, 0 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetMeta({ 0, -1, 0 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetMeta({ 0, 0, -1 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetMeta({ 256, 0, 0 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetMeta({ 0, 256, 0 }, 0)
+    );
+    EXPECT_ANY_THROW(
+        buffer.SetMeta({ 0, 0, 256 }, 0)
+    );
 }
 
+TEST(ChunkDataCoordinates, SetBlockTypeAndMeta)
+{
+    ChunkBlockData buffer;
 
+    // Zero's
+    buffer.SetBlock({ 0, 0, 0 }, 0x0);
+    buffer.SetBlock({ 0, 0, 1 }, 0xab);
+    EXPECT_EQ(buffer.GetBlock({ 0, 0, 0 }), 0x0);
+    EXPECT_EQ(buffer.GetBlock({ 0, 0, 1 }), 0xab);
 
+    buffer.SetMeta({ 0, 16, 0 }, 0x0);
+    buffer.SetMeta({ 0, 16, 1 }, 0xc);
+    EXPECT_EQ(buffer.GetMeta({ 0, 16, 0 }), 0x0);
+    EXPECT_EQ(buffer.GetMeta({ 0, 16, 1 }), 0xc);
+}
 
-
-IMPLEMENT_TEST_MAIN("ChunkData Coordinates",
-	test();
-)
+TEST(ChunkDataCoordinates, Copy)
+{
+    // Operator =
+    ChunkBlockData buffer;
+    buffer.SetBlock({ 0, 0, 0 }, 0x42);
+    ChunkBlockData copy;
+    copy = std::move(buffer);
+    EXPECT_EQ(copy.GetBlock({ 0, 0, 0 }), 0x42);
+}
