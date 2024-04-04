04e2cef464bb229d75483e9c0a4ceb776d36d833
MightyFilipns <filip.milosevicns@gmail.com>
2024-04-04 19:54:32 +0200

    Clang build fix 4

--- src/Protocol/ChunkDataSerializer.cpp
+++ src/Protocol/ChunkDataSerializer.cpp
@@ -570,13 +570,14 @@ inline void cChunkDataSerializer::Serialize573(const int a_ChunkX, const int a_C
 	}
 
 	// BiomeArray
-	int HORIZONTAL_SECTION_COUNT = (int)round(log(16.0) / log(2.0)) - 2;  // 2
-	int VERTICAL_SECTION_COUNT = (int)round(log(256.0) / log(2.0)) - 2;   // 6
-	int DEFAULT_LENGTH = 1 << HORIZONTAL_SECTION_COUNT +  HORIZONTAL_SECTION_COUNT + VERTICAL_SECTION_COUNT;  // should be 1024
+	int HORIZONTAL_SECTION_COUNT = static_cast<int>(round(log(16.0) / log(2.0))) - 2;  // 2
+	int VERTICAL_SECTION_COUNT = static_cast<int>(round(log(256.0) / log(2.0))) - 2;   // 6
+	int DEFAULT_LENGTH = 1 << (HORIZONTAL_SECTION_COUNT +  HORIZONTAL_SECTION_COUNT + VERTICAL_SECTION_COUNT);  // should be 1024
+	
 	/* int HORIZONTAL_BIT_MASK = (1 << HORIZONTAL_SECTION_COUNT) - 1;
 	int VERTICAL_BIT_MASK = (1 << VERTICAL_SECTION_COUNT) - 1;
 	*/
-	for (size_t i = 0; i < DEFAULT_LENGTH; i++)
+	for (int i = 0; i < DEFAULT_LENGTH; i++)
 	{
 		/* int xoffset =
 			i & HORIZONTAL_BIT_MASK;  // reminder when dividing i by 4
@@ -584,7 +585,7 @@ inline void cChunkDataSerializer::Serialize573(const int a_ChunkX, const int a_C
 		int zoffset = i >> HORIZONTAL_SECTION_COUNT & HORIZONTAL_BIT_MASK;  // diving i by 4 and then taking the reminder when dividing by 4 */
 
 		int realx = i % 16;
-		int realz = (i /16) % 16;
+		int realz = (i / 16) % 16;
 		// LOG("X %d - Y %d - Z %d - I %d - RX - %d - RZ - %d", xoffset, ycord, zoffset, i, realx, realz);
 		m_Packet.WriteBEInt32(a_BiomeMap[realx + realz * 16]);  //  Biome ???
 	}
@@ -599,7 +600,7 @@ inline void cChunkDataSerializer::Serialize573(const int a_ChunkX, const int a_C
 			ChunkSectionDataArraySize * 8   // Actual section data, lots of bytes (multiplier 1 long = 8 bytes)
 		);
 
-	const size_t BiomeDataSize = cChunkDef::Width * cChunkDef::Width;
+	//const size_t BiomeDataSize = cChunkDef::Width * cChunkDef::Width;
 	const size_t ChunkSize =
 		(ChunkSectionSize * Bitmask.second  //  +
 		//  BiomeDataSize * 4    // Biome data now BE ints
