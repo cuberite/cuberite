2b7c69c28d81e455cb870a4a6d967b5a42c277fe
MightyFilipns <filip.milosevicns@gmail.com>
2024-06-16 12:02:46 +0200

    Connection support for 1.16.X Partial 1.17 support For 1.17 chunk data packet appear to be sending also some phantom packets causing the client to disconnect

--- src/ClientHandle.cpp
+++ src/ClientHandle.cpp
@@ -2034,6 +2034,8 @@ void cClientHandle::SendData(const ContiguousByteBufferView a_Data)
 		return;
 	}
 
+	LOG("len %d", a_Data.length());
+
 	cCSLock Lock(m_CSOutgoingData);
 	m_OutgoingData += a_Data;
 }
