658408f2a02db4ce4429e7f96706eb597e3efa20
Alexander Harkness <me@bearbin.net>
2024-06-30 16:37:14 +0000

    Apply suggestions from code review

--- src/Map.cpp
+++ src/Map.cpp
@@ -23,7 +23,7 @@ cMap::cMap(unsigned int a_ID, cWorld * a_World):
 	m_Scale(3),
 	m_CenterX(0),
 	m_CenterZ(0),
-	m_Dirty(false),
+	m_Dirty(false), // This constructor is for an empty map object which will be filled by the caller with the correct values - it does not need saving.
 	m_World(a_World),
 	m_Name(fmt::format(FMT_STRING("map_{}"), m_ID))
 {
@@ -41,7 +41,7 @@ cMap::cMap(unsigned int a_ID, int a_CenterX, int a_CenterZ, cWorld * a_World, un
 	m_Scale(a_Scale),
 	m_CenterX(a_CenterX),
 	m_CenterZ(a_CenterZ),
-	m_Dirty(true),
+	m_Dirty(true), // This constructor is for creating a brand new map in game, it will always need saving.
 	m_World(a_World),
 	m_Name(fmt::format(FMT_STRING("map_{}"), m_ID))
 {
