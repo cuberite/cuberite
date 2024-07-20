c5d688698859c089c7f4d2b09afcf96a2b54cb46
MightyFilipns <filip.milosevicns@gmail.com>
2024-07-20 10:38:42 +0200

    1.20 support

---
+++ src/Protocol/Protocol_1_20.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "Protocol_1_19.h"
+
+
+
+
+
+class cProtocol_1_20
+	: public cProtocol_1_19_4
+{
+	using Super = cProtocol_1_19_4;
+public:
+
+	using Super::Super;
+
+protected:
+	virtual void    SendLogin(const cPlayer & a_Player, const cWorld & a_World) override;
+
+	virtual Version GetProtocolVersion() const override;
+};
+
+
+
+
+
+class cProtocol_1_20_2
+	: public cProtocol_1_20
+{
+	using Super = cProtocol_1_20;
+public:
+
+	using Super::Super;
+
+protected:
+
+	virtual Version GetProtocolVersion() const override;
+};
+
+
+
+
+
+class cProtocol_1_20_3
+	: public cProtocol_1_20_2
+{
+	using Super = cProtocol_1_20_2;
+public:
+
+	using Super::Super;
+
+protected:
+
+	virtual Version GetProtocolVersion() const override;
+};
+
+
+
+
+
+class cProtocol_1_20_5
+	: public cProtocol_1_20_3
+{
+	using Super = cProtocol_1_20_3;
+public:
+
+	using Super::Super;
+
+protected:
+
+	virtual Version GetProtocolVersion() const override;
+};
