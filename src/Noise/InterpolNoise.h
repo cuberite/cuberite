80e10101d2bea1bc8ec22a7c87901f5c68f6b58a
Victor <47831936+solvictor@users.noreply.github.com>
2024-08-07 10:01:35 +0000

    Reverted all changes on InterpolNoise.h

--- src/Noise/InterpolNoise.h
+++ src/Noise/InterpolNoise.h
@@ -355,7 +355,6 @@ public:
 		int SameX[MAX_SIZE];
 		int SameY[MAX_SIZE];
 		int NumSameX, NumSameY;
-
 		CalcFloorFrac(a_SizeX, a_StartX, a_EndX, FloorX, FracX, SameX, NumSameX);
 		CalcFloorFrac(a_SizeY, a_StartY, a_EndY, FloorY, FracY, SameY, NumSameY);
 
@@ -414,7 +413,6 @@ public:
 		int SameY[MAX_SIZE];
 		int SameZ[MAX_SIZE];
 		int NumSameX, NumSameY, NumSameZ;
-
 		CalcFloorFrac(a_SizeX, a_StartX, a_EndX, FloorX, FracX, SameX, NumSameX);
 		CalcFloorFrac(a_SizeY, a_StartY, a_EndY, FloorY, FracY, SameY, NumSameY);
 		CalcFloorFrac(a_SizeZ, a_StartZ, a_EndZ, FloorZ, FracZ, SameZ, NumSameZ);
