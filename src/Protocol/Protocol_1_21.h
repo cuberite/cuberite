4bf279d1610a5b93ca3f7ddbc9885d1262f53b3f
MightyFilipns <filip.milosevicns@gmail.com>
2024-10-26 12:38:17 +0200

    Fixes for 1.20.3

--- src/Protocol/Protocol_1_21.h
+++ src/Protocol/Protocol_1_21.h
@@ -36,5 +36,14 @@ public:
 protected:
 
 	virtual void SendSelectKnownPacks() override;
+	virtual void SendLoginSuccess() override;
+	virtual void SendTags(void) override;
+	virtual void SendLogin(const cPlayer & a_Player, const cWorld & a_World) override;
+	virtual void    SendPlayerMoveLook(const Vector3d a_Pos, const float a_Yaw, const float a_Pitch, const bool a_IsRelative) override;
+
+	virtual void HandlePacketClientSettings(cByteBuffer & a_ByteBuffer) override;
+	virtual bool HandlePacket(cByteBuffer & a_ByteBuffer, UInt32 a_PacketType) override;
+
+	virtual UInt32 GetPacketID(ePacketType a_PacketType) const override;
 	virtual Version GetProtocolVersion() const override;
 };
