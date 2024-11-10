70536d7dcc7e739acb14d9f323b91acd6c29db39
MightyFilipns <filip.milosevicns@gmail.com>
2024-11-10 17:10:42 +0100

    Add palete for 1.20 Fix entity metadta wirting in 1.20 Fix sound packet in 1.20

--- src/Protocol/Protocol_1_20.h
+++ src/Protocol/Protocol_1_20.h
@@ -21,6 +21,10 @@ protected:
 
 	virtual void    HandlePacketUpdateSign(cByteBuffer & a_ByteBuffer) override;
 
+	virtual UInt32  GetProtocolBlockType(BlockState a_Block) const override;
+	virtual UInt32  GetProtocolItemType(Item a_ItemID) const override;
+	virtual Item    GetItemFromProtocolID(UInt32 a_ProtocolID) const override;
+
 	virtual int     GetProtocolParticleID(const AString & a_ParticleName) const override;
 	virtual Version GetProtocolVersion() const override;
 };
