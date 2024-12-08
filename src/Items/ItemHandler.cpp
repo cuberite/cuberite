49cade9b13a40000c6533fb77285516fea9915b9
MightyFilipns <filip.milosevicns@gmail.com>
2024-12-08 18:27:26 +0100

    Item handler fix

--- src/Items/ItemHandler.cpp
+++ src/Items/ItemHandler.cpp
@@ -1963,7 +1963,6 @@ const cItemHandler & cItemHandler::For(Item a_ItemType)
 		case Item::GraniteStairs:                      return ItemGraniteStairsHandler;
 		case Item::GraniteWall:                        return ItemGraniteWallHandler;
 		case Item::GrassBlock:                         return ItemGrassBlockHandler;
-		case Item::Grass:                              return ItemGrassHandler;
 		case Item::Gravel:                             return ItemGravelHandler;
 		case Item::GrayBanner:                         return ItemGrayBannerHandler;
 		case Item::GrayBed:                            return ItemGrayBedHandler;
@@ -2489,7 +2488,6 @@ const cItemHandler & cItemHandler::For(Item a_ItemType)
 		case Item::SheepSpawnEgg:                      return ItemSheepSpawnEggHandler;
 		case Item::ShelterPotterySherd:                return ItemShelterPotterySherdHandler;
 		case Item::Shield:                             return ItemShieldHandler;
-		case Item::ShortGrass:                         return ItemShortGrassHandler;
 		case Item::Shroomlight:                        return ItemShroomlightHandler;
 		case Item::ShulkerBox:                         return ItemShulkerBoxHandler;
 		case Item::ShulkerShell:                       return ItemShulkerShellHandler;
