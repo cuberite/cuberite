e8597b7fba6985ee57b4de91f5033e77f7a2a69b
Spiros Vita <vitabytes@proton.me>
2025-04-12 15:07:41 +0300

    Add end platform generation code

---
+++ src/Generating/EndPlatform.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "World.h"
+
+class EndPlatform {
+    public:
+        /** Generates the End Obsidian Platform that players spawn on. It is called by cEntity::DetectPortal */
+        static void Generate(cWorld* m_EndWorld);        
+};
